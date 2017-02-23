f73ae4e421012df152de0754a65a4d12821fc79a
Itamar Turner-Trauring <itamar@datawire.io>
2017-02-23 14:10:11 -0500

    It works, except it doesn't actually.

--- connect.sh
+++ connect.sh
@@ -10,11 +10,11 @@ sleep 10 # Wait for pod to deploy
 CONFIG_DIR=$(mktemp -d)
 VPN_CONF=$CONFIG_DIR/vpn.conf
 kubectl cp telepresence:/client-config/vpn.conf $VPN_CONF
-DOCKER_IP=$(ifconfig docker0 | grep -Po 'inet addr:\K[^ ]*')
-DOCKER_IP=localhost
-sed -i -- "s/VPNSERVER/$DOCKER_IP/g" $VPN_CONF
+sed -i -- "s/VPNSERVER/localhost/g" $VPN_CONF
 sudo docker run --rm --name=k8s-vpn --user $(id -u itamarst) -v $HOME/.kube:/config/.kube:ro -v $HOME/.minikube:$HOME/.minikube:ro  wernight/kubectl port-forward telepresence 1194:1194 &
 sleep 5
 
+# Need something like this with resolv-conf updater for DNS; revisit:
+#sudo docker run --cap-add=NET_ADMIN --device /dev/net/tun --rm --net container:k8s-vpn -v $CONFIG_DIR:/vpn -e DNS=true dperson/openvpn-client
 
 sudo docker run --cap-add=NET_ADMIN --device /dev/net/tun --rm --net container:k8s-vpn -v $CONFIG_DIR:/config -e DNS=true farmcoolcow/openvpn-client --config /config/vpn.conf
