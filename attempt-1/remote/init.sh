64bc7f6000f14475932cedc511b3a278baac2331
Itamar Turner-Trauring <itamar@datawire.io>
2017-03-01 09:43:20 -0500

    Clear out first attempt.

--- attempt-1/remote/init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-# XXX /etc/openpvn is a volume so it gets wiped on container startup :(
-export OPENVPN=/etc/openvpn.temp
-export EASYRSA_PKI=$OPENVPN/pki
-export EASYRSA_VARS_FILE=$OPENVPN/vars
-ovpn_genconfig -u tcp://VPNSERVER
-echo VPNSERVER | OVPN_CN=VPNSERVER ovpn_initpki nopass
