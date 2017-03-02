45030255b1a0093b62a000802bda5c4b296c0452
Itamar Turner-Trauring <itamar@datawire.io>
2017-03-02 17:01:04 -0500

    Use new CLI.

--- testing/connect.sh
+++ testing/connect.sh
@@ -6,4 +6,4 @@ kubectl apply -f service.yaml
 kubectl apply -f deployment.yaml
 sleep 10 # Wait for pod to deploy
 
-sudo docker run --rm --name=yourcode-deployment  -v $HOME/.kube:/opt/.kube:ro -v $HOME/.minikube:$HOME/.minikube:ro -v $PWD:/output datawire/telepresence-local $(id -u itamarst) yourcode-deployment 8080
+../cli/telepresence --expose 8080 yourcode-deployment
