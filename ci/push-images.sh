5a79e7c05cd400beb0c1954e4b82f7dfd4ebbaef
Itamar Turner-Trauring <itamar@datawire.io>
2017-03-21 11:13:44 -0400

    Support a different registry.

--- ci/push-images.sh
+++ ci/push-images.sh
@@ -3,5 +3,5 @@
 make build
 # Push to Google Docker registry:
 export TELEPRESENCE_VERSION=$(make version)
-gcloud docker push gcr.io/${PROJECT_NAME}/datawire/telepresence-local:${TELEPRESENCE_VERSION}
-gcloud docker push gcr.io/${PROJECT_NAME}/datawire/telepresence-k8s:${TELEPRESENCE_VERSION}
+gcloud docker push gcr.io/${PROJECT_NAME}/telepresence-local:${TELEPRESENCE_VERSION}
+gcloud docker push gcr.io/${PROJECT_NAME}/telepresence-k8s:${TELEPRESENCE_VERSION}
