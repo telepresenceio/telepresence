7206f0c5f4ff0a99a505e19e8e4d813bcbc2fd52
Itamar Turner-Trauring <itamar@datawire.io>
2017-03-21 11:45:15 -0400

    Tag images for GCR.

--- ci/push-images.sh
+++ ci/push-images.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 # Build Docker images:
 make build
+# Tag Docker images with GCR tags:
+docker tag datawire/telepresence-local:${TELEPRESENCE_VERSION} \
+           gcr.io/{PROJECT_NAME}/teleperesence-local:${TELEPRESENCE_VERSION}
+docker tag datawire/telepresence-k8s:${TELEPRESENCE_VERSION} \
+       gcr.io/{PROJECT_NAME}/teleperesence-k8s:${TELEPRESENCE_VERSION}
+
 # Push to Google Docker registry:
 export TELEPRESENCE_VERSION=$(make version)
-gcloud docker push gcr.io/${PROJECT_NAME}/telepresence-local:${TELEPRESENCE_VERSION}
-gcloud docker push gcr.io/${PROJECT_NAME}/telepresence-k8s:${TELEPRESENCE_VERSION}
+gcloud docker -- push gcr.io/${PROJECT_NAME}/telepresence-local:${TELEPRESENCE_VERSION}
+gcloud docker -- push gcr.io/${PROJECT_NAME}/telepresence-k8s:${TELEPRESENCE_VERSION}
