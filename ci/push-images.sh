d7e895ca32ed54633d11c9e1e0f107972d91eec5
Itamar Turner-Trauring <itamar@datawire.io>
2017-03-21 13:04:22 -0400

    Fix typo.

--- ci/push-images.sh
+++ ci/push-images.sh
@@ -6,9 +6,9 @@ make build
 # Tag Docker images with GCR tags:
 export TELEPRESENCE_VERSION=$(make version)
 docker tag datawire/telepresence-local:${TELEPRESENCE_VERSION} \
-           gcr.io/${PROJECT_NAME}/teleperesence-local:${TELEPRESENCE_VERSION}
+           gcr.io/${PROJECT_NAME}/telepresence-local:${TELEPRESENCE_VERSION}
 docker tag datawire/telepresence-k8s:${TELEPRESENCE_VERSION} \
-       gcr.io/${PROJECT_NAME}/teleperesence-k8s:${TELEPRESENCE_VERSION}
+       gcr.io/${PROJECT_NAME}/telepresence-k8s:${TELEPRESENCE_VERSION}
 
 # Push to Google Docker registry:
 $HOME/google-cloud-sdk/bin/gcloud docker -- push gcr.io/${PROJECT_NAME}/telepresence-local:${TELEPRESENCE_VERSION}
