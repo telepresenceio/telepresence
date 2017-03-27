e1e35a19b346a46706f835b5fca1c18df740285a
Itamar Turner-Trauring <itamar@datawire.io>
2017-03-27 16:17:44 -0400

    Don't change to user uid, since we'll need to restart proxies.

--- local/entrypoint.py
+++ local/entrypoint.py
@@ -7,7 +7,7 @@ abstractions.
 """
 
 from json import loads
-from os import setuid, rename
+from os import chown, rename
 from subprocess import Popen, check_output, check_call, CalledProcessError
 from sys import argv, exit
 import time
@@ -116,15 +116,17 @@ def get_env_variables(remote_info):
     return in_docker_result, socks_result
 
 
-def write_env(remote_info):
+def write_env(remote_info, uid):
     for_docker_env, for_local_env = get_env_variables(remote_info)
     with open("/output/unproxied.env.tmp", "w") as f:
         for key, value in for_local_env.items():
             f.write("{}={}\n".format(key, value))
+    chown("/output/unproxied.env.tmp", uid)
     rename("/output/unproxied.env.tmp", "/output/unproxied.env")
     with open("/output/docker.env.tmp", "w") as f:
         for key, value in for_docker_env.items():
             f.write("{}={}\n".format(key, value))
+    chown("/output/docker.env.tmp", uid)
     rename("/output/docker.env.tmp", "/output/docker.env")
 
 
@@ -310,8 +312,7 @@ def main(
     time.sleep(5)
     #
     # write docker envfile, which tells CLI we're ready:
-    setuid(uid)
-    write_env(remote_info)
+    write_env(remote_info, uid)
     for p in processes:
         p.wait()
 
