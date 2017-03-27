ac06f00b9f6dcad3cd9eb7aaa85c6f50e4bdc202
Itamar Turner-Trauring <itamar@datawire.io>
2017-03-27 16:44:39 -0400

    gid.

--- local/entrypoint.py
+++ local/entrypoint.py
@@ -127,12 +127,12 @@ def write_env(remote_info, uid):
     with open("/output/unproxied.env.tmp", "w") as f:
         for key, value in for_local_env.items():
             f.write("{}={}\n".format(key, value))
-    chown("/output/unproxied.env.tmp", uid)
+    chown("/output/unproxied.env.tmp", uid, uid)
     rename("/output/unproxied.env.tmp", "/output/unproxied.env")
     with open("/output/docker.env.tmp", "w") as f:
         for key, value in for_docker_env.items():
             f.write("{}={}\n".format(key, value))
-    chown("/output/docker.env.tmp", uid)
+    chown("/output/docker.env.tmp", uid, uid)
     rename("/output/docker.env.tmp", "/output/docker.env")
 
 
