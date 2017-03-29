0e1d7091e9b635db9c99e80690aaf710eeccb065
Itamar Turner-Trauring <itamar@datawire.io>
2017-03-29 13:10:59 -0400

    Catch a few more issues automatically, by running pylint.

--- ci/test.sh
+++ ci/test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 virtualenv/bin/flake8 local/*.py remote/*.py cli/telepresence
+virtualenv/bin/pylint -E local/entrypoint.py
 cli/telepresence --version
 cli/telepresence --help
 [ -z "$TELEPRESENCE_TESTS" ] && export TELEPRESENCE_TESTS="tests remote/test_socks.py"
