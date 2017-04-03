320a530daee734d0fde924c7ad7b8907906b0378
Itamar Turner-Trauring <itamar@datawire.io>
2017-04-03 15:32:50 -0400

    Try to run Docker image builds in parallel.

--- ci/push-images.sh
+++ ci/push-images.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 set -e
-# Build Docker images:
-make build
+
+export TELEPRESENCE_VERSION=$(make version)
+
+# Build local image, tag, then push to GCR:
+make build-local && \
+    docker tag datawire/telepresence-local:${TELEPRESENCE_VERSION} \
+           gcr.io/${PROJECT_NAME}/telepresence-local:${TELEPRESENCE_VERSION} && \
+    $HOME/google-cloud-sdk/bin/gcloud docker -- push gcr.io/${PROJECT_NAME}/telepresence-local:${TELEPRESENCE_VERSION} &
+
 
 # Tag Docker images with GCR tags:
-export TELEPRESENCE_VERSION=$(make version)
-docker tag datawire/telepresence-local:${TELEPRESENCE_VERSION} \
-           gcr.io/${PROJECT_NAME}/telepresence-local:${TELEPRESENCE_VERSION}
-docker tag datawire/telepresence-k8s:${TELEPRESENCE_VERSION} \
-       gcr.io/${PROJECT_NAME}/telepresence-k8s:${TELEPRESENCE_VERSION}
+make build-remote && \
+    docker tag datawire/telepresence-k8s:${TELEPRESENCE_VERSION} \
+           gcr.io/${PROJECT_NAME}/telepresence-k8s:${TELEPRESENCE_VERSION} && \
+    $HOME/google-cloud-sdk/bin/gcloud docker -- push gcr.io/${PROJECT_NAME}/telepresence-k8s:${TELEPRESENCE_VERSION} &
 
-# Push to Google Docker registry:
-$HOME/google-cloud-sdk/bin/gcloud docker -- push gcr.io/${PROJECT_NAME}/telepresence-local:${TELEPRESENCE_VERSION}
-$HOME/google-cloud-sdk/bin/gcloud docker -- push gcr.io/${PROJECT_NAME}/telepresence-k8s:${TELEPRESENCE_VERSION}
+# Wait for jobs to finish
+wait
