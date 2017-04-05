9553b020a3a1717cd377b6c0623faaa82361b238
Itamar Turner-Trauring <itamar@datawire.io>
2017-04-05 10:54:58 -0400

    Remove local image from build process.

--- ci/push-images.sh
+++ ci/push-images.sh
@@ -4,18 +4,7 @@ set -e
 export TELEPRESENCE_VERSION
 TELEPRESENCE_VERSION=$(make version)
 
-# Build local image, tag, then push to GCR:
-make build-local && \
-    docker tag datawire/telepresence-local:${TELEPRESENCE_VERSION} \
-           gcr.io/${PROJECT_NAME}/telepresence-local:${TELEPRESENCE_VERSION} && \
-    $HOME/google-cloud-sdk/bin/gcloud docker -- push gcr.io/${PROJECT_NAME}/telepresence-local:${TELEPRESENCE_VERSION} &
-
-
-# Tag Docker images with GCR tags:
-make build-remote && \
-    docker tag datawire/telepresence-k8s:${TELEPRESENCE_VERSION} \
-           gcr.io/${PROJECT_NAME}/telepresence-k8s:${TELEPRESENCE_VERSION} && \
-    $HOME/google-cloud-sdk/bin/gcloud docker -- push gcr.io/${PROJECT_NAME}/telepresence-k8s:${TELEPRESENCE_VERSION} &
-
-# Wait for jobs to finish
-wait
+make build-remote
+docker tag "datawire/telepresence-k8s:${TELEPRESENCE_VERSION}" \
+           "gcr.io/${PROJECT_NAME}/telepresence-k8s:${TELEPRESENCE_VERSION}"
+$HOME/google-cloud-sdk/bin/gcloud docker -- push "gcr.io/${PROJECT_NAME}/telepresence-k8s:${TELEPRESENCE_VERSION}"
