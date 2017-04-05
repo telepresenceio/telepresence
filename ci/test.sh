48b55fb098c91fb7557773fadb48b6f8bd1e6ef1
Itamar Turner-Trauring <itamar@datawire.io>
2017-04-05 16:45:24 -0400

    Don't do pylint on Travis OS X.

--- ci/test.sh
+++ ci/test.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
 virtualenv/bin/flake8 local/*.py remote/*.py cli/telepresence
-virtualenv/bin/pylint -E cli/telepresence
+# pylint doesn't work on Travis OS X, perhaps because it's python 3.6:
+if [ "$(uname)" == "Linux" ]; then virtualenv/bin/pylint -E cli/telepresence; fi
 cli/telepresence --version
 cli/telepresence --help
 [ -z "$TELEPRESENCE_TESTS" ] && export TELEPRESENCE_TESTS="-n 4 tests remote/test_socks.py"
