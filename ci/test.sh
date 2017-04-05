92a9c37020596f1632e5986b40cd20669dcc3a64
Itamar Turner-Trauring <itamar@datawire.io>
2017-04-05 16:18:38 -0400

    Longer timeout.

--- ci/test.sh
+++ ci/test.sh
@@ -6,4 +6,4 @@ cli/telepresence --version
 cli/telepresence --help
 [ -z "$TELEPRESENCE_TESTS" ] && export TELEPRESENCE_TESTS="-n 4 tests remote/test_socks.py"
 env PATH="$PWD/cli/:$PATH" virtualenv/bin/py.test -v \
-    --timeout 240 --timeout-method thread --fulltrace $TELEPRESENCE_TESTS
+    --timeout 360 --timeout-method thread --fulltrace $TELEPRESENCE_TESTS
