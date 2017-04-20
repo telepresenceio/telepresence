e7798e9e121cc0793a14484c6a8781cb41653d11
Itamar Turner-Trauring <itamar@datawire.io>
2017-04-20 11:37:15 -0400

    Initial minimally working package building.

---
+++ packaging/build-package.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# This will be run inside a Docker image for each operating system, which is
+# presumed to have fpm pre-installed.
+#
+# Inputs:
+# $PACKAGE_VERSION is the package version to use.
+# $PACKAGE_TYPE is rpm or deb.
+# Command line arguments are the dependencies.
+set -e
+
+# Set proper ownership before exiting, so the created packages aren't owned by
+# root.
+trap 'chown -R --reference /build-inside/build-package.sh /out/' EXIT
+
+# Package only includes /usr/bin/telepresence:
+cd /out
+fpm -t "$PACKAGE_TYPE" \
+    --name telepresence \
+    --version "$PACKAGE_VERSION" \
+    --description "Local development for a remote Kubernetes cluster." \
+    ${@/#/--depends } \
+    --prefix /usr/bin \
+    --chdir /source/cli \
+    --input-type dir \
+    telepresence
---
+++ packaging/create-linux-packages.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+from subprocess import run
+from pathlib import Path
+
+THIS_DIRECTORY = Path(__file__).absolute().parent
+
+
+def build_package(builder_image, package_type, version, dependencies):
+    """
+    Build a deb or RPM package using a fpm-within-docker Docker image.
+
+    :param package_type str: "rpm" or "deb".
+    :param version str: The package version.
+    :param dependencies list: package names the resulting package should depend
+        on.
+    """
+    run(["sudo", "docker", "run",  "--rm",
+         "-e", "PACKAGE_VERSION=" + version,
+         "-e", "PACKAGE_TYPE=" + package_type,
+         "-v", "{}:/build-inside:ro".format(THIS_DIRECTORY),
+         "-v", "{}:/source:ro".format(THIS_DIRECTORY.parent),
+         "-v",  "{}/out:/out".format(THIS_DIRECTORY),
+         "-w", "/build-inside",
+         builder_image, "/build-inside/build-package.sh", *dependencies],
+        check=True)
+
+
+def main():
+    run(["rm", "-f", str(THIS_DIRECTORY / "out" / "*")], check=True)
+    build_package("alanfranz/fwd-ubuntu-xenial:latest", "deb",
+                  "0.32", ["torsocks", "python3", "openssh-client", "sshfs"])
+
+
+if __name__ == '__main__':
+    main()
