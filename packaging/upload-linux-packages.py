4cc55db66859e4d2776fdef0843d397a376392a4
Itamar Turner-Trauring <itamar@datawire.io>
2017-04-21 09:04:58 -0400

    Switch to packagecloud.io.

--- packaging/upload-linux-packages.py
+++ packaging/upload-linux-packages.py
@@ -14,30 +14,26 @@ class Uploader(object):
     """
     Upload packages to Bintray.
     """
-    def __init__(self, version, api_key):
+
+    def __init__(self, version):
         self.version = version
-        self.api_key = api_key
 
     def upload_ubuntu(self, release):
         """Upload a .deb for a specific release, e.g. 'xenial'."""
         file_path = PACKAGES / release / "telepresence_{}_amd64.deb".format(
-            self.version)
-        self._upload(
-            file_path, "telepresence",
-            ";deb_distribution={}".format(release) +
-            ";deb_component=main;deb_architecture=amd64")
+            self.version
+        )
+        self._upload(file_path, "telepresence", "ubuntu/" + release)
 
     def upload_fedora(self, release):
-        """Upload a .rpm for a specific Fedora release, e.g. 'fedora-25'."""
-        # XXX haven't figured out how to differentiate different Fedora
-        # releases' packages, so need to get back to this in later branch.
-        # Disabled for now.
-        return
-        file_path = PACKAGES / release / "telepresence-{}-1.x86_64.rpm".format(
-            self.version)
-        self._upload(file_path, "telepresence-rpm", "")
+        """Upload a .rpm for a specific Fedora release, e.g. '25'."""
+        file_path = (
+            PACKAGES / ("fedora-" + release) /
+            "telepresence-{}-1.x86_64.rpm".format(self.version)
+        )
+        self._upload(file_path, "telepresence-rpm", "fedora/" + release)
 
-    def _upload(self, file_path, repository, extra):
+    def _upload(self, file_path, repository, distro):
         """Upload a file to a repository.
 
         :param file_path Path: Path to package to upload.
@@ -45,20 +41,21 @@ class Uploader(object):
         :param version str: Version of package.
         :param extra str: Extra options to attach to end of Bintray URL.
         """
-        run(["curl", "-T", str(file_path),
-             "-udatawireio:" + self.api_key,
-             "https://api.bintray.com/content/datawireio/" +
-             "{}/telepresence/{}/{}{}?publish=1".format(
-                 repository, file_path.basename, self.version, extra)],
+        run([
+            "package_cloud",
+            "push",
+            "datawireio/telepresence/" + distro,
+            str(file_path),
+        ],
             check=True)
 
 
-def main(version, api_key):
-    uploader = Uploader(version, api_key)
+def main(version):
+    uploader = Uploader(version)
     for release in ["xenial", "yakkety", "zesty"]:
         uploader.upload_ubuntu(release)
-    # uploader.upload_fedora("fedora-25")
+    uploader.upload_fedora("25")
 
 
 if __name__ == '__main__':
-    main(sys.argv[1], sys.argv[2])
+    main(sys.argv[1])
