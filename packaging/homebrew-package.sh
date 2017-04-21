5f7ed5adf8e28b0bfde04bd519775956c14ed659
Itamar Turner-Trauring <itamar@datawire.io>
2017-04-21 11:17:55 -0400

    Packaging fixes.

--- packaging/homebrew-package.sh
+++ packaging/homebrew-package.sh
@@ -8,7 +8,7 @@ git clone git@github.com:datawire/homebrew-blackbird.git "${BUILD_HOMEBREW_DIR}"
 FORMULA="${BUILD_HOMEBREW_DIR}/Formula/telepresence.rb"
 
 # Update recipe
-cp ci/homebrew-formula.rb "$FORMULA"
+cp packaging/homebrew-formula.rb "$FORMULA"
 sed "s/__NEW_VERSION__/${TELEPRESENCE_VERSION}/g" -i "$FORMULA"
 TARBALL_HASH=$(curl --silent -L "https://github.com/datawire/telepresence/archive/${TELEPRESENCE_VERSION}.tar.gz" | sha256sum | cut -f 1 -d " ")
 sed "s/__TARBALL_HASH__/${TARBALL_HASH}/g" -i "$FORMULA"
