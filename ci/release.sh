851da5ed03a7f644cc126c40e6ec46e4a930a5b5
Itamar Turner-Trauring <itamar@datawire.io>
2017-04-21 13:12:26 -0400

    Login to Docker as part of release.

--- ci/release.sh
+++ ci/release.sh
@@ -2,6 +2,9 @@
 # Run the release!
 set -e
 
+# Login to Docker Hub
+sudo docker login -p "$DOCKER_PASSWORD" -u d6eautomaton
+
 # Store the SSH key used to push to github.com/datawire/homebrew-blackbird; this
 # key is set as environment variable on Travis repo:
 echo $HOMEBREW_KEY > packaging/homebrew.rsa
