d83c60066a16cd3889ae7263a8c3e1d4d8638215
Itamar Turner-Trauring <itamar@datawire.io>
2017-04-24 11:56:26 -0400

    Try to decode key.

--- ci/release.sh
+++ ci/release.sh
@@ -11,7 +11,7 @@ sudo docker login -p "$DOCKER_PASSWORD" -u d6eautomaton
 
 # Store the SSH key used to push to github.com/datawire/homebrew-blackbird; this
 # key is set as environment variable on Travis repo:
-echo "$HOMEBREW_KEY" > packaging/homebrew.rsa
+echo -e "$HOMEBREW_KEY" > packaging/homebrew.rsa
 chmod 600 packaging/homebrew.rsa
 
 # Add ssh keys we need to push to github.com/datawire/homebrew-blackbird:
