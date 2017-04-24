fc4a10df795a51205161a677b4d1b85dc1b8cbc4
Itamar Turner-Trauring <itamar@datawire.io>
2017-04-24 13:05:08 -0400

    Need package_cloud installed.

--- ci/release.sh
+++ ci/release.sh
@@ -18,5 +18,8 @@ chmod 600 packaging/homebrew.rsa
 eval $(ssh-agent)
 ssh-add packaging/homebrew.rsa
 
+# Install package cloud CLI:
+sudo gem install package_cloud
+
 # Run the release:
 make release
