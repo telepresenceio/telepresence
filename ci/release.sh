535aa51bef8493a5e8d28db6d497aced34748108
Itamar Turner-Trauring <itamar@datawire.io>
2017-04-24 11:21:54 -0400

    Try to fix release automation.

--- ci/release.sh
+++ ci/release.sh
@@ -2,12 +2,16 @@
 # Run the release!
 set -e
 
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+    exit 0;
+fi
+
 # Login to Docker Hub
 sudo docker login -p "$DOCKER_PASSWORD" -u d6eautomaton
 
 # Store the SSH key used to push to github.com/datawire/homebrew-blackbird; this
 # key is set as environment variable on Travis repo:
-echo $HOMEBREW_KEY > packaging/homebrew.rsa
+echo "$HOMEBREW_KEY" > packaging/homebrew.rsa
 
 # Add ssh keys we need to push to github.com/datawire/homebrew-blackbird:
 eval $(ssh-agent)
