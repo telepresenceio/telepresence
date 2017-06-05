2d9c691931a4d939569760f04b7a0278182ec1af
Itamar Turner-Trauring <itamar@datawire.io>
2017-06-05 15:33:29 -0400

    Return the correct name to queries.

--- remote/forwarder.py
+++ remote/forwarder.py
@@ -49,11 +49,10 @@ class LocalResolver(object):
         # figure out what it is.
         self.suffix = []
 
-    def _got_ips(self, query, ips, record_type):
+    def _got_ips(self, name, ips, record_type):
         """
         Generate the response to a query, given an IP.
         """
-        name = query.name.name
         print("Result for {} is {}".format(name, ips))
         answers = [
             dns.RRHeader(name=name, payload=record_type(address=ip))
@@ -67,7 +66,11 @@ class LocalResolver(object):
         print(failure)
         return defer.fail(error.DomainError(str(failure)))
 
-    def query(self, query, timeout=None):
+    def query(self, query, timeout=None, real_name=None):
+        # Preserve real name asked in query, in case we need to truncate suffix
+        # during lookup:
+        if real_name is None:
+            real_name = query.name.name
         # We use a special marker hostname, which is always sent by
         # telepresence, to figure out the search suffix set by the client
         # machine's resolv.conf. We then remove it since it masks our
@@ -79,7 +82,7 @@ class LocalResolver(object):
             self.suffix = parts[1:]
             print("Set DNS suffix we filter out to: {}".format(self.suffix))
         if parts == [b"hellotelepresence"] + self.suffix:
-            return self._got_ips(query, [b"127.0.0.1"], dns.Record_A)
+            return self._got_ips(real_name, [b"127.0.0.1"], dns.Record_A)
         if parts[-len(self.suffix):] == self.suffix:
             new_query = deepcopy(query)
             new_query.name.name = b".".join(parts[:-len(self.suffix)])
@@ -87,26 +90,32 @@ class LocalResolver(object):
 
             def failed(f):
                 print(
-                    "Failed to lookup {}, falling back to {}".
-                    format(new_query.name.name, query.name.name)
+                    "Failed to lookup {} due to {}, falling back to {}".
+                    format(new_query.name.name, f, query.name.name)
                 )
                 return self.fallback.query(query, timeout=timeout)
 
-            return self.query(new_query, timeout=timeout).addErrback(failed)
+            return defer.maybeDeferred(
+                self.query,
+                new_query,
+                timeout=(1, 1),
+                real_name=query.name.name,
+            ).addErrback(failed)
 
         # No special suffix:
         if query.type == dns.A:
             print("A query: {}".format(query.name.name))
             d = deferToThread(resolve, query.name.name)
-            d.addCallback(lambda ips: self._got_ips(query, ips, dns.Record_A)
-                          ).addErrback(self._got_error)
+            d.addCallback(
+                lambda ips: self._got_ips(real_name, ips, dns.Record_A)
+            ).addErrback(self._got_error)
             return d
         elif query.type == dns.AAAA:
             # Kubernetes can't do IPv6:
-            return self._got_ips(query, [], dns.Record_AAAA)
+            return self._got_ips(real_name, [], dns.Record_AAAA)
         elif query.type == dns.A6:
             # Kubernetes can't do IPv6:
-            return self._got_ips(query, [], dns.Record_A6)
+            return self._got_ips(real_name, [], dns.Record_A6)
         else:
             print("{} query:".format(query.type, query.name.name))
             return self.fallback.query(query, timeout=timeout)
