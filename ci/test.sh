aeba811b76eb25c74969cd1d4146cce8c9c5b7fc
Itamar Turner-Trauring <itamar@datawire.io>
2017-06-19 12:44:25 -0400

    No parallelism for container method either.

--- ci/test.sh
+++ ci/test.sh
@@ -7,9 +7,10 @@ cli/telepresence --version
 echo | cli/telepresence --help
 if [ -z "$TELEPRESENCE_TESTS" ]; then
     # Don't want parallism for OpenShift (causes problems with OpenShift
-    # Online's limited free plan) and don't want parallism for VPN-y method
-    # since should only have one running a time.
-    [ -z "$TELEPRESENCE_OPENSHIFT" ] && [ "$TELEPRESENCE_METHOD" != "vpn-tcp" ] && export TELEPRESENCE_TESTS="-n 4";
+    # Online's limited free plan) and don't want parallelism for VPN-y method
+    # since should only have one running a time, and parallelism breaks container
+    # method on OS X.
+    [ -z "$TELEPRESENCE_OPENSHIFT" ] && [ "$TELEPRESENCE_METHOD" == "inject-tcp" ] && export TELEPRESENCE_TESTS="-n 4";
 fi
 env PATH="$PWD/cli/:$PATH" virtualenv/bin/py.test -v \
     --timeout 360 --timeout-method thread --fulltrace $TELEPRESENCE_TESTS tests k8s-proxy/test_socks.py
