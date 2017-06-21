d27aaeab3be624b19ad5c4b999de5940e8226621
Itamar Turner-Trauring <itamar@datawire.io>
2017-06-21 10:50:15 -0400

    Build local image too.

--- ci/push-images.sh
+++ ci/push-images.sh
@@ -4,6 +4,7 @@ set -e
 export TELEPRESENCE_VERSION
 TELEPRESENCE_VERSION=$(make version)
 
+make build-local
 make build-k8s-proxy
 docker tag "datawire/telepresence-k8s:${TELEPRESENCE_VERSION}" \
            "gcr.io/${PROJECT_NAME}/telepresence-k8s:${TELEPRESENCE_VERSION}"
