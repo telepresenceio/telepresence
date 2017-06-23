bb3f5b74a4c63b936f473fab8536506b072be1ff
Itamar Turner-Trauring <itamar@datawire.io>
2017-06-23 11:54:40 -0400

    Complete switch to dropbear.

--- k8s-proxy/run.sh
+++ k8s-proxy/run.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env sh
-
+set -e
 USER_ID="$(id -u)"
 GROUP_ID="$(id -g)"
 
 # This is a terrible hack to allow SSH login to a runtime-specified UID
 echo "telepresence::${USER_ID}:${GROUP_ID}:Telepresence User:/usr/src/app:/bin/ash" >> /etc/passwd
 
-#/usr/sbin/sshd -E /usr/src/app/sshd.log
-dropbear -R -B -E -a -p 8022
+dropbear -R -B -E -a -p 8022 -K 1 -I 10
 exec env PYTHONPATH=/usr/src/app twistd -n -y ./forwarder.py
