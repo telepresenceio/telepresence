941ade61eaed7e692e6003d6d665cba2667d991f
Itamar Turner-Trauring <itamar@datawire.io>
2017-06-26 09:56:31 -0400

    Possibly causes timeouts when it shouldn't.

--- k8s-proxy/run.sh
+++ k8s-proxy/run.sh
@@ -6,5 +6,5 @@ GROUP_ID="$(id -g)"
 # This is a terrible hack to allow SSH login to a runtime-specified UID
 echo "telepresence::${USER_ID}:${GROUP_ID}:Telepresence User:/usr/src/app:/bin/ash" >> /etc/passwd
 
-dropbear -R -B -E -a -p 8022 -K 1 -I 10
+dropbear -R -B -E -a -p 8022 -K 1
 exec env PYTHONPATH=/usr/src/app twistd -n -y ./forwarder.py
