e603870230a4ae66ddd18bfca363030f23d074d8
Itamar Turner-Trauring <itamar@datawire.io>
2017-06-29 10:43:43 -0400

    Switch back to openssh in hopes unreliability was caused by dropbear.

--- k8s-proxy/run.sh
+++ k8s-proxy/run.sh
@@ -6,5 +6,9 @@ GROUP_ID="$(id -g)"
 # This is a terrible hack to allow SSH login to a runtime-specified UID
 echo "telepresence::${USER_ID}:${GROUP_ID}:Telepresence User:/usr/src/app:/bin/ash" >> /etc/passwd
 
-dropbear -R -B -E -a -p 8022 -K 1
+if [ "$USER_ID" = "0" ]; then
+    chmod -R 0600 /etc/ssh/*
+fi
+
+/usr/sbin/sshd -e
 exec env PYTHONPATH=/usr/src/app twistd -n -y ./forwarder.py
