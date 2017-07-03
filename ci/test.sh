dbe54415d911cbc3a7485a08d1f9cc8f120293d4
Itamar Turner-Trauring <itamar@datawire.io>
2017-07-03 12:16:59 -0400

    Restore parallel tests.

--- ci/test.sh
+++ ci/test.sh
@@ -5,5 +5,12 @@ virtualenv/bin/flake8 local-docker/*.py k8s-proxy/*.py cli/telepresence
 if [ "$(uname)" == "Linux" ]; then virtualenv/bin/pylint -f parseable -E cli/telepresence; fi
 cli/telepresence --version
 echo | cli/telepresence --help
+if [ -z "$TELEPRESENCE_TESTS" ]; then
+    # Don't want parallism for OpenShift (causes problems with OpenShift
+    # Online's limited free plan) and don't want parallelism for VPN-y method
+    # since should only have one running a time, and parallelism breaks container
+    # method on OS X.
+    [ -z "$TELEPRESENCE_OPENSHIFT" ] && [ "$TELEPRESENCE_METHOD" == "inject-tcp" ] && export TELEPRESENCE_TESTS="-n 4";
+fi
 env PATH="$PWD/cli/:$PATH" virtualenv/bin/py.test -v \
     --timeout 360 --timeout-method thread --fulltrace $TELEPRESENCE_TESTS tests k8s-proxy/test_socks.py
