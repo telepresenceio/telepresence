2465ca46e8ca3d7f4cc336b5ff719bc8e9a2d6a3
Abhay Saxena <ark3@datawire.io>
2017-08-29 17:54:08 -0400

    Remove direct socat dependency due to conflict

--- packaging/homebrew-formula.rb
+++ packaging/homebrew-formula.rb
@@ -1,20 +1,25 @@
 # This script is generated automatically by the release automation code in the
 # Telepresence repository:
 class Telepresence < Formula
-  desc "Local development environment attached to a remote Kubernetes cluster"
-  homepage "http://telepresence.io"
+  desc "Local dev environment attached to a remote Kubernetes cluster"
+  homepage "https://telepresence.io"
   url "https://github.com/datawire/telepresence/archive/__NEW_VERSION__.tar.gz"
   sha256 "__TARBALL_HASH__"
 
   depends_on "python3"
-  depends_on "torsocks"
-  depends_on "sshfs"
-  depends_on "socat"
+  depends_on "torsocks" => :run
+  depends_on "sshfs" => :run
 
   def install
-    system "env", "PATH=#{ENV['PATH']}:/usr/local/bin", "make", "virtualenv/bin/sshuttle-telepresence"
+    system "env", "PATH=#{ENV["PATH"]}:/usr/local/bin", "make", "virtualenv/bin/sshuttle-telepresence"
     bin.install "cli/telepresence"
     bin.install "virtualenv/bin/sshuttle-telepresence"
+  end
+
+  def caveats; <<-EOS.undent
+    Use of the container method requires socat.
+      brew install socat
+    EOS
   end
 
   test do
