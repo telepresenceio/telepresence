d60f51b21c71cbfa8f49d4a98ee202808f912c8e
Richard Li <richard@datawire.io>
2017-09-08 15:09:15 -0400

    add --isolated

--- ci/test.sh
+++ ci/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ex
-virtualenv/bin/flake8 local-docker/*.py k8s-proxy/*.py cli/telepresence
+virtualenv/bin/flake8 --isolated local-docker/*.py k8s-proxy/*.py cli/telepresence
 # pylint doesn't work on Travis OS X, perhaps because it's python 3.6:
 if [ "$(uname)" == "Linux" ]; then virtualenv/bin/pylint -f parseable -E cli/telepresence; fi
 # MYPYPATH is stupid hack to get a telepresence.py:
