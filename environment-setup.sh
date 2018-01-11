05b6ba43f2e1bdea2b63b104729f04c6f4671622
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2018-01-11 11:34:46 -0500

    Try installing google-cloud-sdk with apt if we can
    The wget/shell script installer is pretty unreliable any it's nicer to
    have packaged software if we can, anyway.
    
    right?

--- environment-setup.sh
+++ environment-setup.sh
@@ -24,7 +24,16 @@ case "$(uname -s)" in
 
     Linux)
         sudo apt install sshfs conntrack python3
-        ;;
+
+        # Create an environment variable for the correct distribution
+        export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
+        # Add the Cloud SDK distribution URI as a package source
+        echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+        # Import the Google Cloud Platform public key
+        curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+        # Update the package list and install the Cloud SDK
+        sudo apt-get update && sudo apt-get install google-cloud-sdk
+	;;
 
     *)
         echo "Unknown platform."
