f400f53c8ae03de318f2230d86f5f70f564a351c
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2018-01-11 12:16:06 -0500

    get pip and virtualenv another much more awesomer way

--- environment-setup.sh
+++ environment-setup.sh
@@ -27,6 +27,8 @@ case "$(uname -s)" in
              sshfs conntrack \
              lsb-release
 
+        sudo pip install --upgrade pip virtualenv
+
         # Create an environment variable for the correct distribution
         export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
         # Add the Cloud SDK distribution URI as a package source
