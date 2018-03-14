269fb5e57d888d8f625793e6ff41448d5365df93
Abhay Saxena <ark3@datawire.io>
2018-03-14 17:15:09 -0400

    Add script to perform pre-release tasks (WIP)

---
+++ packaging/prepare-release.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+"""
+Perform the steps required to build and deploy, but not release, a new version
+of Telepresence.
+
+Prep for package/release
+- changelog
+- version and tag:
+  ./virtualenv/bin/bumpversion --verbose --list minor
+- docker build/push:
+  ./build --registry datawire --build-and-push --version-suffix="" --no-tests
+- run tests
+- Display change log diff
+
+Package
+- Make packages for Linux:
+  ./packaging/create-linux-packages.py <new version number>
+- Build Scout blob (example in #437)
+- Build Gitter announcement
+
+Release
+- Upload linux packages to package cloud:
+  rvm install 2.1 (from ci/release.sh)
+  gem install package_cloud (from ci/release.sh)
+  sh ./dist/upload_linux_packages.sh
+- Update Homebrew package:
+  (GitHub key stuff from ci/release.sh)
+  ./packaging/homebrew-package.sh
+- test linux packages remotely
+- Push scout blobs
+- Post on Gitter
+"""
+
+import json
+import subprocess
+
+from pathlib import Path
+from shutil import rmtree
+
+THIS_DIRECTORY = Path(__file__).absolute().resolve().parent
+DIST = THIS_DIRECTORY.parent / "dist"
+VENV_BIN = THIS_DIRECTORY.parent / "virtualenv" / "bin"
+
+
+def get_versions():
+    """Retrieve the current and new version numbers"""
+    bumpversion = str(VENV_BIN / "bumpversion")
+    command = [bumpversion] + "--list --dry-run --allow-dirty minor".split()
+    data = subprocess.check_output(command)
+    lines = data.decode("utf-8").splitlines()
+    current = new = None
+    for line in lines:
+        if line.startswith("current_version="):
+            current = line.split("=", 1)[1]
+        elif line.startswith("new_version="):
+            new = line.split("=", 1)[1]
+    assert current, lines
+    assert new, lines
+    return current, new
+
+
+_S3_UPLOADER = """#!/bin/bash
+set -e
+cd "$(dirname "$0")"
+export AWS_DEFAULT_REGION=us-east-1
+aws s3api put-object \\
+    --bucket datawire-static-files \\
+    --key telepresence/stable.txt \\
+    --body {release_version_path.name}
+aws s3api put-object \\
+    --bucket scout-datawire-io \\
+    --key telepresence/app.json \\
+    --body {scout_blob_path.name}
+"""
+
+
+def emit_release_info(version, notices=None):
+    """Generate files in dist that handle scout and release info"""
+    release_version_path = DIST / "release_version.txt"
+    release_version_path.write_text(version)
+
+    scout_info = {
+        "application": "telepresence",
+        "latest_version": version,
+        "notices": notices or []
+    }
+    scout_blob_path = DIST / "scout_blob.json"
+    scout_blob_path.write_text(json.dumps(scout_info))
+
+    s3_uploader_path = DIST / "s3_uploader.sh"
+    s3_uploader_path.write_text(_S3_UPLOADER.format(**locals()))
+    s3_uploader_path.chmod(0o775)
+
+
+def main():
+    """
+    Perform the steps required to build and deploy, but not release, a new
+    version of Telepresence.
+    """
+    if DIST.exists():
+        rmtree(str(DIST))
+    DIST.mkdir(parents=True)
+    current, new = get_versions()
+    emit_release_info(new)
+
+
+if __name__ == "__main__":
+    main()
