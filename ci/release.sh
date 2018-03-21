a4a69d1b79243295c8807277a34eeb3688c9449c
Abhay Saxena <ark3@datawire.io>
2018-03-21 17:46:40 -0400

    Rewrite release script (WIP)

--- ci/release.sh
+++ ci/release.sh
@@ -1,28 +1,55 @@
 #!/bin/bash
 # Run the release!
-set -e
+set -eu
 
-if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-    exit 0;
+# Inputs
+echo Releasing Telepresence $VERSION
+echo "$HOMEBREW_KEY $DOCKER_PASSWORD $PACKAGECLOUD_TOKEN" > /dev/null
+if [ ! -x dist/upload_linux_packages.sh ]; then
+    echo "Built distribution not found."
+    exit 1
 fi
 
-# Newer Ruby needed for Package Cloud
-rvm install 2.1
+# Preparation
+# -----------
+
+# ssh stuff to allow a push to github.com/datawire/homebrew-blackbird
+mkdir -p ~/.ssh
+ssh-keyscan -H github.com > ~/.ssh/known_hosts
+echo -e "$HOMEBREW_KEY" > ~/.ssh/homebrew.rsa
+chmod 600 ~/.ssh/homebrew.rsa
+eval $(ssh-agent)
+ssh-add ~/.ssh/homebrew.rsa
+
+# Git wants this stuff set
+git config --global user.email "services@datawire.io"
+git config --global user.name "d6e automaton"
+
+# Install/test package cloud CLI (uses PACKAGECLOUD_TOKEN)
+gem install package_cloud
+package_cloud repository list | fgrep public
 
 # Login to Docker Hub
 docker login -p "$DOCKER_PASSWORD" -u d6eautomaton
+gcloud docker --authorize-only
 
-# Store the SSH key used to push to github.com/datawire/homebrew-blackbird; this
-# key is set as environment variable on Travis repo:
-echo -e "$HOMEBREW_KEY" > packaging/homebrew.rsa
-chmod 600 packaging/homebrew.rsa
+# Release
+# -------
 
-# Add ssh keys we need to push to github.com/datawire/homebrew-blackbird:
-eval $(ssh-agent)
-ssh-add packaging/homebrew.rsa
+# Docker Images
+docker pull gcr.io/datawireio/telepresence-k8s:$VERSION
+docker pull gcr.io/datawireio/telepresence-local:$VERSION
+docker push datawireio/telepresence-k8s:$VERSION
+docker push datawireio/telepresence-local:$VERSION
 
-# Install package cloud CLI
-gem install package_cloud
+# Linux Packages
+dist/upload_linux_packages.sh
 
-# Run the release:
-make release
+# Homebrew
+env TELEPRESENCE_VERSION=$VERSION packaging/homebrew-package.sh
+
+# Scout blobs
+dist/s3_uploader.sh
+
+# Ask user to post on Gitter
+cat dist/announcement.md
