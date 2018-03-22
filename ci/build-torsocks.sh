324b66bb61e7d360dda3fb98df827efefb49387a
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2018-03-22 08:01:37 -0400

    paper over trusty/artful schism

--- ci/build-torsocks.sh
+++ ci/build-torsocks.sh
@@ -8,7 +8,7 @@ if [[ "$OS" == "osx" ]]; then
 fi
 
 if [[ "$OS" == "linux" ]]; then
-    sudo apt-get install -y coreutils
+    sudo apt-get install -y realpath coreutils
     PATCH="$(realpath $(dirname $0))/torsocks-h_addrtype.patch"
     cd /tmp
     wget https://github.com/dgoulet/torsocks/archive/v2.1.0.tar.gz
