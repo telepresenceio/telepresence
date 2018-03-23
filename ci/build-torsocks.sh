28fcf317fa64ccd7415ee5c8e218b22d2886db88
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2018-03-23 12:13:42 -0400

    try using Linux OS package manager to get torsocks

--- ci/build-torsocks.sh
+++ ci/build-torsocks.sh
@@ -8,12 +8,6 @@ if [[ "$OS" == "osx" ]]; then
 fi
 
 if [[ "$OS" == "linux" ]]; then
-    cd /tmp
-    wget https://github.com/dgoulet/torsocks/archive/v2.1.0.tar.gz
-    tar xvfz v2.1.0.tar.gz
-    cd torsocks-2.1.0
-    ./autogen.sh
-    ./configure --prefix=/usr
-    make
-    sudo make install;
+    type -p dnf && sudo dnf install torsocks \
+    || type -p apt-get && sudo apt-get install -y
 fi
