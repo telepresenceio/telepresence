87f56d24f55fffe986e1738046eb932d9fd08aab
Abhay Saxena <ark3@datawire.io>
2018-03-28 17:18:54 -0400

    Add some docstrings to Cache

--- telepresence/cache.py
+++ telepresence/cache.py
@@ -4,8 +4,26 @@ from time import time
 
 
 class Cache(object):
+    """
+    Cache JSON-compatible values with string key lookup.
+
+    Usage:
+
+    >>> cache = Cache.load("cache.json")
+    >>> cache.invalidate(12 * 60 * 60)  # 12 hours
+    >>> sound_cache = cache.child("sound")
+    >>> sound_cache.lookup("moo", lambda: "Sound heard near cows")
+    'Sound heard near cows'
+    >>> sound_cache["moo"]
+    'Sound heard near cows'
+    >>> number_cache = cache.child("numbers")
+    >>> number_cache["one"] = 1
+    >>> number_cache["pi"] = 22.0/7.0
+    """
+
     @classmethod
     def load(cls, filename):
+        """Return a cache loaded from a file"""
         try:
             with open(filename, "r") as f:
                 cache = json.load(f)
@@ -15,6 +33,7 @@ class Cache(object):
         result = Cache(cache)
 
         def save():
+            """Overwrite the original file with current cache contents"""
             with open(filename, "w") as f:
                 json.dump(result.values, f)
 
@@ -34,6 +53,10 @@ class Cache(object):
         self.values[key] = value
 
     def child(self, key):
+        """
+        Retrieve a child cache that operates over a separate keyspace but is
+        loaded and saved with the parent cache.
+        """
         if key in self.values:
             child = self.values[key]
         else:
@@ -42,6 +65,11 @@ class Cache(object):
         return Cache(child)
 
     def invalidate(self, ttl):
+        """
+        Clear the cache if it is too old.
+
+        :param ttl: Time in seconds that the cache is considered valid.
+        """
         now = time()
         created = self.lookup("created", lambda: 0)
         if (now - created) > ttl:
@@ -49,6 +77,15 @@ class Cache(object):
             self["created"] = now
 
     def lookup(self, key, function):
+        """
+        Retrieve the value for the associated key. If the value is not already
+        cached, call function with no arguments to compute the value and cache
+        it for future reference.
+
+        :param key: Identifier for cached value.
+        :param function: Thunk used to compute the value if it is not cached.
+        :return: The requested value.
+        """
         if key in self.values:
             return self.values[key]
         else:
@@ -57,4 +94,5 @@ class Cache(object):
             return value
 
     def clear(self):
+        """Clear the cache"""
         self.values.clear()
