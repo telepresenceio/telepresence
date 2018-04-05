89b1331c7515090d0e0f8d4aeb205c0a939ba6d7
Abhay Saxena <ark3@datawire.io>
2018-04-05 15:48:17 -0400

    Avoid spurious crash during shutdown

--- telepresence/runner.py
+++ telepresence/runner.py
@@ -137,6 +137,8 @@ class Runner(object):
 
     def write(self, message: str, prefix="TEL") -> None:
         """Write a message to the log."""
+        if self.logfile.closed:
+            return
         for sub_message in message.splitlines():
             line = "{:6.1f} {} | {}\n".format(
                 time() - self.start_time, prefix, sub_message.rstrip()
