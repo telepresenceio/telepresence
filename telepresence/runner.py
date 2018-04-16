4964614ebca72259e1178f4e71531552481eed83
Abhay Saxena <ark3@datawire.io>
2018-04-16 13:38:03 -0400

    Narrow required cluster permissions

--- telepresence/runner.py
+++ telepresence/runner.py
@@ -1,3 +1,17 @@
+# Copyright 2018 Datawire. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from collections import deque
 import shlex
 import sys
@@ -195,6 +209,7 @@ class Runner(object):
         except OSError as exc:
             self.write("[{}] {}".format(track, exc))
             raise
+        #self.write("CMD: {}".format(str_command(args)))
         return process
 
     def run_command(self, track, msg1, msg2, out_cb, err_cb, args, **kwargs):
