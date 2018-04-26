7a23f9b3eea795b51e5ed71a9846d8bdef581154
Abhay Saxena <ark3@datawire.io>
2018-04-26 13:18:39 -0400

    Fix unit test. Oops.

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -1,3 +1,16 @@
+# Copyright 2018 Datawire. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 """
 Unit tests (in-memory, small units of code).
 """
@@ -175,7 +188,6 @@ def test_swap_deployment_changes():
         "nginxhttps",
         "random_id_123",
         "datawire/telepresence-k8s:0.777",
-        False,
         False,
     ) == (expected, original["spec"]["template"]["spec"]["containers"][1])
 
