3c24a9674018bd6ad26d67baf3912b66d05e0d75
Abhay Saxena <ark3@datawire.io>
2018-05-11 16:14:50 -0400

    Bump version: 0.86 → 0.87

--- telepresence/__init__.py
+++ telepresence/__init__.py
@@ -19,7 +19,7 @@ import os
 
 # Don't modify next line without modifying corresponding line in
 # .bumpversion.cfg:
-__version__ = "0.86"
+__version__ = "0.87"
 # Test runs can override version so we use specific custom Docker images:
 if os.environ.get("TELEPRESENCE_VERSION") is not None:
     __version__ = os.environ["TELEPRESENCE_VERSION"]
