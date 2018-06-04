dacce7b9db160e2dd45a4c6a2704dddeef9e9407
Abhay Saxena <ark3@datawire.io>
2018-06-04 13:00:14 -0400

    Handle special cases for the version number for images

--- telepresence/__init__.py
+++ telepresence/__init__.py
@@ -22,17 +22,25 @@ from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
 
+# Use the most recent released image version. Override below:
+try:
+    image_version = __version__[:__version__.index("+")]
+except ValueError:
+    image_version = __version__
+
 # Test runs can override version so we use specific custom Docker images:
+version_override = False
 if os.environ.get("TELEPRESENCE_VERSION") is not None:
-    __version__ = os.environ["TELEPRESENCE_VERSION"]
+    image_version = os.environ["TELEPRESENCE_VERSION"]
+    version_override = True
 
 REGISTRY = os.environ.get("TELEPRESENCE_REGISTRY", "datawire")
 TELEPRESENCE_LOCAL_IMAGE = "{}/telepresence-local:{}".format(
-    REGISTRY, __version__
+    REGISTRY, image_version
 )
 TELEPRESENCE_REMOTE_IMAGE = "{}/telepresence-k8s:{}".format(
-    REGISTRY, __version__
+    REGISTRY, image_version
 )
 TELEPRESENCE_REMOTE_IMAGE_PRIV = "{}/telepresence-k8s-priv:{}".format(
-    REGISTRY, __version__
+    REGISTRY, image_version
 )
