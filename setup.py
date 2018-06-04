7599475bb8323a42a1694ef75d171756bb5061e2
Abhay Saxena <ark3@datawire.io>
2018-06-04 12:11:26 -0400

    Clean up setup.py

--- setup.py
+++ setup.py
@@ -2,22 +2,9 @@
 Telepresence: local development environment for a remote Kubernetes cluster.
 """
 
-import os
-import re
 from setuptools import setup
 
 import versioneer
-
-
-def get_version(filename="telepresence/__init__.py"):
-    """Parse out version info"""
-    base_dir = os.path.dirname(os.path.abspath(__file__))
-    with open(os.path.join(base_dir, filename)) as initfile:
-        for line in initfile.readlines():
-            match = re.match("__version__ *= *['\"](.*)['\"]", line)
-            if match:
-                return match.group(1)
-
 
 setup(
     name="telepresence",
