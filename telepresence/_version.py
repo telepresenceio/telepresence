347928fa4258a3c35d60b649e597113ffdbfcba3
Abhay Saxena <ark3@datawire.io>
2018-06-04 16:57:19 -0400

    Switch to git describe style version numbers

--- telepresence/_version.py
+++ telepresence/_version.py
@@ -58,7 +58,7 @@ def get_config():
     # _version.py
     cfg = VersioneerConfig()
     cfg.VCS = "git"
-    cfg.style = "pep440"
+    cfg.style = "git-describe"
     cfg.tag_prefix = ""
     cfg.parentdir_prefix = "telepresence-"
     cfg.versionfile_source = "telepresence/_version.py"
