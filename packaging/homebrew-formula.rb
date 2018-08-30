985d82127914804a1123b0d14b3b6cd860f5cfbc
Abhay Saxena <ark3@datawire.io>
2018-08-30 15:55:22 -0400

    Update Homebrew formula and packaging to use new tarball

--- packaging/homebrew-formula.rb
+++ packaging/homebrew-formula.rb
@@ -4,7 +4,7 @@ class Telepresence < Formula
   include Language::Python::Virtualenv
   desc "Local dev environment attached to a remote Kubernetes cluster"
   homepage "https://telepresence.io"
-  url "https://github.com/telepresenceio/telepresence/archive/__NEW_VERSION__.tar.gz"
+  url "https://s3.amazonaws.com/datawire-static-files/telepresence/telepresence-__NEW_VERSION__.tar.gz"
   sha256 "__TARBALL_HASH__"
 
   depends_on "python"
@@ -12,10 +12,8 @@ class Telepresence < Formula
   depends_on "sshfs"
 
   def install
-    venv = virtualenv_create(libexec, "python3")
-    venv.pip_install "git+https://github.com/datawire/sshuttle.git@telepresence"
-    bin.install libexec/"bin/sshuttle-telepresence"
-    venv.pip_install_and_link buildpath
+    bin.install "telepresence"
+    bin.install "sshuttle-telepresence"
   end
 
   def caveats
