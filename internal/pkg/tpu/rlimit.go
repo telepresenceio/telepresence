d01a5cc6c747d9ade301890e9d9e1cbc44d9c8cc
Rafael Schloming <rhs@alum.mit.edu>
2018-10-29 16:32:03 -0400

    Add rlimit code to teleproxy utils.

---
+++ internal/pkg/tpu/rlimit.go
@@ -0,0 +1,30 @@
+package tpu
+
+import (
+	"log"
+	"syscall"
+)
+
+func Rlimit() {
+	var rLimit syscall.Rlimit
+	err := syscall.Getrlimit(syscall.RLIMIT_NOFILE, &rLimit)
+	if err != nil {
+		log.Println("Error getting rlimit:", err)
+	} else {
+		log.Println("Initial rlimit:", rLimit)
+	}
+
+	rLimit.Max = 999999
+	rLimit.Cur = 999999
+	err = syscall.Setrlimit(syscall.RLIMIT_NOFILE, &rLimit)
+	if err != nil {
+		log.Println("Error setting rlimit:", err)
+	}
+
+	err = syscall.Getrlimit(syscall.RLIMIT_NOFILE, &rLimit)
+	if err != nil {
+		log.Println("Error getting rlimit:", err)
+	} else {
+		log.Println("Final rlimit", rLimit)
+	}
+}
