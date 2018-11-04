b2e2b0847d1941c60c49c9a34bf07c658abbc24f
Rafael Schloming <rhs@alum.mit.edu>
2018-11-04 13:48:51 -0500

    kubernaut -> gubernaut

--- kubernaut.mk
+++ kubernaut.mk
@@ -30,18 +30,6 @@
 #     clobber: kubernaut.clobber
 #
 
-KUBERNAUT_BASE=.
-KUBERNAUT_VERSION=2018.10.24-d46c1f1
-KUBERNAUT=$(KUBERNAUT_BASE)/kubernaut
-
-GOOS=$(shell go env GOOS)
-GOARCH=$(shell go env GOARCH)
-
-$(KUBERNAUT):
-	mkdir -p $(shell dirname $(KUBERNAUT))
-	curl -o $(KUBERNAUT) http://releases.datawire.io/kubernaut/$(KUBERNAUT_VERSION)/$(GOOS)/$(GOARCH)/kubernaut
-	chmod +x $(KUBERNAUT)
-
 .PRECIOUS: %.knaut.claim
 .SECONDARY: %.knaut.claim
 
@@ -52,24 +40,124 @@ $(KUBERNAUT):
 		echo $(@:%.knaut.claim=%)-$${USER}-$(shell uuidgen) > $@; \
 	fi
 
+KUBERNAUT=gubernaut
 KUBERNAUT_CLAIM_FILE=$(@:%.knaut=%.knaut.claim)
 KUBERNAUT_CLAIM_NAME=$(shell cat $(KUBERNAUT_CLAIM_FILE))
-KUBERNAUT_CLAIM=$(KUBERNAUT) claims create --name $(KUBERNAUT_CLAIM_NAME) --cluster-group main
-KUBERNAUT_DISCARD=$(KUBERNAUT) claims delete $(KUBERNAUT_CLAIM_NAME)
 
 %.knaut : %.knaut.claim $(KUBERNAUT)
-	$(KUBERNAUT_DISCARD)
-	$(KUBERNAUT_CLAIM)
-	cp ~/.kube/$(KUBERNAUT_CLAIM_NAME).yaml $@
+	$(KUBERNAUT) -release $(KUBERNAUT_CLAIM_NAME)
+	$(KUBERNAUT) -claim $(KUBERNAUT_CLAIM_NAME) -output $@
 
 .PHONY: %.knaut.clean
 
-%.knaut.clean :
-	if [ -e $(@:%.clean=%.claim) ]; then $(KUBERNAUT) claims delete $$(cat $(@:%.clean=%.claim)); fi
+%.knaut.clean : $(KUBERNAUT)
+	if [ -e $(@:%.clean=%.claim) ]; then $(KUBERNAUT) -release $$(cat $(@:%.clean=%.claim)); fi
 	rm -f $(@:%.knaut.clean=%.knaut)
 	rm -f $(@:%.clean=%.claim)
 
 .PHONY: kubernaut.clobber
 
 kubernaut.clobber:
-	rm -f $(KUBERNAUT)
+	rm -f gubernaut gubernaut.go
+
+gubernaut.go:
+	echo "$${GUBERNAUT}" > gubernaut.go
+
+gubernaut: gubernaut.go
+	go build gubernaut.go
+
+export GUBERNAUT
+define GUBERNAUT
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func claims(method, name, body string) Response {
+	req, err := http.NewRequest(method, "https://next.kubernaut.io/claims" + name, strings.NewReader(body))
+	if err != nil { log.Fatal(err) }
+	req.Header.Add("Authorization", "Bearer " + *token)
+	req.Header.Add("Content-Type", "application/json")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil { log.Fatal(err) }
+	if resp.StatusCode / 100 != 2 {
+		log.Fatal(resp)
+	}
+	respbody, err := ioutil.ReadAll(resp.Body)
+	if err != nil { log.Fatal(err) }
+	var response Response
+	if len(respbody) > 0 {
+		err = json.Unmarshal(respbody, &response)
+		if err != nil { log.Fatal(err) }
+		response.Raw = string(respbody)
+	}
+
+	return response
+}
+
+type Response struct {
+	Claim struct {
+		Name string
+		Kubeconfig string
+	}
+	Claims []struct {
+		Name string
+		ClusterId string
+	}
+	Raw string
+}
+
+var token = flag.String("token", os.Getenv("KUBERNAUT_TOKEN"), "kubernaut API token")
+var claim = flag.String("claim", "", "claim name")
+var output = flag.String("output", "", "path to write kubeconfig file")
+var release = flag.String("release", "", "claim name")
+var list = flag.Bool("list", false, "list claims")
+
+func main() {
+	flag.Parse()
+
+	if *token == "" {
+		log.Fatal("a valid token is required, please put one in the KUBERNAUT_TOKEN env variable")
+	}
+
+	if *claim != "" {
+		if *output == "" {
+			log.Fatal("please specify an output path for your kubeconfig")
+		}
+
+		response := claims("POST", "", fmt.Sprintf(`
+{
+    "name": "%s",
+    "group": "main"
+}
+`, *claim))
+
+		err := ioutil.WriteFile(*output, []byte(response.Claim.Kubeconfig), 0644)
+		if err != nil { log.Fatal(err) }
+	}
+
+	if *release != "" {
+		claims("DELETE", "/" + *release,"")
+	}
+
+	if *list {
+		response := claims("GET", "", "")
+		for _, claim := range response.Claims {
+			if claim.ClusterId != "" {
+				fmt.Printf("%s: %s\n", claim.Name, claim.ClusterId)
+			} else {
+				fmt.Printf("%s\n", claim.Name)
+			}
+		}
+	}
+}
+endef
