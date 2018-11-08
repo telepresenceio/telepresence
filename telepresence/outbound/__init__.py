373834942ab4104d44e0571f0c8c5090782c1c68
Abhay Saxena <ark3@datawire.io>
2018-11-08 12:48:02 -0500

    Move all setup stuff into a separate file

--- telepresence/outbound/__init__.py
+++ telepresence/outbound/__init__.py
@@ -11,19 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from telepresence.outbound.container import setup_container
-from telepresence.outbound.local import setup_inject, setup_vpn
-from telepresence.runner import Runner
 
+from telepresence.outbound.setup import setup
 
-def setup(runner: Runner, args):
-    if args.method == "inject-tcp":
-        return setup_inject(runner, args)
-
-    if args.method == "vpn-tcp":
-        return setup_vpn(runner, args)
-
-    if args.method == "container":
-        return setup_container(runner, args)
-
-    assert False, args.method
+__all__ = "setup",
