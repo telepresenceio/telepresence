9111fca68ac97cc46106abc9ab4bd0d8c049c20f
Rafael Schloming <rhs@alum.mit.edu>
2018-12-04 02:31:16 -0500

    add template support

--- cmd/kubeapply/kubeapply.go
+++ cmd/kubeapply/kubeapply.go
@@ -1,22 +1,50 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"sort"
 	"strings"
+	"text/template"
 	"time"
 	"unicode"
 
 	"github.com/datawire/teleproxy/internal/pkg/tpu"
-
 	"github.com/datawire/teleproxy/pkg/k8s"
+
+	"github.com/Masterminds/sprig"
 )
 
+func envBool(name string) bool {
+	val := os.Getenv(name)
+	switch strings.TrimSpace(strings.ToLower(val)) {
+	case "true":
+		return true
+	case "yes":
+		return true
+	case "1":
+		return true
+	case "false":
+		return false
+	case "no":
+		return false
+	case "0":
+		return false
+	case "":
+		return false
+	}
+
+	return true
+}
+
+var debug = flag.Bool("debug", envBool("KUBEAPPLY_DEBUG"), "enable debug mode, expanded files will be preserved")
 var timeout = flag.Int("t", 60, "timeout in seconds")
 var files tpu.ArrayFlags
 
@@ -34,7 +62,7 @@ func main() {
 	}
 
 	for _, names := range p.phases() {
-		phase(names)
+		phase(names, nil)
 	}
 }
 
@@ -103,33 +131,105 @@ func (p *Phaser) phases() (result [][]string) {
 	return
 }
 
-func phase(names []string) {
+func phase(names []string, data interface{}) {
+	expanded, err := expand(names, data)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	waiter := k8s.NewWaiter(nil)
+
+	valid := make(map[string]bool)
+	abort := false
+	for _, n := range expanded {
+		err := waiter.Scan(n)
+		if err != nil {
+			fmt.Printf("%s: %v\n", n, err)
+			valid[n] = false
+			abort = true
+		} else {
+			valid[n] = true
+		}
+	}
+
+	if !abort {
+		apply(expanded)
+	}
+
+	if !*debug {
+		for _, n := range expanded {
+			if valid[n] {
+				err := os.Remove(n)
+				if err != nil {
+					log.Print(err)
+				}
+			}
+		}
+	}
+
+	if abort {
+		return
+	}
+
+	if !waiter.Wait(time.Duration(*timeout) * time.Second) {
+		panic("not ready")
+	}
+}
+
+func expand(names []string, data interface{}) ([]string, error) {
+	fmt.Printf("expanding %s\n", strings.Join(names, " "))
+	var result []string
+	for _, n := range names {
+		tmpl := template.New("base").Funcs(sprig.TxtFuncMap())
+		_, err := tmpl.ParseFiles(n)
+		if err != nil {
+			return nil, fmt.Errorf("%s: %v", n, err)
+		}
+		buf := bytes.NewBuffer(nil)
+		err = tmpl.ExecuteTemplate(buf, filepath.Base(n), data)
+		if err != nil {
+			return nil, fmt.Errorf("%s: %v", n, err)
+		}
+		out := n + ".o"
+		err = ioutil.WriteFile(out, buf.Bytes(), 0644)
+		if err != nil {
+			return nil, fmt.Errorf("%s: %v", out, err)
+		}
+		result = append(result, out)
+	}
+	return result, nil
+}
+
+func apply(names []string) {
 	args := []string{"apply"}
 	for _, n := range names {
 		args = append(args, "-f", n)
 	}
-	run("kubectl", args...)
-	wait(names)
-}
+	fmt.Printf("kubectl %s\n", strings.Join(args, " "))
+	cmd := exec.Command("kubectl", args...)
 
-func run(command string, args ...string) {
-	fmt.Printf("%s %s\n", command, strings.Join(args, " "))
-	cmd := exec.Command(command, args...)
-	out, err := cmd.CombinedOutput()
-	fmt.Print(string(out))
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		panic(err)
+	}
+	go copy(os.Stdout, stdout)
+
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		panic(err)
+	}
+	go copy(os.Stderr, stderr)
+
+	err = cmd.Run()
 	if err != nil {
 		panic(err)
 	}
 }
 
-func wait(names []string) {
-	w := k8s.NewWaiter(nil)
-	err := w.ScanPaths(names)
+func copy(dst io.Writer, src io.Reader) {
+	_, err := io.Copy(dst, src)
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
-	if !w.Wait(time.Duration(*timeout) * time.Second) {
-		panic("not ready")
-	}
-
 }
