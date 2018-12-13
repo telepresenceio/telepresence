0b96b217dca04e1a1db8b9203bdb3b36d8b8317a
Abhay Saxena <ark3@datawire.io>
2018-12-13 16:59:16 -0500

    Log the paths of the binaries we check for

--- telepresence/runner/runner.py
+++ telepresence/runner/runner.py
@@ -224,7 +224,14 @@ class Runner(object):
         """
         Find unavailable commands from a set of dependencies
         """
-        return [command for command in commands if which(command) is None]
+        missing = []
+        for command in commands:
+            path = which(command)
+            if path:
+                self.write("Found {} -> {}".format(command, path))
+            else:
+                missing.append(command)
+        return missing
 
     def require(self, commands: typing.Iterable[str], message: str) -> None:
         """
