13533a066f6e1d44fd8be454ab94aaf0f8dd8a31
Luke Shumaker <lukeshu@datawire.io>
2018-12-21 15:33:09 -0800

    go-*.mk: check-go-fmt: Don't check 'vendor' directories

--- build-aux/_go-common.mk
+++ build-aux/_go-common.mk
@@ -43,7 +43,7 @@ go-build: $(addprefix bin_$(GOOS)_$(GOARCH)/,$(notdir $(go.bins)))
 .PHONY: go-build
 
 check-go-fmt: ## Check whether the code conforms to `gofmt`
-	test -z "$$(git ls-files -z '*.go' | xargs -0 gofmt -d | tee /dev/stderr)"
+	test -z "$$(git ls-files '*.go' | grep -v -e ^vendor/ -e /vendor/ | xargs gofmt -d | tee /dev/stderr)"
 .PHONY: check-go-fmt
 
 go-vet: ## Check the code with `go vet`
