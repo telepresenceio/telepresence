913f28c3f77a5d8bb02d5e81a3a37961fe672836
Luke Shumaker <lukeshu@datawire.io>
2018-12-23 18:09:37 -0500

    Makefile: Make the tests safe with parallel Make
    Use flock(1) to make sure that only one target uses the firewall at a time.

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 include build-aux/common.mk
 include build-aux/go.mk
+include build-aux/flock.mk
 include build-aux/kubernaut-ui.mk
 include build-aux/help.mk
 
@@ -17,9 +18,9 @@ go.DISABLE_GO_TEST = y
 go-test-nat: go-get
 	go test -v -exec sudo $(go.module)/internal/pkg/nat
 go-test-teleproxy: go-get test-cluster
-	go test -v -exec "sudo env PATH=${PATH} KUBECONFIG=${KUBECONFIG}" $(go.module)/cmd/teleproxy
+	$(FLOCK) firewall.lock go test -v -exec "sudo env PATH=${PATH} KUBECONFIG=${KUBECONFIG}" $(go.module)/cmd/teleproxy
 go-test-other: go-get
-	go test -v $(filter-out $(go.module)/internal/pkg/nat $(go.module)/cmd/teleproxy,$(go.pkgs))
+	$(FLOCK) firewall.lock go test -v $(filter-out $(go.module)/internal/pkg/nat $(go.module)/cmd/teleproxy,$(go.pkgs))
 .PHONY: go-test-nat go-test-teleproxy go-test-other
 go-test: go-test-nat go-test-teleproxy go-test-other
 
@@ -27,7 +28,7 @@ check-docker:
 ifneq ($(shell which docker 2>/dev/null),)
 check-docker: $(addprefix bin_linux_amd64/,$(notdir $(go.bins)))
 	docker build -f scripts/Dockerfile . -t teleproxy-make
-	docker run --cap-add=NET_ADMIN teleproxy-make go-test-nat
+	$(if $(filter linux,$(GOOS)),$(FLOCK) firewall.lock) docker run --cap-add=NET_ADMIN teleproxy-make go-test-nat
 else
 	@echo "SKIPPING DOCKER TESTS"
 endif
@@ -35,6 +36,7 @@ endif
 check: check-docker
 
 clean: release
+	$(FLOCK) firewall.lock rm firewall.lock
 
 # Utility targets
 
---
+++ build-aux/flock.go
@@ -0,0 +1,95 @@
+// Copyright 2018 Datawire. All rights reserved.
+
+// +build ignore
+
+// flock.go is a minimal implementation of flock(1) (from util-linux)
+// for systems that don't have flock(1) but do have flock(2).
+//
+// According to the Linux "man-pages"[1] flock(2) documentation:
+//
+//     CONFORMING TO
+//            4.4BSD (the flock() call first appeared in 4.2BSD).  A
+//            version of flock(), possibly implemented in terms of
+//            fcntl(2), appears on most UNIX systems.
+//
+// At the very least, macOS has flock(2).
+//
+// [1]: https://www.kernel.org/doc/man-pages/
+//
+// No, BSD/macOS shlock(1) is not an accepable substitute.  It is the
+// opposite of robust.
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path"
+	"strings"
+	"syscall"
+)
+
+var program_invocation_short_name = path.Base(os.Args[0])
+
+func usage() {
+	fmt.Printf("Usage: %s FILE|DIRECTORY COMMAND [ARGUMENTS]\n", program_invocation_short_name)
+	fmt.Printf("Manage file locks from shell scripts.\n")
+	fmt.Printf("\n")
+	fmt.Printf("This implementation does not support any options.\n")
+	fmt.Printf("This implementation does not support other usages.\n")
+	os.Exit(0)
+}
+
+func exit(err error) {
+	fmt.Fprintf(os.Stderr, "%s: %v\n", program_invocation_short_name, err)
+	os.Exit(1)
+}
+
+func errusage(format string, a ...interface{}) {
+	if format != "" {
+		fmt.Fprintf(os.Stderr, "%s: %s\n", program_invocation_short_name, fmt.Sprintf(format, a...))
+	}
+	fmt.Fprintf(os.Stderr, "Try '%s --help' for more information.\n", program_invocation_short_name)
+	os.Exit(64)
+}
+
+func main() {
+	if len(os.Args) < 3 {
+		if len(os.Args) > 1 {
+			if os.Args[1] == "-h" || os.Args[1] == "--help" {
+				usage()
+			}
+			if strings.HasPrefix(os.Args[1], "-") {
+				errusage("invalid option: %q", os.Args[1])
+			}
+		}
+		errusage("not enough arguments")
+	}
+
+	file, err := os.OpenFile(os.Args[1], os.O_RDONLY|os.O_CREATE, 0666)
+	if pe, ok := err.(*os.PathError); ok && pe.Err == syscall.EISDIR {
+		file, err = os.OpenFile(os.Args[1], os.O_RDONLY, 0666)
+	}
+	if err != nil {
+		exit(err)
+	}
+
+	err = syscall.Flock(int(file.Fd()), syscall.LOCK_EX)
+	if err != nil {
+		err = &os.PathError{Op: "flock", Path: file.Name(), Err: err}
+		exit(err)
+	}
+
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err = cmd.Run()
+	if err != nil {
+		if ee, ok := err.(*exec.ExitError); ok {
+			os.Exit(ee.ProcessState.Sys().(syscall.WaitStatus).ExitStatus())
+		}
+		exit(err)
+	}
+	file.Close()
+}
