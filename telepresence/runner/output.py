b5083450141488ffdabe959f6dad09d05a9cb1f9
Abhay Saxena <ark3@datawire.io>
2019-01-04 15:01:04 -0500

    Fail fast when cwd doesn't exist
    Fixes #882

--- telepresence/runner/output.py
+++ telepresence/runner/output.py
@@ -50,6 +50,13 @@ class Output(object):
         :param logfile_path: Path or string file path or "-" for stdout
         """
 
+        # Fail if current working directory does not exist so we don't crash in
+        # standard library path-handling code.
+        try:
+            os.getcwd()
+        except OSError:
+            exit("T: Error: Current working directory does not exist.")
+
         if logfile_path == "-":
             self.logfile = sys.stdout
         else:
