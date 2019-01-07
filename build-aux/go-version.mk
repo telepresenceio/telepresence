35cfb0936509c06fb2ca9264ade750c3d09cb059
Luke Shumaker <lukeshu@datawire.io>
2019-01-07 11:29:48 -0500

    Add go-version.mk

---
+++ build-aux/go-version.mk
@@ -0,0 +1,16 @@
+# Copyright 2018 Datawire. All rights reserved.
+#
+# Makefile snippet for automatically including the version number in
+# Go executables.
+#
+## Inputs ##
+#  - Variable: VERSION ?= …
+## Outputs ##
+#  - Variable: go.LDFLAGS += …
+ifeq ($(words $(filter $(abspath $(lastword $(MAKEFILE_LIST))),$(abspath $(MAKEFILE_LIST)))),1)
+include $(dir $(lastword $(MAKEFILE_LIST)))common.mk
+
+VERSION ?= $(patsubst v%,%,$(shell git describe --tags --always --dirty))
+go.LDFLAGS += -X main.Version=$(VERSION)
+
+endif
