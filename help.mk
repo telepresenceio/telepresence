93be46b4558a9b9ed1ef75f1c556c68fd822c329
Luke Shumaker <lukeshu@datawire.io>
2019-01-08 18:02:18 -0500

    Squashed 'build-aux/' changes from 7de4081..6bd75ce
    6bd75ce (teleproxy) Merge branch 'master' into lukeshu/merge
    7d3dc65 (teleproxy) Add go-version.mk
    20296b9 (teleproxy) _go-common.mk: Add a `go.LDFLAGS ?=` variable
    33d5354 (teleproxy) common.mk: Add quote.shell; help.mk: Use that instead of _help.escape_squotes
    
    git-subtree-dir: build-aux
    git-subtree-split: 6bd75ce2436fbe0caf01e2afd7ae885c8c59a962

--- help.mk
+++ help.mk
@@ -45,30 +45,17 @@
 # Because your editor's syntax-highlighting might be unhappy with
 # things inside of help.body, you may prefix lines with "#" or "# ".
 ifeq ($(words $(filter $(abspath $(lastword $(MAKEFILE_LIST))),$(abspath $(MAKEFILE_LIST)))),1)
+include $(dir $(lastword $(MAKEFILE_LIST)))common.mk
 
 help.body ?=
 
 help:  ## Show this message
 	@echo 'Usage: make [TARGETS...]'
 	@echo
-	@printf '%s\n' $(call _help.quote.shell,$(help.body)) | sed -e 's/^# //' -e 's/^#//'
+	@printf '%s\n' $(call quote.shell,$(help.body)) | sed -e 's/^# //' -e 's/^#//'
 	@echo
 	@echo TARGETS:
 	@sed -En 's/^([^#]*) *: *[#]# */\1	/p' ${MAKEFILE_LIST} | column -t -s '	' | sed 's/^/  /'
 .PHONY: help
-
-define _help.nl
-
-
-endef
-
-# I put this as the last line in the file because it confuses Emacs
-# syntax highlighting and makes the remainder of the file difficult to
-# edit.
-#
-# Based on
-# https://git.lukeshu.com/autothing/tree/build-aux/Makefile.once.head/00-quote.mk?id=9384e763b00774603208b3d44977ed0e6762a09a
-# but modified to make newlines work with shells other than Bash.
-_help.quote.shell = "$$(printf '%s\n' $(subst $(_help.nl),' ','$(subst ','\'',$1)'))"
 
 endif
