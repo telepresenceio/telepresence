0be48a018bf9e832f492c6631d0942947c1a87ae
Rafael Schloming <rhs@alum.mit.edu>
2019-03-19 13:15:33 -0400

    make limiter smarter about when it tells you to delay stuff

--- pkg/limiter/limiter.go
+++ pkg/limiter/limiter.go
@@ -3,17 +3,17 @@ package limiter
 import "time"
 
 // A limiter can be used to rate limit and/or coalesce a series of
-// time-based events. This just captures the logic of deciding whether
-// an event occurring at a specific time T should be ignored, acted
-// upon, or delayed.
+// time-based events. This interface captures the logic of deciding
+// what action should be taken when an event occurs at a specific time
+// T. The possible actions are act upon the event, do nothing, or
+// check back after a specific delay.
 type Limiter interface {
 	// The Limit() method works kinda like an 8 ball. You pass it
 	// the time of an event, and it returns one of Yes, No, or
 	// "Later". A zero return value means "Yes", the event should
 	// be acted upon right away. A negative return value means
-	// "No", the event should be dropped entirely, and a positive
-	// return value means to check back within the returned
-	// interval.
+	// "No", do nothing, and a positive return value means to
+	// check back after the returned delay.
 	//
 	// The order that this is invoked is important, and it is
 	// expected that this is invoked with a monotonically
@@ -22,12 +22,21 @@ type Limiter interface {
 	// of time.Now(), but for testing or other cases, this could
 	// be invoked with any set of historic or future timestamps so
 	// long as they are invoked in monotonically increasing order.
+	//
+	// The result of this (when positive) is always relative to
+	// the passed in time. In other words to compute a deadline
+	// rather than a delay, you should take the result of Limit()
+	// and add it to whatever value you passed in:
+	//
+	//   deadline = now + Limit(now).
+	//
 	Limit(now time.Time) time.Duration
 }
 
 type limiter struct {
 	interval   time.Duration
 	lastAction time.Time
+	deadline   time.Time
 }
 
 // Constructs a new limiter that will coalesce any events occurring
@@ -40,10 +49,15 @@ func NewIntervalLimiter(interval time.Duration) Limiter {
 
 func (l *limiter) Limit(now time.Time) time.Duration {
 	since := now.Sub(l.lastAction)
-	if since >= l.interval {
+	switch {
+	case since >= l.interval:
 		l.lastAction = now
 		return 0
-	} else {
-		return l.interval - since
+	case l.deadline.After(now):
+		return -1
+	default:
+		delay := l.interval - since
+		l.deadline = now.Add(delay)
+		return delay
 	}
 }
