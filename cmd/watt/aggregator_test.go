cfd8e823dfe5e274852118e5c8303803816a4d1f
Rafael Schloming <rhs@alum.mit.edu>
2019-03-22 16:14:23 -0400

    added some testutils to remove boilerplate from aggregator test code

--- cmd/watt/aggregator_test.go
+++ cmd/watt/aggregator_test.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"strings"
 	"testing"
 	"time"
 
@@ -11,17 +11,78 @@ import (
 	"github.com/datawire/teleproxy/pkg/supervisor"
 )
 
-// Bugs:
-//
-//  0. shutdown happens prior to bootstrap state being achieved
-//
-//  1. start up against an empty cluster
-//     + we end up never achieving a bootstrapped state
-//
-//  2. start up against a changing cluster
-//     + we learn about an initial set of consul services
-//     + one of them gets deleted before its watcher gets an answer for us
-//     + we will always have a nil entry in the required services map
+type aggIsolator struct {
+	// input channels
+	kubewatchesToAggregatorCh   chan k8sEvent
+	consulwatchesToAggregatorCh chan consulwatch.Endpoints
+	// output channels
+	aggregatorToConsulwatchmanCh chan []k8s.Resource
+	aggregatorToInvokerCh        chan string
+	aggregator                   *aggregator
+	sup                          *supervisor.Supervisor
+	done                         chan struct{}
+	t                            *testing.T
+	cancel                       context.CancelFunc
+}
+
+func newAggIsolator(t *testing.T) *aggIsolator {
+	iso := &aggIsolator{
+		// by using zero length channels for inputs here, we can
+		// control the total ordering of all inputs and therefore
+		// intentionally trigger any order of events we want to test
+		kubewatchesToAggregatorCh:   make(chan k8sEvent),
+		consulwatchesToAggregatorCh: make(chan consulwatch.Endpoints),
+		// we need to create buffered channels for outputs because
+		// nothing is asynchronously reading them in the test
+		aggregatorToConsulwatchmanCh: make(chan []k8s.Resource, 100),
+		aggregatorToInvokerCh:        make(chan string, 100),
+		// for signaling when the isolator is done
+		done: make(chan struct{}),
+	}
+	iso.aggregator = &aggregator{
+		kubernetesEventsCh: iso.kubewatchesToAggregatorCh,
+		consulEndpointsCh:  iso.consulwatchesToAggregatorCh,
+		consulWatchesCh:    iso.aggregatorToConsulwatchmanCh,
+		snapshotCh:         iso.aggregatorToInvokerCh,
+		// XXX: the stuff below is really initializing
+		// internal data structures of the aggregator, we
+		// should probably have a constructor or something so
+		// this kind of stuff doesn't need to appear in the
+		// tests
+		kubernetesResources: make(map[string][]k8s.Resource),
+		consulEndpoints:     make(map[string]consulwatch.Endpoints),
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	iso.cancel = cancel
+	iso.sup = supervisor.WithContext(ctx)
+	iso.sup.Supervise(&supervisor.Worker{
+		Name: "aggregator",
+		Work: iso.aggregator.Work,
+	})
+	return iso
+}
+
+func startAggIsolator(t *testing.T) *aggIsolator {
+	iso := newAggIsolator(t)
+	iso.Start()
+	return iso
+}
+
+func (iso *aggIsolator) Start() {
+	go func() {
+		errs := iso.sup.Run()
+		if len(errs) > 0 {
+			iso.t.Errorf("unexpected errors: %v", errs)
+		}
+		close(iso.done)
+	}()
+}
+
+func (iso *aggIsolator) Stop() {
+	iso.sup.Shutdown()
+	iso.cancel()
+	<-iso.done
+}
 
 func resources(input string) []k8s.Resource {
 	result, err := k8s.ParseResources("aggregator-test", input)
@@ -61,75 +122,43 @@ data:
 `)
 )
 
-func TestAggregator(t *testing.T) {
-	// by using zero length channels for inputs here, we can
-	// control the total ordering of all inputs and therefore
-	// intentionally trigger any order of events we want to test
-	kubewatchesToAggregatorCh := make(chan k8sEvent)
-	consulwatchesToAggregatorCh := make(chan consulwatch.Endpoints)
-	// we need to create buffered channels for outputs because
-	// nothing is asynchronously reading them in the test
-	aggregatorToConsulwatchmanCh := make(chan []k8s.Resource, 100)
-	aggregatorToInvokerCh := make(chan string, 100)
+// Bugs:
+//
+//  0. shutdown happens prior to bootstrap state being achieved
+//
+//  1. start up against an empty cluster
+//     + we end up never achieving a bootstrapped state
+//
+//  2. start up against a changing cluster
+//     + we learn about an initial set of consul services
+//     + one of them gets deleted before its watcher gets an answer for us
+//     + we will always have a nil entry in the required services map
 
-	agg := &aggregator{
-		kubernetesEventsCh: kubewatchesToAggregatorCh,
-		consulEndpointsCh:  consulwatchesToAggregatorCh,
-		consulWatchesCh:    aggregatorToConsulwatchmanCh,
-		snapshotCh:         aggregatorToInvokerCh,
-		// XXX: the stuff below is really initializing
-		// internal data structures of the aggregator, we
-		// should probably have a constructor or something so
-		// this kind of stuff doesn't need to appear in the
-		// tests
-		kubernetesResources: make(map[string][]k8s.Resource),
-		consulEndpoints:     make(map[string]consulwatch.Endpoints),
-	}
+func TestAggregatorBug1(t *testing.T) {
+	iso := startAggIsolator(t)
+	defer iso.Stop()
 
-	sup := supervisor.WithContext(context.Background())
-
-	go func() {
-		timeout := time.NewTimer(1 * time.Second)
-
-		// initial kubernetes state is just services
-		kubewatchesToAggregatorCh <- k8sEvent{"service", SERVICES}
-
-		// now we should see some watches
-		// we expect aggregator to generate a snapshot after the first event
-		select {
-		case snapshot := <-aggregatorToInvokerCh:
-			fmt.Println(snapshot)
-		case <-timeout.C:
-			t.Errorf("expecting to see a snapshot")
-		}
-
-		// whenever the aggregator sees updated k8s state, it
-		// should send an update to the consul watch manager,
-		// in this case it will be empty
-		select {
-		case watches := <-aggregatorToConsulwatchmanCh:
-			fmt.Println(watches)
-		case <-timeout.C:
-			t.Errorf("expecting to see an empty watch update")
-		}
-
-		kubewatchesToAggregatorCh <- k8sEvent{"configmap", RESOLVER}
-
-		select {
-		case watches := <-aggregatorToConsulwatchmanCh:
-			fmt.Println(watches)
-		case <-timeout.C:
-			t.Errorf("expecting to see bar")
-		}
-		sup.Shutdown()
-	}()
-
-	sup.Supervise(&supervisor.Worker{
-		Name: "aggregator",
-		Work: agg.Work,
+	// initial kubernetes state is just services
+	iso.kubewatchesToAggregatorCh <- k8sEvent{"service", SERVICES}
+	// we expect aggregator to generate a snapshot after the first event
+	expect(t, iso.aggregatorToInvokerCh, func(value string) bool {
+		return strings.Contains(value, "snapshot")
 	})
-	errs := sup.Run()
-	if len(errs) > 0 {
-		t.Errorf("unexpected errors: %v", errs)
-	}
+	// whenever the aggregator sees updated k8s state, it
+	// should send an update to the consul watch manager,
+	// in this case it will be empty
+	expect(t, iso.aggregatorToConsulwatchmanCh, []k8s.Resource(nil))
+
+	iso.kubewatchesToAggregatorCh <- k8sEvent{"configmap", RESOLVER}
+	expect(t, iso.aggregatorToConsulwatchmanCh, func(watches []k8s.Resource) bool {
+		if len(watches) != 1 {
+			return false
+		}
+
+		if watches[0].Name() != "bar" {
+			return false
+		}
+
+		return true
+	})
 }
