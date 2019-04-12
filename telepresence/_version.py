8d4cfde5984f35a72c48734c6c4fdbd4c2be0b54
Abhay Saxena <ark3@datawire.io>
2019-04-12 15:15:00 -0400

    Satisfy latest yapf

--- telepresence/_version.py
+++ telepresence/_version.py
@@ -343,9 +343,8 @@ def git_pieces_from_vcs(tag_prefix, root, verbose, run_command=run_command):
         pieces["distance"] = int(count_out)  # total number of commits
 
     # commit date: see ISO-8601 comment in git_versions_from_keywords()
-    date = run_command(
-        GITS, ["show", "-s", "--format=%ci", "HEAD"], cwd=root
-    )[0].strip()
+    date = run_command(GITS, ["show", "-s", "--format=%ci", "HEAD"],
+                       cwd=root)[0].strip()
     pieces["date"] = date.strip().replace(" ", "T", 1).replace(" ", "", 1)
 
     return pieces
