15d568b982bf7cdb6103a791b96d7b2eff664811
Rafael Schloming <rhs@alum.mit.edu>
2019-04-26 13:27:05 -0400

    add an index page

--- cmd/watt/invoker.go
+++ cmd/watt/invoker.go
@@ -74,6 +74,13 @@ func (a *invoker) getSnapshot(id int) string {
 	return a.invokedSnapshots[id]
 }
 
+func (a *invoker) getKeys() (result []int) {
+	for i := range a.invokedSnapshots {
+		result = append(result, i)
+	}
+	return
+}
+
 func (a *invoker) invoke() {
 	id := a.storeSnapshot(a.latestSnapshot)
 	for _, n := range a.notify {
@@ -91,22 +98,31 @@ type apiServer struct {
 
 func (s *apiServer) Work(p *supervisor.Process) error {
 	http.HandleFunc("/snapshots/", func(w http.ResponseWriter, r *http.Request) {
-		id, err := strconv.Atoi(strings.TrimPrefix(r.URL.Path, "/snapshots/"))
-		if err != nil {
-			http.Error(w, "ID is not an integer", http.StatusBadRequest)
-			return
-		}
+		relpath := strings.TrimPrefix(r.URL.Path, "/snapshots/")
 
-		snapshot := s.invoker.getSnapshot(id)
+		if relpath == "" {
+			w.Header().Set("content-type", "text/html")
+			if _, err := w.Write([]byte(s.index())); err != nil {
+				p.Logf("write index error: %v", err)
+			}
+		} else {
+			id, err := strconv.Atoi(relpath)
+			if err != nil {
+				http.Error(w, "ID is not an integer", http.StatusBadRequest)
+				return
+			}
 
-		if snapshot == "" {
-			w.WriteHeader(http.StatusNotFound)
-			return
-		}
+			snapshot := s.invoker.getSnapshot(id)
 
-		w.Header().Set("content-type", "application/json")
-		if _, err := w.Write([]byte(snapshot)); err != nil {
-			p.Logf("write snapshot error: %v", err)
+			if snapshot == "" {
+				w.WriteHeader(http.StatusNotFound)
+				return
+			}
+
+			w.Header().Set("content-type", "application/json")
+			if _, err := w.Write([]byte(snapshot)); err != nil {
+				p.Logf("write snapshot error: %v", err)
+			}
 		}
 	})
 
@@ -128,4 +144,18 @@ func (s *apiServer) Work(p *supervisor.Process) error {
 
 	<-p.Shutdown()
 	return srv.Shutdown(p.Context())
+}
+
+func (s *apiServer) index() string {
+	var result strings.Builder
+
+	result.WriteString("<html><body><ul>\n")
+
+	for _, v := range s.invoker.getKeys() {
+		result.WriteString(fmt.Sprintf("  <li><a href=\"%d\">%d</a></li>\n", v, v))
+	}
+
+	result.WriteString("</ul></body></html>\n")
+
+	return result.String()
 }
