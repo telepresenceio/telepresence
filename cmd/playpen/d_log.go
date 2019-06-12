1f3831b7e85eadf3cdd917095abcf927f8b7403a
Abhay Saxena <ark3@datawire.io>
2019-06-12 12:18:54 -0400

    Emit compact timestamps when logging to the console

--- cmd/playpen/d_log.go
+++ cmd/playpen/d_log.go
@@ -14,7 +14,9 @@ import (
 )
 
 // DaemonFormatter formats log messages for the Playpen Daemon
-type DaemonFormatter struct{}
+type DaemonFormatter struct {
+	TimestampFormat string
+}
 
 // Format implement logrus.Formatter
 func (f *DaemonFormatter) Format(entry *logrus.Entry) ([]byte, error) {
@@ -25,7 +27,7 @@ func (f *DaemonFormatter) Format(entry *logrus.Entry) ([]byte, error) {
 		b = &bytes.Buffer{}
 	}
 
-	fmt.Fprintf(b, "%s %s", entry.Time.Format("2006/01/02 15:04:05"), entry.Message)
+	fmt.Fprintf(b, "%s %s", entry.Time.Format(f.TimestampFormat), entry.Message)
 
 	if len(entry.Data) > 0 {
 		keys := make([]string, 0, len(entry.Data))
@@ -44,9 +46,14 @@ func (f *DaemonFormatter) Format(entry *logrus.Entry) ([]byte, error) {
 
 // SetUpLogging sets up standard Playpen Daemon logging
 func SetUpLogging() supervisor.Logger {
+	loggingToTerminal := terminal.IsTerminal(int(os.Stdout.Fd()))
 	logger := logrus.StandardLogger()
-	logger.Formatter = new(DaemonFormatter)
-	if !terminal.IsTerminal(int(os.Stdout.Fd())) {
+	formatter := new(DaemonFormatter)
+	logger.Formatter = formatter
+	if loggingToTerminal {
+		formatter.TimestampFormat = "15:04:05"
+	} else {
+		formatter.TimestampFormat = "2006/01/02 15:04:05"
 		logger.SetOutput(&lumberjack.Logger{
 			Filename:   logfile,
 			MaxSize:    10,   // megabytes
