5c7baf1983108924bfd5e8b5c5093c4dd2c46227
Rafael Schloming <rhs@alum.mit.edu>
2019-06-14 17:29:34 -0400

    improved error messages

--- pkg/dtest/manifests.go
+++ pkg/dtest/manifests.go
@@ -9,10 +9,35 @@ import (
 	"github.com/datawire/teleproxy/pkg/kubeapply"
 )
 
+const msg = `
+kubeconfig does not exist: %s
+
+  Run "make claim" to acquire a kubernaut cluster, or use
+  TELEPROXY_DEV_KUBECONFIG to point elsewhere
+
+`
+
 func Manifests(kubeconfig string, files ...string) {
+	override := os.Getenv("TELEPROXY_DEV_KUBECONFIG")
+	if override != "" {
+		kubeconfig = override
+	}
+
+	if _, err := os.Stat(kubeconfig); os.IsNotExist(err) {
+		fmt.Printf(msg, kubeconfig)
+		os.Exit(1)
+	}
+
 	err := kubeapply.Kubeapply(k8s.NewKubeInfo(kubeconfig, "", ""), 30*time.Second, false, files...)
 	if err != nil {
+		fmt.Println()
 		fmt.Println(err)
+		fmt.Print(`
+  Please note, if this is a timeout, your kubernaut cluster may have
+  been preempted or expired. Use "make unclaim && make claim" to get a
+  fresh cluster.
+
+`)
 		os.Exit(1)
 	}
 }
