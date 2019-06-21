7adb828dd23ca1c38cf666ce067bc0278029af14
Abhay Saxena <ark3@datawire.io>
2019-06-21 12:01:21 -0400

    Rewrite Resources again, use tasks instead of locks

--- cmd/playpen/d_resource.go
+++ cmd/playpen/d_resource.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"fmt"
-	"sync"
 	"syscall"
+	"time"
 
 	"github.com/datawire/teleproxy/pkg/supervisor"
 )
@@ -12,163 +12,170 @@ import (
 // network intercepts (via teleproxy intercept) and cluster connectivity.
 type Resource interface {
 	Name() string
-	Enable(*supervisor.Process) error
-	Monitor(*supervisor.Process) error
-	Disable(*supervisor.Process) error
-	IsEnabled() bool
 	IsOkay() bool
+	Close() error
 }
 
-// CommandResource represents a resource that is associated with a running
-// subprocess
-type CommandResource struct {
-	name    string
-	args    []string
-	rai     *RunAsInfo
-	check   func(*supervisor.Process) error
-	lock    sync.Mutex
-	enabled bool            // (Enable/Disable) disabled by default
-	broken  bool            // (Monitor) okay by default (disabled -> okay)
-	cmd     *supervisor.Cmd // (run loop) tracks the cmd for killing it
-	running bool            // (run loop) not running by default, of course
+// crCmd is a handle to a checked retrying command
+type crCmd struct {
+	name     string
+	args     []string
+	rai      *RunAsInfo
+	check    func() error
+	tasks    chan func() error
+	callerP  *supervisor.Process // processor's Process
+	cmd      *supervisor.Cmd     // (run loop) tracks the cmd for killing it
+	quitting bool                // (Close) to get everything to quit
+	okay     bool                // (monitor) cmd is running and check passes
 }
 
-// NewCommandResource returns a new Command Resource with the specified name and
-// command arguments
-func NewCommandResource(name string, args []string, rai *RunAsInfo) *CommandResource {
-	return &CommandResource{
-		name: name,
-		args: args,
-		rai:  rai,
+// CheckedRetryingCommand launches a command, restarting it repeatedly if it
+// quits, and killing and restarting it if it fails the given check.
+func CheckedRetryingCommand(
+	p *supervisor.Process, name string, args []string, rai *RunAsInfo, check func() error,
+) (Resource, error) {
+	if check == nil {
+		check = func() error { return nil }
 	}
+	crc := &crCmd{
+		name:    name,
+		args:    args,
+		rai:     rai,
+		check:   check,
+		tasks:   make(chan func() error, 1),
+		callerP: p,
+	}
+	p.Supervisor().Supervise(&supervisor.Worker{
+		Name: "crc/" + crc.name,
+		Work: crc.processor,
+	})
+	if err := crc.launch(); err != nil {
+		return nil, err
+	}
+	return crc, nil
 }
 
 // Name implements Resource
-func (cr *CommandResource) Name() string {
-	cr.lock.Lock()
-	defer cr.lock.Unlock()
-	return cr.name
+func (crc *crCmd) Name() string {
+	res := make(chan string)
+	crc.tasks <- func() error {
+		res <- crc.name
+		return nil
+	}
+	return <-res
 }
 
-// Enable marks the command resource as enabled. It launches a supervised
-// goroutine (the runLoop) to keep a subprocess running for this command.
-func (cr *CommandResource) Enable(p *supervisor.Process) error {
-	cr.lock.Lock()
-	defer cr.lock.Unlock()
-	if cr.enabled {
-		return fmt.Errorf("trying to enable already-enabled CR %s", cr.name)
+// IsOkay returns whether the resource is okay as far as monitoring is aware
+func (crc *crCmd) IsOkay() bool {
+	res := make(chan bool)
+	crc.tasks <- func() error {
+		res <- crc.okay
+		return nil
 	}
-	cr.enabled = true
-	p.Supervisor().Supervise(&supervisor.Worker{
-		Name:  "run/" + cr.name,
-		Work:  cr.runLoop,
-		Retry: true,
+	return <-res
+}
+
+// Close shuts down this resource
+func (crc *crCmd) Close() error {
+	done := make(chan struct{})
+	crc.tasks <- func() error {
+		defer close(done)
+		return crc.quit()
+	}
+	<-done
+
+	// FIXME: Wait until things have closed?
+	return nil
+}
+
+func (crc *crCmd) processor(p *supervisor.Process) error {
+	ticker := time.NewTicker(3 * time.Second)
+	defer ticker.Stop()
+	p.Ready()
+	for {
+		var task func() error
+		select {
+		case fn := <-crc.tasks: // There is work to do
+			task = fn
+		case <-ticker.C: // Ticker says it's time to monitor
+			task = func() error { return crc.monitor(p) }
+		case <-p.Shutdown(): // Supervisor told us to quit
+			task = crc.quit
+		}
+		if err := task(); err != nil {
+			return err
+		}
+		if crc.quitting {
+			return nil
+		}
+	}
+}
+
+func (crc *crCmd) launch() error {
+	if crc.cmd != nil {
+		panic(fmt.Errorf("launching %s: already launched", crc.name))
+	}
+	launchErr := make(chan error)
+	crc.callerP.Supervisor().Supervise(&supervisor.Worker{
+		Name: "proc/" + crc.name,
+		Work: func(p *supervisor.Process) error {
+			// Launch the subprocess
+			crc.cmd = crc.rai.Command(p, crc.args...)
+			if err := crc.cmd.Start(); err != nil {
+				launchErr <- err
+				return nil
+			}
+			launchErr <- nil
+			p.Ready()
+
+			// Wait for the subprocess to end, log
+			p.Logf("subprocess ended: %v", p.DoClean(crc.cmd.Wait, crc.kill))
+			crc.cmd = nil
+
+			return nil
+		},
 	})
-	return nil
+	select {
+	case err := <-launchErr:
+		return err
+	case <-crc.callerP.Shutdown():
+		return nil
+	}
 }
 
-// Disable marks the command resource as disabled. It then kills any running
-// subprocess, which will allow the runLoop goroutine to finish.
-func (cr *CommandResource) Disable(p *supervisor.Process) error {
-	cr.lock.Lock()
-	defer cr.lock.Unlock()
-	if !cr.enabled {
-		return fmt.Errorf("trying to disable already-disabled CR %s", cr.name)
+func (crc *crCmd) kill() error {
+	if crc.cmd != nil {
+		return crc.cmd.Process.Signal(syscall.SIGTERM)
 	}
-	cr.enabled = false
-	if cr.running {
-		return cr.cmd.Process.Signal(syscall.SIGTERM)
-	}
-	return nil
+	return nil // Or fmt.Errorf("trying to kill non-running subprocess for %s", crc.name)
 }
 
-// Monitor updates the recorded status of the command resource.
-func (cr *CommandResource) Monitor(p *supervisor.Process) error {
-	cr.lock.Lock()
-	defer cr.lock.Unlock()
+func (crc *crCmd) quit() error {
+	crc.quitting = true
+	return crc.kill()
+}
 
-	// Determine and record whether the resource is broken
-	cr.broken = func() bool {
-		if !cr.enabled {
-			// Disabled resources are always okay
-			return false
-		}
-		if !cr.running {
-			// Not running is broken. The runLoop will restart the process at
-			// some point soon.
-			return true
-		}
-		if cr.check == nil {
-			// No additional check, so just running is okay
-			return false
-		}
-		err := cr.check(p)
-		if err == nil {
-			// Check passed
-			return false
-		}
-		p.Logf("Resource %s failed check: %v", cr.name, err)
-		return true
-	}()
-
-	// Kill the process if it's in a bad state
-	if cr.broken && cr.running {
-		err := cr.cmd.Process.Signal(syscall.SIGTERM)
-		if err != nil {
+// monitor determines and records whether the resource is okay
+func (crc *crCmd) monitor(p *supervisor.Process) error {
+	defer func(old bool) { MaybeNotify(p, crc.name, old, crc.okay) }(crc.okay)
+	if crc.cmd == nil {
+		crc.okay = false // Not running is not okay
+		crc.tasks <- crc.launch
+		return nil
+	}
+	if err := crc.check(); err != nil {
+		crc.okay = false // Check failed is not okay
+		p.Logf("check failed: %v", err)
+		// Kill the process because it's in a bad state
+		if err := crc.kill(); err != nil {
 			// Failure to kill is a fatal error
 			// FIXME: This will be a problem if the resource is in the process
 			// of dying when we user-check it, but is dead when we get around to
 			// killing it.
-			p.Logf("Failed to kill %s on check fail: %v", cr.name, err)
-			p.Log("Giving up.")
+			p.Logf("failed to kill: %v", err)
 			return err
 		}
-		// The runLoop will restart the process, at which point we hope things
-		// will be better.
 	}
-
+	crc.okay = true
 	return nil
-}
-
-// IsEnabled returns whether this resource is enabled
-func (cr *CommandResource) IsEnabled() bool {
-	cr.lock.Lock()
-	defer cr.lock.Unlock()
-	return cr.enabled
-}
-
-// IsOkay returns whether the resource is okay as far as monitoring is aware
-func (cr *CommandResource) IsOkay() bool {
-	cr.lock.Lock()
-	defer cr.lock.Unlock()
-	return !cr.broken
-}
-
-// SetCheck sets the check function that will be called by Monitor()
-func (cr *CommandResource) SetCheck(check func(*supervisor.Process) error) {
-	cr.lock.Lock()
-	defer cr.lock.Unlock()
-	cr.check = check
-}
-
-// runLoop runs the command and manages cr.cmd, cr.running. Supervisor does the
-// actual looping thanks to the "Retry" parameter.
-func (cr *CommandResource) runLoop(p *supervisor.Process) error {
-	if !cr.IsEnabled() {
-		return nil // Indicate success so Supervisor does not retry
-	}
-
-	cr.cmd = cr.rai.Command(p, cr.args...)
-	err := cr.cmd.Start()
-	if err != nil {
-		return err
-	}
-	p.Ready()
-
-	cr.running = true
-	err = p.DoClean(cr.cmd.Wait, func() error { return cr.Disable(p) })
-	cr.running = false
-
-	// Return an error so Supervisor retries
-	return fmt.Errorf("%s subprocessed ended: %v", cr.Name(), err)
 }
