66066d4980ed4cfc99caf61440202871eaedd56b
Abhay Saxena <ark3@datawire.io>
2019-06-25 17:40:03 -0400

    Add ResourceBase and KCluster Resource
    This is broken because other code cannot access useful stuff in
    KCluster if it's treated as a resource. Maybe need to has-a?

--- cmd/playpen/d_resource.go
+++ cmd/playpen/d_resource.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	"syscall"
 	"time"
 
@@ -14,6 +15,151 @@ type Resource interface {
 	Name() string
 	IsOkay() bool
 	Close() error
+}
+
+// ResourceBase has helpers to create a monitored resource
+type ResourceBase struct {
+	name     string
+	doCheck  func(*supervisor.Process) error
+	doQuit   func() error
+	tasks    chan func() error
+	quitting bool // (Close) to get everything to quit
+	okay     bool // (monitor) cmd is running and check passes
+}
+
+// Name implements Resource
+func (rb *ResourceBase) Name() string {
+	res := make(chan string)
+	rb.tasks <- func() error {
+		res <- rb.name
+		return nil
+	}
+	return <-res
+}
+
+// IsOkay returns whether the resource is okay as far as monitoring is aware
+func (rb *ResourceBase) IsOkay() bool {
+	res := make(chan bool)
+	rb.tasks <- func() error {
+		res <- rb.okay
+		return nil
+	}
+	return <-res
+}
+
+// Close shuts down this resource
+func (rb *ResourceBase) Close() error {
+	done := make(chan struct{})
+	rb.tasks <- func() error {
+		defer close(done)
+		return rb.quit()
+	}
+	<-done
+
+	// FIXME: Wait until things have closed?
+	return nil
+}
+
+func (rb *ResourceBase) quit() error {
+	rb.quitting = true
+	return rb.doQuit()
+}
+
+func (rb *ResourceBase) monitor(p *supervisor.Process) error {
+	old := rb.okay
+	if err := rb.doCheck(p); err != nil {
+		rb.okay = false // Check failed is not okay
+		p.Logf("check failed: %v", err)
+	} else {
+		rb.okay = true
+	}
+	MaybeNotify(p, rb.name, old, rb.okay)
+	return nil
+}
+
+func (rb *ResourceBase) processor(p *supervisor.Process) error {
+	ticker := time.NewTicker(3 * time.Second)
+	defer ticker.Stop()
+	p.Ready()
+	for {
+		var task func() error
+		select {
+		case fn := <-rb.tasks: // There is work to do
+			task = fn
+		case <-ticker.C: // Ticker says it's time to monitor
+			task = func() error { return rb.monitor(p) }
+		case <-p.Shutdown(): // Supervisor told us to quit
+			task = rb.quit
+		}
+		if err := task(); err != nil {
+			return err
+		}
+		if rb.quitting {
+			return nil
+		}
+	}
+}
+
+// cluster is a Kubernetes cluster reference
+type cluster struct {
+	context string
+	server  string
+	rai     *RunAsInfo
+	kargs   []string
+	ResourceBase
+}
+
+func (c *cluster) getKubectlCmd(p *supervisor.Process, args ...string) *supervisor.Cmd {
+	cmdArgs := make([]string, 0, 1+len(c.kargs)+len(args))
+	cmdArgs = append(cmdArgs, "kubectl")
+	cmdArgs = append(cmdArgs, c.kargs...)
+	cmdArgs = append(cmdArgs, args...)
+	return c.rai.Command(p, cmdArgs...)
+}
+
+// check for cluster connectivity
+func (c *cluster) check(p *supervisor.Process) error {
+	cmd := c.getKubectlCmd(p, "get", "po", "ohai", "--ignore-not-found")
+	return cmd.Run()
+}
+
+// KCluster tracks connectivity to a cluster
+func KCluster(p *supervisor.Process, args *ConnectArgs) (Resource, error) {
+	c := &cluster{
+		rai:   args.RAI,
+		kargs: args.KArgs,
+		ResourceBase: ResourceBase{
+			name:  "cluster",
+			tasks: make(chan func() error, 1),
+		},
+	}
+	c.doCheck = c.check
+	c.doQuit = func() error { return nil }
+
+	if err := c.check(p); err != nil {
+		return nil, err
+	}
+
+	cmd := c.getKubectlCmd(p, "config", "current-context")
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return nil, err
+	}
+	c.context = strings.TrimSpace(string(output))
+
+	cmd = c.getKubectlCmd(p, "config", "view", "--minify", "-o", "jsonpath={.clusters[0].cluster.server}")
+	output, err = cmd.CombinedOutput()
+	if err != nil {
+		return nil, err
+	}
+	c.server = strings.TrimSpace(string(output))
+
+	p.Supervisor().Supervise(&supervisor.Worker{
+		Name: "cluster",
+		Work: c.processor,
+	})
+
+	return c, nil
 }
 
 // crCmd is a handle to a checked retrying command
