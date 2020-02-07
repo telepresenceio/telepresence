c6d21d32e0c359a5be8d7c60a3487927022a9412
Charlie Catney <charliecatney@hotmail.com>
2020-02-07 22:11:45 +0000

    Remove hostAliases to test.

--- tests/cluster/parameterize_utils.py
+++ tests/cluster/parameterize_utils.py
@@ -406,10 +406,6 @@ def create_deployment(deployment_ident, image, args, environ, ports, replicas):
                     "containers": [container, sidecar_container],
                 },
             },
-            "hostAliases": [{
-                "ip": "127.0.0.1",
-                "hostnames": ["foo.local"]
-            }]
         },
     })
     check_output([KUBECTL, "create", "-f", "-"],
