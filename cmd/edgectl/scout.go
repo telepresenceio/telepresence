231e50200bdf1684ae7f5999a1b86ed0a0148f98
Luke Shumaker <lukeshu@datawire.io>
2020-03-27 13:35:10 -0400

    Add a common pkg/metriton (mimicing scout.py) so we can stop reinventing that wheel
    And add lots of comments so I can stop re-learning that info.

--- cmd/edgectl/scout.go
+++ cmd/edgectl/scout.go
@@ -1,22 +1,17 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"net/http"
-	"os"
-	"path/filepath"
 
 	"github.com/google/uuid"
 	"github.com/pkg/errors"
+
+	"github.com/datawire/ambassador/pkg/metriton"
 )
 
 type Scout struct {
-	installID string
-	index     int
-	metadata  map[string]interface{}
+	index    int
+	reporter *metriton.Reporter
 }
 
 type ScoutMeta struct {
@@ -25,70 +20,37 @@ type ScoutMeta struct {
 }
 
 func NewScout(mode string) (s *Scout) {
-	// Fixed (growing) metadata passed with every report
-	metadata := make(map[string]interface{})
-	metadata["mode"] = mode
-	metadata["trace_id"] = uuid.New().String()
-
-	// The result
-	s = &Scout{metadata: metadata}
-
-	// Get or create the persistent install ID for Edge Control
-	err := func() error {
-		// We store the persistent ID in ~/.config/edgectl/id to be consistent
-		// with Telepresence. We could instead use os.UserConfigDir() as the
-		// root of the destination, which may be the same on Linux, but is
-		// definitely different on MacOS.
-		home, err := os.UserHomeDir()
-		if err != nil {
-			return err
-		}
-		configDir := filepath.Join(home, ".config", "edgectl")
-		if err := os.MkdirAll(configDir, 0755); err != nil {
-			return err
-		}
-		idFilename := filepath.Join(configDir, "id")
-
-		// Try to read an existing install ID
-		if installIDBytes, err := ioutil.ReadFile(idFilename); err == nil {
-			s.installID = string(installIDBytes)
-			// Validate UUID format
-			if _, err := uuid.Parse(s.installID); err == nil {
-				metadata["new_install"] = false
-				return nil
-			} // else the value is not a UUID
-		} // else file doesn't exist, etc.
-
-		// Try to create a new install ID
-		s.installID = uuid.New().String()
-		metadata["new_install"] = true
-		if err := ioutil.WriteFile(idFilename, []byte(s.installID), 0755); err != nil {
-			return err
-		}
-		return nil
-	}()
-	if err != nil {
-		s.installID = "00000000-0000-0000-0000-000000000000"
-		metadata["new_install"] = true
-		metadata["install_id_error"] = err.Error()
+	return &Scout{
+		reporter: &metriton.Reporter{
+			Application: "edgectl",
+			Version:     Version,
+			GetInstallID: func(r *metriton.Reporter) (string, error) {
+				id, err := metriton.InstallIDFromFilesystem(r)
+				if err != nil {
+					id = "00000000-0000-0000-0000-000000000000"
+					r.BaseMetadata["new_install"] = true
+					r.BaseMetadata["install_id_error"] = err.Error()
+				}
+				return id, nil
+			},
+			// Fixed (growing) metadata passed with every report
+			BaseMetadata: map[string]interface{}{
+				"mode":     mode,
+				"trace_id": uuid.New().String(),
+			},
+		},
 	}
-
-	return
 }
 
 func (s *Scout) SetMetadatum(key string, value interface{}) {
-	oldValue, ok := s.metadata[key]
+	oldValue, ok := s.reporter.BaseMetadata[key]
 	if ok {
 		panic(fmt.Sprintf("trying to replace metadata[%q] = %q with %q", key, oldValue, value))
 	}
-	s.metadata[key] = value
+	s.reporter.BaseMetadata[key] = value
 }
 
 func (s *Scout) Report(action string, meta ...ScoutMeta) error {
-	if os.Getenv("SCOUT_DISABLE") != "" {
-		return nil
-	}
-
 	// Construct the report's metadata. Include the fixed (growing) set of
 	// metadata in the Scout structure and the pairs passed as arguments to this
 	// call. Also include and increment the index, which can be used to
@@ -99,33 +61,16 @@ func (s *Scout) Report(action string, meta ...ScoutMeta) error {
 		"action": action,
 		"index":  s.index,
 	}
-	for metaKey, metaValue := range s.metadata {
-		metadata[metaKey] = metaValue
-	}
 	for _, metaItem := range meta {
 		metadata[metaItem.Key] = metaItem.Value
 	}
 
-	data := map[string]interface{}{
-		"application": "edgectl",
-		"install_id":  s.installID,
-		"version":     Version,
-		"metadata":    metadata,
-	}
-
-	body, err := json.MarshalIndent(data, "", "  ")
-	if err != nil {
-		panic(err)
-	}
-	metritonEndpoint := "https://metriton.datawire.io/scout"
-	resp, err := http.Post(metritonEndpoint, "application/json", bytes.NewBuffer(body))
+	_, err := s.reporter.Report(metadata)
 	if err != nil {
 		return errors.Wrap(err, "scout report")
 	}
-
-	// TODO: Do something useful (?) with the response?
-	_, _ = ioutil.ReadAll(resp.Body)
-	_ = resp.Body.Close()
+	// TODO: Do something useful (alert the user if there's an available
+	// upgrade?) with the response (discarded as "_" above)?
 
 	return nil
 }
