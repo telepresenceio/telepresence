9a7400bfc6a284d75e72b6bff85e34ea1cae9f54
Abhay Saxena <ark3@datawire.io>
2020-05-29 14:11:12 -0400

    How about this?

--- ci/build-torsocks.sh
+++ ci/build-torsocks.sh
@@ -8,12 +8,5 @@ if [[ "$OS" == "osx" ]]; then
 fi
 
 if [[ "$OS" == "linux" ]]; then
-    cd /tmp
-    wget https://github.com/dgoulet/torsocks/archive/v2.1.0.tar.gz
-    tar xvfz v2.1.0.tar.gz
-    cd torsocks-2.1.0
-    ./autogen.sh
-    ./configure --prefix=/usr
-    make
-    sudo make install;
+    apt-get -qq install torsocks
 fi
