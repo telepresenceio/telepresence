e6e85a61d9c2882f6c4f304dc8cf63b9928931a8
alex <alex@datawire.io>
2020-08-03 10:04:59 -0400

    path -> Path

--- pkg/edgectl/cluster.go
+++ pkg/edgectl/cluster.go
@@ -204,10 +204,10 @@ func getClusterPreviewHostname(p *supervisor.Process, cluster *KCluster) (hostna
 			continue
 		}
 
-		// missing type, default is "path" --> success
-		// type is present, set to "path" --> success
+		// missing type, default is "Path" --> success
+		// type is present, set to "Path" --> success
 		// otherwise --> failure
-		if pType, ok := previewUrlSpec["type"].(string); ok && pType != "path" {
+		if pType, ok := previewUrlSpec["type"].(string); ok && pType != "Path" {
 			p.Logf(logEntry+": %#v", "unsupported preview URL type", previewUrlSpec["type"])
 			continue
 		}
