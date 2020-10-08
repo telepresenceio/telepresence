f7dff55a9c3cadfaf8ea2a94586519fcf2981097
Thomas Hallgren <thomas@tada.se>
2020-10-08 14:17:31 +0200

    Remove extra newline at end of edgectl version error message

--- pkg/edgectl/client/client.go
+++ pkg/edgectl/client/client.go
@@ -33,7 +33,7 @@ func Version(cmd *cobra.Command, _ []string) error {
 		return nil
 	}
 	if err == daemonIsNotRunning {
-		return fmt.Errorf("Client %s\n%s\n", edgectl.DisplayVersion(), err.Error())
+		return fmt.Errorf("Client %s\n%s", edgectl.DisplayVersion(), err.Error())
 	}
 	// Socket exists but connection failed anyway.
 	return fmt.Errorf("Client %s\nUnable to connect to daemon: %s", edgectl.DisplayVersion(), err.Error())
