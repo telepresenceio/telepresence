1f3eeaf994e7307b4a32d789b405cb7de187313f
Thomas Hallgren <thomas@tada.se>
2020-10-19 14:23:43 +0200

    Move some target defs from config.yml to go.mk

--- build/go.mk
+++ build/go.mk
@@ -1,7 +1,17 @@
+.DEFAULT_GOAL = all
+
 # Ensure that go environment prerequisites are met
 GOPATH=$(shell go env GOPATH)
 GOHOSTOS=$(shell go env GOHOSTOS)
 GOHOSTARCH=$(shell go env GOHOSTARCH)
+
+BINDIR=bin_$(GOHOSTOS)_$(GOHOSTARCH)
+
+RED='\033[1;31m'
+GRN='\033[1;32m'
+BLU='\033[1;34m'
+CYN='\033[1;36m'
+END='\033[0m'
 
 # Install protoc-gen and protoc-gen-go-grpc
 $(GOPATH)/bin/protoc-gen-go $(GOPATH)/bin/protoc-gen-go-grpc:
@@ -17,26 +27,37 @@ pkg/%.pb.go pkg/%_grpc.pb.go: %.proto
 TP_RPC_FILES = pkg/rpc/daemon.pb.go pkg/rpc/daemon_grpc.pb.go
 
 .PHONY: generate
-generate: protoc-tools $(TP_RPC_FILES)
+generate: protoc-tools $(TP_RPC_FILES) ## (Generate) update generated files that get checked in to Git.
 
 .PHONY: generate-clean
-generate-clean:
+generate-clean: ## (Generate) delete generated files that get checked in to Git.
 	rm -rf pkg/rpc/*
 
 # pkg sources excluding rpc
 TP_PKG_SOURCES = $(shell find pkg -type f -name '*.go' | grep -v '/testdata/' | grep -v '_test.go' | grep -v '/rpc/')
 
-bin_$(GOHOSTOS)_$(GOHOSTARCH)/telepresence: main.go $(TP_PKG_SOURCES) $(TP_RPC_FILES)
-	go build -o bin_$(GOHOSTOS)_$(GOHOSTARCH)/telepresence .
+$(BINDIR)/telepresence: main.go $(TP_PKG_SOURCES) $(TP_RPC_FILES)
+	go build -o $(BINDIR)/telepresence .
 
 .PHONY: build
-build: bin_$(GOHOSTOS)_$(GOHOSTARCH)/telepresence
+build: $(BINDIR)/telepresence  ## (Build) runs go build
 
-.phony: test
-test: telepresence
+.phony: clean
+clean: ## (Build) cleans built artefacts
+	rm -rf $(BINDIR)
+
+$(BINDIR)/golangci-lint: ## (Lint) install golangci-lint
+	curl -sfL https://install.goreleaser.com/github.com/golangci/golangci-lint.sh | sh -s -- -b $(BINDIR) latest
+
+.phony: lint
+lint: $(BINDIR)/golangci-lint ## (Lint) runs golangci-lint
+	$(BINDIR)/golangci-lint run ./...
+
+.phony: gotest
+gotest: build  ## (Test) runs go test
 	go test .
 
-define _help.targets
+define targets-string
   $(BLD)$(MAKE) $(BLU)help$(END)            -- displays the main help message.
 
   $(BLD)$(MAKE) $(BLU)targets$(END)         -- displays this message.
@@ -53,3 +74,13 @@ define _help.targets
 
   $(BLD)$(MAKE) $(BLU)clean$(END)           -- runs go test
 endef
+
+.PHONY: all
+all: gotest
+
+.PHONY: help
+help:  ## (Common) Show this message
+	@echo 'Usage: make [TARGETS...]'
+	@echo
+	@echo TARGETS:
+	@sed -En 's/^([^#]*) *:[^#]* *[#]# *(\([^)]*\))? */\2	\1	/p' $(sort $(abspath $(MAKEFILE_LIST))) | sed 's/^	/($(or $(NAME),this project))&/' | column -t -s '	' | sed 's/^/  /' | sort
