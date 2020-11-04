e59905b35816f9c8f694b1450fef8ac6c0a9fc83
Abhay Saxena <ark3@datawire.io>
2020-11-04 11:41:52 -0500

    agent: Configure using environment variables
    Signed-off-by: Abhay Saxena <ark3@datawire.io>

--- cmd/traffic/agent.go
+++ cmd/traffic/agent.go
@@ -8,10 +8,22 @@ import (
 	"syscall"
 
 	"github.com/datawire/ambassador/pkg/dlog"
+	"github.com/sethvargo/go-envconfig"
 	"golang.org/x/sync/errgroup"
 
 	"github.com/datawire/telepresence2/pkg/version"
 )
+
+type Config struct {
+	Name    string `env:"AGENT_NAME,required"`
+	AppPort int    `env:"APP_PORT,required"`
+
+	ManagerAddress   string `env:"MANAGER_ADDRESS,default=traffic-manager:8081"`
+	SshHost          string `env:"SSH_HOST,default=traffic-manager"`
+	SshPort          int    `env:"SSH_PORT,default=8022"`
+	AgentPort        int    `env:"AGENT_PORT,default=9900"`
+	DefaultMechanism string `env:"DEFAULT_MECHANISM,default=tcp"`
+}
 
 func agent_main() {
 	// Set up context with logger
@@ -23,6 +35,25 @@ func agent_main() {
 	}
 
 	dlog.Infof(ctx, "Traffic Agent %s [pid:%d]", version.Version, os.Getpid())
+
+	// Add defaults for development work
+	if os.Getenv("USER") != "" {
+		dlog.Infof(ctx, "Launching in dev mode ($USER is set)")
+		if os.Getenv("AGENT_NAME") == "" {
+			os.Setenv("AGENT_NAME", "test-agent")
+		}
+		if os.Getenv("APP_PORT") == "" {
+			os.Setenv("APP_PORT", "8080")
+		}
+	}
+
+	// Handle configuration
+	config := Config{}
+	if err := envconfig.Process(ctx, &config); err != nil {
+		dlog.Error(ctx, err)
+		os.Exit(1)
+	}
+	dlog.Infof(ctx, "%+v", config)
 
 	// Handle shutdown
 	g.Go(func() error {
