8a4b288482b0274ea206979aab9cf1ad4eccf11d
Thomas Hallgren <thomas@tada.se>
2020-11-28 15:26:49 +0100

    Use $(TOOLSBINDIR)/protoc. MacOS fails to use PATH setting.
    The PATH setting in `tools.mk` doesn't have any effect when using Make
    3.8.1 (the default on MacOS Catalina). This change ensures that the
    correct `protoc` is used.

--- build/go.mk
+++ build/go.mk
@@ -19,7 +19,7 @@ PROTO_SRCS = $(shell echo rpc/*/*.proto)
 .PHONY: generate
 generate: ## (Generate) Update generated files that get checked in to Git
 generate: $(tools/protoc) $(tools/protoc-gen-go) $(tools/protoc-gen-go-grpc)
-	protoc --proto_path=. --go_out=. --go-grpc_out=. --go_opt=module=github.com/datawire/telepresence2 --go-grpc_opt=module=github.com/datawire/telepresence2 $(PROTO_SRCS)
+	$(TOOLSBINDIR)/protoc --proto_path=. --go_out=. --go-grpc_out=. --go_opt=module=github.com/datawire/telepresence2 --go-grpc_opt=module=github.com/datawire/telepresence2 $(PROTO_SRCS)
 	cd ./pkg/rpc && go mod init github.com/datawire/telepresence2/pkg/rpc
 	cd ./pkg/rpc && go mod tidy
 	cd ./pkg/rpc && go mod vendor
