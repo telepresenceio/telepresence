96e80e1bc2203998b8dbe6d726903db67a202362
Thomas Hallgren <thomas@tada.se>
2020-12-18 17:43:44 +0100

    Enable tests to mock the os.UserCacheDir function
    This might be generically useful as we leverage the user cache more and
    want to write tests that don't clobber the real thing.

--- pkg/client/cachedir.go
+++ pkg/client/cachedir.go
@@ -7,10 +7,25 @@ import (
 
 const telepresenceCacheDir = "telepresence"
 
+var ucFunc = os.UserCacheDir
+
+// UserCacheDir will call os.UserCacheDir and return the result unless that function has
+// been overridden by SetUserCacheDirFunc
+func UserCacheDir() (string, error) {
+	return ucFunc()
+}
+
+// SetUserCacheDirFunc overrides the definition of UserCacheDir
+//
+// Intended for test purposes only.
+func SetUserCacheDirFunc(ucf func() (string, error)) {
+	ucFunc = ucf
+}
+
 // CacheDir returns the full path to the directory "telepresence", parented by the directory returned
-// by os.UserCacheDir(). The directory is created if it does not exist.
+// by UserCacheDir(). The directory is created if it does not exist.
 func CacheDir() (string, error) {
-	userCacheDir, err := os.UserCacheDir()
+	userCacheDir, err := UserCacheDir()
 	if err != nil {
 		return "", err
 	}
