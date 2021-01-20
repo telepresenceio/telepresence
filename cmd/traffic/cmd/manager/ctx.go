4e71817f5887bbaa86806328cff84e5c3cb0acdc
Luke Shumaker <lukeshu@datawire.io>
2021-01-20 11:59:39 -0700

    manager: Attach the session ID to the request Context when applicable

---
+++ cmd/traffic/cmd/manager/ctx.go
@@ -0,0 +1,31 @@
+package manager
+
+import (
+	"context"
+
+	"github.com/datawire/dlib/dlog"
+	"github.com/datawire/telepresence2/pkg/rpc/manager"
+)
+
+func WithSessionInfo(ctx context.Context, si *manager.SessionInfo) context.Context {
+	if id := si.GetSessionId(); id != "" {
+		return WithSessionID(ctx, id)
+	}
+	return ctx
+}
+
+func WithSessionID(ctx context.Context, sessionID string) context.Context {
+	ctx = context.WithValue(ctx, sessionContextKey{}, sessionID)
+	ctx = dlog.WithField(ctx, "session_id", sessionID)
+	return ctx
+}
+
+func GetSessionID(ctx context.Context) string {
+	id := ctx.Value(sessionContextKey{})
+	if id == nil {
+		return ""
+	}
+	return id.(string)
+}
+
+type sessionContextKey struct{}
