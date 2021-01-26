3f81ae11372fcba44a913275a21375243f5ea7c3
Abhay Saxena <ark3@email.com>
2021-01-25 20:17:06 -0500

    packaging: Avoid linting failure
    Signed-off-by: Abhay Saxena <ark3@email.com>

--- packaging/distros.py
+++ packaging/distros.py
@@ -30,7 +30,6 @@ distros = [
     ("ubuntu", "xenial", "deb", ubuntu_deps, install_deb),  # 16.04 (LTS2021)
     ("ubuntu", "bionic", "deb", ubuntu_deps_2, install_deb),  # 18.04 (LTS2023)
     ("ubuntu", "focal", "deb", ubuntu_deps_2, install_deb),  # 20.04 (LTS2025)
-    #("ubuntu", "groovy", "deb", ubuntu_deps_2, install_deb),  # 20.10 2021-07 # not yet in packagecloud.io, as of 2021-01-25
     ("debian", "stretch", "deb", ubuntu_deps, install_deb),  # 9
     ("debian", "buster", "deb", ubuntu_deps_2, install_deb),  # 10
     ("fedora", "26", "rpm", fedora_deps, install_rpm),  # EOL 2018-05-29
@@ -40,5 +39,8 @@ distros = [
     ("fedora", "30", "rpm", fedora_deps, install_rpm),  # EOL 2020-05-26
     ("fedora", "31", "rpm", fedora_deps, install_rpm),  # EOL 2020-11-24
     ("fedora", "32", "rpm", fedora_deps, install_rpm),
-    #("fedora", "33", "rpm", fedora_deps, install_rpm), # not yet in packagecloud.io, as of 2021-01-25
 ]
+
+# not yet in packagecloud.io, as of 2021-01-25
+#   ("ubuntu", "groovy", "deb", ubuntu_deps_2, install_deb),  # 20.10 2021-07
+#   ("fedora", "33", "rpm", fedora_deps, install_rpm),
