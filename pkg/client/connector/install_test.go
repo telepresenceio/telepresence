2f10569f0a5e8fdbe7b806357666075ddf9ccb41
Luke Shumaker <lukeshu@datawire.io>
2021-03-08 10:35:12 -0700

    connector: TestAddAgentToDeployment: Use Go templates instead of Bash-ish syntax

--- pkg/client/connector/install_test.go
+++ pkg/client/connector/install_test.go
@@ -1,6 +1,7 @@
 package connector
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"io/ioutil"
@@ -10,6 +11,7 @@ import (
 	"path/filepath"
 	"strings"
 	"testing"
+	"text/template"
 	"time"
 
 	"github.com/pkg/errors"
@@ -269,42 +271,50 @@ func TestAddAgentToDeployment(t *testing.T) {
 		}
 		tcName := strings.TrimSuffix(fi.Name(), ".input.yaml")
 
-		var tmp struct {
-			Deployment *kates.Deployment `json:"deployment"`
-			Service    *kates.Service    `json:"service"`
+		loadFile := func(filename string) (*kates.Deployment, *kates.Service, error) {
+			tmpl, err := template.ParseFiles(filepath.Join("testdata/addAgentToDeployment", filename))
+			if err != nil {
+				return nil, nil, fmt.Errorf("read template: %s: %w", filename, err)
+			}
+
+			var buff bytes.Buffer
+			err = tmpl.Execute(&buff, map[string]interface{}{
+				"Version": strings.TrimPrefix(testVersion, "v"),
+			})
+			if err != nil {
+				return nil, nil, fmt.Errorf("execute template: %s: %w", filename, err)
+			}
+
+			var dat struct {
+				Deployment *kates.Deployment `json:"deployment"`
+				Service    *kates.Service    `json:"service"`
+			}
+			if err := yaml.Unmarshal(buff.Bytes(), &dat); err != nil {
+				return nil, nil, fmt.Errorf("parse yaml: %s: %w", filename, err)
+			}
+
+			return dat.Deployment, dat.Service, nil
 		}
 
 		var tc testcase
+		var err error
 
-		inBody, err := ioutil.ReadFile(filepath.Join("testdata/addAgentToDeployment", fi.Name()))
+		tc.InputDeployment, tc.InputService, err = loadFile(tcName + ".input.yaml")
 		if err != nil {
-			t.Fatalf("%s.input.yaml: %v", tcName, err)
+			t.Fatal(err)
 		}
-		inStr := strings.ReplaceAll(string(inBody), "${TELEPRESENCE_VERSION#v}", strings.TrimPrefix(testVersion, "v"))
-		if err = yaml.Unmarshal([]byte(inStr), &tmp); err != nil {
-			t.Fatalf("%s.input.yaml: %v", tcName, err)
-		}
-		tc.InputDeployment = tmp.Deployment
-		tc.InputService = tmp.Service
-		tmp.Deployment = nil
-		tmp.Service = nil
 
-		outBody, err := ioutil.ReadFile(filepath.Join("testdata/addAgentToDeployment", tcName+".output.yaml"))
+		tc.OutputDeployment, tc.OutputService, err = loadFile(tcName + ".output.yaml")
 		if err != nil {
-			t.Fatalf("%s.output.yaml: %v", tcName, err)
+			t.Fatal(err)
 		}
-		outStr := strings.ReplaceAll(string(outBody), "${TELEPRESENCE_VERSION#v}", strings.TrimPrefix(testVersion, "v"))
-		if err = yaml.Unmarshal([]byte(outStr), &tmp); err != nil {
-			t.Fatalf("%s.output.yaml: %v", tcName, err)
-		}
-		tc.OutputDeployment = tmp.Deployment
-		tc.OutputService = tmp.Service
 
 		// If it is a test case for a service with multiple ports,
 		// we need to specify the name of the port we want to intercept
 		if strings.Contains(tcName, "mp-tc") {
 			tc.InputPortName = "https"
 		}
+
 		testcases[tcName] = tc
 	}
 
