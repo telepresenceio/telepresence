f61716a6ee83249fc572b7a6a8763c24a1f67098
mattmcclure-dw <73489204+mattmcclure-dw@users.noreply.github.com>
2021-03-11 10:02:05 -0500

    (from getambassador.io) more fixes

--- quick-start/qs-cards.js
+++ quick-start/qs-cards.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
-import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles((theme) => ({
