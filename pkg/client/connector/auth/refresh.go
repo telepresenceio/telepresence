0610ed4920a24ffcdf840bccb3ba460102268bb5
Luke Shumaker <lukeshu@datawire.io>
2021-04-28 15:46:40 -0600

    connector: Don't keep retrying if the access/refresh tokens are expired
    Signed-off-by: Luke Shumaker <lukeshu@datawire.io>

--- pkg/client/connector/auth/refresh.go
+++ pkg/client/connector/auth/refresh.go
@@ -6,21 +6,31 @@ import (
 	"golang.org/x/oauth2"
 )
 
+// cbTokenSource is an oauth2.TokenSource that calls a callback whenever a new token is issued or a
+// new token fails to be issued.
 type cbTokenSource struct {
+	// Static
 	inner oauth2.TokenSource
-	cb    func(context.Context, *oauth2.Token) error
-	ctx   context.Context
+	ctx   context.Context // to pass to the callbacks
+	tokCB func(context.Context, *oauth2.Token) error
+	errCB func(context.Context, error) error
 
+	// Stateful
 	lastToken string
 }
 
+// Token implements oauth2.TokenSource.
 func (ts *cbTokenSource) Token() (*oauth2.Token, error) {
 	ret, err := ts.inner.Token()
+	if (ret != nil) == (err != nil) {
+		panic("TokenSource is broken")
+	}
 	if err != nil {
+		err = ts.errCB(ts.ctx, err)
 		return nil, err
 	}
 	if ret.AccessToken != ts.lastToken {
-		if err := ts.cb(ts.ctx, ret); err != nil {
+		if err := ts.tokCB(ts.ctx, ret); err != nil {
 			return nil, err
 		}
 	}
@@ -28,11 +38,21 @@ func (ts *cbTokenSource) Token() (*oauth2.Token, error) {
 	return ret, nil
 }
 
-func newTokenSource(ctx context.Context, cfg oauth2.Config, cur *oauth2.Token, cb func(context.Context, *oauth2.Token) error) oauth2.TokenSource {
+// newTokenSource returns a new cbTokenSource.
+func newTokenSource(
+	// Static
+	ctx context.Context,
+	cfg oauth2.Config,
+	tokCB func(context.Context, *oauth2.Token) error,
+	errCB func(context.Context, error) error,
+	// The initial token, must not be nil
+	cur *oauth2.Token,
+) oauth2.TokenSource {
 	return &cbTokenSource{
 		inner: cfg.TokenSource(ctx, cur),
-		cb:    cb,
 		ctx:   ctx,
+		tokCB: tokCB,
+		errCB: errCB,
 
 		lastToken: cur.AccessToken,
 	}
