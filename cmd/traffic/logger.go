82ce9023cf5a8b91f421a786b8a3836f696e35ec
Jose Cortes <josecortes@datawire.io>
2021-06-08 17:57:56 -0400

    Fix race between reading from the cluster and closing the connection
    Basically when transferring a large file from the cluster, sometimes the
    daemon would process a FIN from the remote end before all packets that
    came before the FIN had been routed.
    
    Signed-off-by: Jose Cortes <josecortes@datawire.io>

--- cmd/traffic/logger.go
+++ cmd/traffic/logger.go
@@ -12,11 +12,11 @@ import (
 func makeBaseLogger() dlog.Logger {
 	logrusLogger := logrus.New()
 	logrusFormatter := &logrus.TextFormatter{
-		TimestampFormat: "2006-01-02 15:04:05",
+		TimestampFormat: "2006-01-02 15:04:05.0000",
 		FullTimestamp:   true,
 	}
 	logrusLogger.SetFormatter(logrusFormatter)
-	logrusLogger.SetReportCaller(false)
+	logrusLogger.SetReportCaller(true)
 
 	const defaultLogLevel = logrus.InfoLevel
 
