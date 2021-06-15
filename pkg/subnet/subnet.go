c36d8caf0e4f72bbe793defc67b7f0ebc8dc8f31
Thomas Hallgren <thomas@datawire.io>
2021-06-15 16:53:35 +0200

    Remove things from the subnet package that are not used.
    Signed-off-by: Thomas Hallgren <thomas@datawire.io>

--- pkg/subnet/subnet.go
+++ pkg/subnet/subnet.go
@@ -3,19 +3,9 @@ package subnet
 
 import (
 	"bytes"
-	"errors"
-	"fmt"
 	"net"
 	"sort"
 )
-
-type ipAndNetwork struct {
-	ip      net.IP
-	network *net.IPNet
-}
-
-// stubbable version
-var interfaceAddrs = net.InterfaceAddrs
 
 // CoveringCIDRs returns the ip networks needed to cover the given IPs with as
 // big mask as possible for each subnet. The analyze starts by finding all
@@ -98,54 +88,6 @@ func CoveringCIDRs(ips []net.IP) []*net.IPNet {
 	return subnets
 }
 
-// FindAvailableClassC returns the first class C subnet CIDR in the address ranges reserved
-// for private (non-routed) use that isn't in use by an existing network interface.
-func FindAvailableClassC() (*net.IPNet, error) {
-	addrs, err := interfaceAddrs()
-	if err != nil {
-		return nil, fmt.Errorf("failed to obtain interface addresses: %w", err)
-	}
-
-	cidrs := make([]*ipAndNetwork, 0, len(addrs))
-	for _, a := range addrs {
-		if ip, network, err := net.ParseCIDR(a.String()); err == nil {
-			cidrs = append(cidrs, &ipAndNetwork{ip: ip, network: network})
-		}
-	}
-	for i := 0; i < 256; i++ {
-		if found := findChunk(cidrs, 10, i); found >= 0 {
-			return cidr24(10, i, found), nil
-		}
-	}
-	for i := 16; i < 32; i++ {
-		if found := findChunk(cidrs, 17, i); found >= 0 {
-			return cidr24(17, i, found), nil
-		}
-	}
-	if found := findChunk(cidrs, 192, 168); found >= 0 {
-		return cidr24(192, 168, found), nil
-	}
-	return nil, errors.New("no available CIDR")
-}
-
-func Available(subnet *net.IPNet) (bool, error) {
-	addrs, err := interfaceAddrs()
-	if err != nil {
-		return false, fmt.Errorf("failed to obtain interface addresses: %w", err)
-	}
-
-	for _, a := range addrs {
-		_, network, err := net.ParseCIDR(a.String())
-		if err != nil {
-			return false, fmt.Errorf("failed to parse interface address: %w", err)
-		}
-		if Covers(network, subnet) {
-			return false, nil
-		}
-	}
-	return true, nil
-}
-
 // compareIPs is like bytes.Compare but will always consider IPv4 less than IPv6.
 func compareIPs(a, b net.IP) int {
 	dl := len(a) - len(b)
@@ -158,25 +100,6 @@ func compareIPs(a, b net.IP) int {
 		dl = 1
 	}
 	return dl
-}
-
-func findChunk(cidrs []*ipAndNetwork, ar1, ar2 int) int {
-	_, wantedRange, err := net.ParseCIDR(fmt.Sprintf("%d.%d.0.0/16", ar1, ar2))
-	if err != nil {
-		panic(err)
-	}
-	return findAvailableChunk(wantedRange, cidrs)
-}
-
-func cidr24(ar1, ar2, ar3 int) *net.IPNet {
-	ip := make(net.IP, 4)
-	ip[0] = byte(ar1)
-	ip[1] = byte(ar2)
-	ip[2] = byte(ar3)
-	return &net.IPNet{
-		IP:   ip,
-		Mask: net.CIDRMask(24, 32),
-	}
 }
 
 // Covers answers the question if network range a contains all of network range b
@@ -203,36 +126,4 @@ func Covers(a, b *net.IPNet) bool {
 		}
 	}
 	return a.Contains(m)
-}
-
-func findAvailableChunk(wantedRange *net.IPNet, cidrs []*ipAndNetwork) int {
-	inUse := [256]bool{}
-	for _, cid := range cidrs {
-		if Covers(cid.network, wantedRange) {
-			return -1
-		}
-		if !wantedRange.Contains(cid.ip) {
-			continue
-		}
-		ones, bits := cid.network.Mask.Size()
-		if bits != 32 {
-			return -1
-		}
-		if ones >= 24 {
-			inUse[cid.network.IP[2]] = true
-		} else {
-			ones -= 16
-			mask := 0xff >> ones
-			for i := 0; i <= mask; i++ {
-				inUse[i] = true
-			}
-		}
-	}
-
-	for i := 0; i < 256; i++ {
-		if !inUse[i] {
-			return i
-		}
-	}
-	return -1
 }
