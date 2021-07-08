ca5ac5120a4d3893d7fcb86c7b0aea05ef705276
Luke Shumaker <lukeshu@datawire.io>
2021-07-08 11:55:23 -0600

    grpctun: Add a comment on what Wait is for
    Signed-off-by: Luke Shumaker <lukeshu@datawire.io>

--- pkg/systema/grpctun/common.go
+++ pkg/systema/grpctun/common.go
@@ -7,6 +7,12 @@ import (
 // Conn is a net.Conn, plus a Wait() method.
 type Conn interface {
 	net.Conn
+
+	// Wait until either the connection is Close()d, or one of Read() or Write() encounters an
+	// error (*not* counting errors caused by deadlines).  If this returns because Close() was
+	// called, nil is returned; otherwise the triggering error is returned.
+	//
+	// Essentially: Wait until the connection is finished.
 	Wait() error
 }
 
