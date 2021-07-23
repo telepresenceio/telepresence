d4323e4fae61384d32889d1b338d966ea2da7825
Luke Shumaker <lukeshu@datawire.io>
2021-07-23 17:50:29 -0600

    (from getambassador.io) docs/telepresence: Use the @src alias

--- quick-start/qs-tabs.js
+++ quick-start/qs-tabs.js
@@ -6,10 +6,10 @@ import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Box from '@material-ui/core/Box';
 import HubspotForm from 'react-hubspot-form';
-import CodeBlock from '../../../../../src/components/CodeBlock';
-import LinuxIcon from '../../../../../src/assets/icons/linux.inline.svg';
-import AppleIcon from '../../../../../src/assets/icons/apple.inline.svg';
-import WindowsIcon from '../../../../../src/assets/icons/windows.inline.svg';
+import CodeBlock from '@src/components/CodeBlock';
+import LinuxIcon from '@src/assets/icons/linux.inline.svg';
+import AppleIcon from '@src/assets/icons/apple.inline.svg';
+import WindowsIcon from '@src/assets/icons/windows.inline.svg';
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
