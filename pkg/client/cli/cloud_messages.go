b5e6e60192a08f79fcbf7c3ebe2dbf8490d415dc
Donny Yung <donaldyung@datawire.io>
2021-07-26 18:09:46 -0400

    Add Ambassador Cloud RPC
    This is adding the actual call to Ambassador Cloud. The RPC has not been
    added to Ambassador Cloud at this point, so it won't fully work yet.
    
    Signed-off-by: Donny Yung <donaldyung@datawire.io>

--- pkg/client/cli/cloud_messages.go
+++ pkg/client/cli/cloud_messages.go
@@ -2,17 +2,25 @@ package cli
 
 import (
 	"context"
+	"crypto/tls"
 	"fmt"
+	"net/url"
 	"os"
 	"strings"
 	"time"
 
-	"github.com/datawire/dlib/dtime"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	empty "google.golang.org/protobuf/types/known/emptypb"
 
 	"github.com/spf13/cobra"
 
+	"github.com/datawire/dlib/dtime"
+
 	"github.com/telepresenceio/telepresence/rpc/v2/systema"
+	"github.com/telepresenceio/telepresence/v2/pkg/client"
 	"github.com/telepresenceio/telepresence/v2/pkg/client/cache"
+	"github.com/telepresenceio/telepresence/v2/pkg/client/cli/cliutil"
 )
 
 const messagesCacheFilename = "cloud-messages.json"
@@ -43,29 +51,64 @@ func newCloudMessageCache(ctx context.Context) (*cloudMessageCache, error) {
 // For now this function will just return a fake result from the cloud
 // this will be replaced with the code to talk to Ambassador Cloud
 // via GetUnauthenticatedCommandMessages in a later commit
-func (cmc *cloudMessageCache) getCloudMessages() error {
-	msgResponse := systema.CommandMessageResponse{
-		Intercept: "Use `login` to connect to Ambassador Cloud and perform selective intercepts.",
+func (cmc *cloudMessageCache) getCloudMessages(ctx context.Context, systemaURL string) error {
+	u, err := url.Parse(systemaURL)
+	if err != nil {
+		return err
 	}
-	cmc.Intercept = msgResponse.GetIntercept()
+	conn, err := grpc.DialContext(ctx,
+		(&url.URL{Scheme: "dns", Path: "/" + u.Host}).String(),
+		grpc.WithTransportCredentials(credentials.NewTLS(&tls.Config{ServerName: u.Hostname()})))
+	if err != nil {
+		return err
+	}
+
+	systemaClient := systema.NewSystemACliClient(conn)
+	resp, err := systemaClient.GetUnauthenticatedCommandMessages(ctx, &empty.Empty{})
+	if err != nil {
+		/*
+			we can uncomment this to 'fake' it while we wait for Ambassador Cloud
+			to implement this RPC
+
+			msgResponse := systema.CommandMessageResponse{
+				Intercept: "Use `login` to connect to Ambassador Cloud and perform selective intercepts.",
+			}
+			cmc.Intercept = msgResponse.GetIntercept()
+		*/
+		return err
+	}
+	cmc.Intercept = resp.GetIntercept()
 	return nil
 }
 
 // raiseCloudMessage is what is called from `PostRunE` in a command and is responsible
 // for raising the message for the command used.
 func raiseCloudMessage(cmd *cobra.Command, _ []string) error {
+	ctx := cmd.Context()
+	// Currently we only have messages that should be served when a user
+	// isn't logged in, so we check that here
+	if cliutil.HasLoggedIn(cmd.Context()) {
+		if _, err := cliutil.GetCloudUserInfo(ctx, false, true); err == nil {
+			return nil
+		}
+	}
 	// The command is the first word of cmd.Use
 	cmdUsed := strings.Split(cmd.Use, " ")[0]
 
 	// Load the config
-	cmc, err := newCloudMessageCache(cmd.Context())
+	cmc, err := newCloudMessageCache(ctx)
 	if err != nil {
 		return err
 	}
 
 	// Check if it is time to get new messages from Ambassador Cloud
 	if dtime.Now().After(cmc.NextCheck) {
-		err := cmc.getCloudMessages()
+		env, err := client.LoadEnv(ctx)
+		if err != nil {
+			return err
+		}
+		systemaURL := fmt.Sprintf("https://%s:%s", env.SystemAHost, env.SystemAPort)
+		err = cmc.getCloudMessages(ctx, systemaURL)
 		if err != nil {
 			// We try again in an hour since we encountered an error
 			cmc.NextCheck = dtime.Now().Add(1 * time.Hour)
@@ -100,7 +143,7 @@ func raiseCloudMessage(cmd *cobra.Command, _ []string) error {
 	}
 	cmc.MessagesDelivered[cmdUsed] = true
 	fmt.Fprintf(cmd.OutOrStdout(), "%s", msg)
-	_ = cache.SaveToUserCache(cmd.Context(), cmc, messagesCacheFilename)
+	_ = cache.SaveToUserCache(ctx, cmc, messagesCacheFilename)
 
 	return nil
 }
