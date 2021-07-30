cf41604465a2958e97e965757e248ad89e391cff
Donny Yung <donaldyung@datawire.io>
2021-07-30 10:05:31 -0400

    PR feedback
    Signed-off-by: Donny Yung <donaldyung@datawire.io>

--- packaging/install-telepresence.ps1
+++ packaging/install-telepresence.ps1
@@ -2,4 +2,5 @@ $current_directory = (pwd).path
 echo $current_directory
 Start-Process msiexec -Wait -verb runAs -Args "/i $current_directory\winfsp.msi /passive /qn /L*V winfsp-install.log"
 Start-Process msiexec -Wait -verb runAs -Args "/i $current_directory\sshfs-win.msi /passive /qn /L*V sshfs-win-install.log"
-[Environment]::SetEnvironmentVariable("Path", "C:\\;C:\\Program Files\\SSHFS-Win\\bin;$ENV:Path", "Machine")
+# We update the PATH to include telepresence and its dependency, sshfs-win
+[Environment]::SetEnvironmentVariable("Path", "C:\telepresence;C:\\Program Files\\SSHFS-Win\\bin;$ENV:Path")
--- packaging/windows-package.sh
+++ packaging/windows-package.sh
@@ -12,6 +12,9 @@ then
    exit 1
 fi
 
+WINFSP_VERSION=1.9.21096
+SSHFS_WIN_VERSION=3.7.21011
+WINTUN_VERSION=0.12
 BINDIR="${BINDIR:-./build-output/bin}"
 
 ZIPDIR="${ZIPDIR:-./telepresence-windows}"
@@ -23,11 +26,11 @@ if [[ ! "${ZIPDIR}" ]]; then
 fi
 
 # Download sshfs-win.msi + winfsp.msi
-curl -L -o "${ZIPDIR}"/winfsp.msi https://github.com/billziss-gh/winfsp/releases/download/v1.9/winfsp-1.9.21096.msi
-curl -L -o "${ZIPDIR}"/sshfs-win.msi https://github.com/billziss-gh/sshfs-win/releases/download/v3.7.21011/sshfs-win-3.7.21011-x64.msi
+curl -L -o "${ZIPDIR}"/winfsp.msi "https://github.com/billziss-gh/winfsp/releases/download/v1.9/winfsp-${WINFSP_VERSION}.msi"
+curl -L -o "${ZIPDIR}"/sshfs-win.msi "https://github.com/billziss-gh/sshfs-win/releases/download/v${SSHFS_WIN_VERSION}/sshfs-win-${SSHFS_WIN_VERSION}-x64.msi"
 
 # Download wintun
-curl -L https://www.wintun.net/builds/wintun-0.12.zip | tar -vf - -Ox wintun/bin/amd64/wintun.dll > "${ZIPDIR}/wintun.dll"
+curl -L "https://www.wintun.net/builds/wintun-${WINTUN_VERSION}.zip" | tar -vf - -Ox wintun/bin/amd64/wintun.dll > "${ZIPDIR}/wintun.dll"
 cp "${BINDIR}/telepresence.exe" "${ZIPDIR}/telepresence.exe"
 
 # Copy powershell install script into $ZIPDIR
