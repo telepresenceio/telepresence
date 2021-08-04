c80451b90d4948552ff1130868005270cc4db579
Jose Cortes <josecortes@datawire.io>
2021-08-04 13:59:20 -0400

    Link to the windows nightly build
    Signed-off-by: Jose Cortes <josecortes@datawire.io>

--- install/nightly-version-tabs.js
+++ install/nightly-version-tabs.js
@@ -63,6 +63,7 @@ export default function SimpleTabs() {
         <Tabs value={value} onChange={handleChange} aria-label="operating system tabs">
           <Tab icon={<AppleIcon />} label="macOS" {...a11yProps(0)} style={{ minWidth: "10%", textTransform: 'none' }} />
           <Tab icon={<LinuxIcon />} label="Linux" {...a11yProps(1)} style={{ minWidth: "10%", textTransform: 'none' }} />
+          <Tab icon={<WindowsIcon />} label="Windows" {...a11yProps(2)} style={{ minWidth: "10%", textTransform: 'none' }} />
         </Tabs>
       </AppBar>
       <TabPanel value={value} index={0}>
@@ -76,6 +77,13 @@ export default function SimpleTabs() {
         <CodeBlock>
         {
           'https://app.getambassador.io/download/tel2/linux/amd64/nightly/telepresence'
+        }
+        </CodeBlock>
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        <CodeBlock>
+        {
+          'https://app.getambassador.io/download/tel2/windows/amd64/nightly/telepresence'
         }
         </CodeBlock>
       </TabPanel>
