5b11b470e96dfd07005dcd04f4a273a51404659f
Donny Yung <donaldyung@datawire.io>
2021-08-04 14:32:52 -0400

    Update *-tabs to include developer preview
    Signed-off-by: Donny Yung <donaldyung@datawire.io>

--- quick-start/qs-tabs.js
+++ quick-start/qs-tabs.js
@@ -102,6 +102,8 @@ export default function SimpleTabs() {
       <TabPanel value={value} index={2}>
         <CodeBlock>
         {
+          '# Windows is in Developer Preview, here is how you can install it:' +
+          '\n' +
           '# Make sure you run the following from Powershell as Administrator' +  
           '\n' +
           '# 1. Download the latest windows zip containing telepresence.exe and its dependencies (~50 MB):' +
