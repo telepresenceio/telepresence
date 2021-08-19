0659a9045c0020a1131cf9dd26ff8886e2a0c6e8
Donny Yung <donaldyung@datawire.io>
2021-08-19 12:31:30 -0400

    Update tests for airgap
    Now that the SYSTEMA_* values come from the config.yml, we set them to
    be localhost during the tests to ensure OSS Telepresence doesn't depend
    on any extensions to function.
    
    Signed-off-by: Donny Yung <donaldyung@datawire.io>

--- pkg/client/cli/telepresence_test.go
+++ pkg/client/cli/telepresence_test.go
@@ -232,7 +232,15 @@ func (ts *telepresenceSuite) TestA_WithNoDaemonRunning() {
 
 		ctx := dlog.NewTestContext(t, false)
 		registry := dtest.DockerRegistry(ctx)
-		configYml := fmt.Sprintf("logLevels:\n  rootDaemon: debug\nimages:\n  registry: %s\n", registry)
+		configYml := fmt.Sprintf(`
+logLevels:
+  rootDaemon: debug
+images:
+  registry: %[1]s
+  webhookRegistry: %[1]s
+cloud:
+  systemaHost: 127.0.0.1
+`, registry)
 		ctx, err := client.SetConfig(ctx, configDir, configYml)
 		require.NoError(err)
 
@@ -298,7 +306,15 @@ func (ts *telepresenceSuite) TestA_WithNoDaemonRunning() {
 
 		ctx := dlog.NewTestContext(t, false)
 		registry := dtest.DockerRegistry(ctx)
-		configYml := fmt.Sprintf("logLevels:\n  rootDaemon: debug\nimages:\n  registry: %s\n", registry)
+		configYml := fmt.Sprintf(`
+logLevels:
+  rootDaemon: debug
+images:
+  registry: %[1]s
+  webhookRegistry: %[1]s
+cloud:
+  systemaHost: 127.0.0.1
+`, registry)
 		ctx, err = client.SetConfig(ctx, tmpDir, configYml)
 		require.NoError(err)
 
@@ -352,7 +368,15 @@ func (ts *telepresenceSuite) TestA_WithNoDaemonRunning() {
 		// latter that is used in the traffic-manager
 		ctx := dlog.NewTestContext(t, false)
 		registry := dtest.DockerRegistry(ctx)
-		configYml := fmt.Sprintf("images:\n  registry: %s\n  agentImage: notUsed:0.0.1\n  webhookAgentImage: imageFromConfig:0.0.1\n  webhookRegistry: %s", registry, registry)
+		configYml := fmt.Sprintf(`
+images:
+  registry: %[1]s
+  agentImage: notUsed:0.0.1
+  webhookRegistry: %[1]s
+  webhookAgentImage: imageFromConfig:0.0.1
+cloud:
+  systemaHost: 127.0.0.1
+`, registry)
 		ctx, err := client.SetConfig(ctx, configDir, configYml)
 		require.NoError(err)
 
@@ -452,6 +476,8 @@ timeouts:
   trafficManagerAPI: 120s
 grpc:
   maxReceiveSize: 10Mi
+cloud:
+  systemaHost: 127.0.0.1
 `, registry)
 	configYml = strings.TrimSpace(configYml)
 	c, err := client.SetConfig(c, configDir, configYml)
@@ -1428,6 +1454,8 @@ func (hs *helmSuite) helmInstall(ctx context.Context, managerNamespace string, a
 		"--set", fmt.Sprintf("image.tag=%s", hs.tpSuite.testVersion[1:]),
 		"--set", fmt.Sprintf("clientRbac.namespaces={%s}", strings.Join(append(appNamespaces, managerNamespace), ",")),
 		"--set", fmt.Sprintf("managerRbac.namespaces={%s}", strings.Join(append(appNamespaces, managerNamespace), ",")),
+		// We don't want the tests or telepresence to depend on an extension host resolving, so we set it to localhost.
+		"--set", "systemaHost=127.0.0.1",
 		"-f", helmValues,
 	)
 	if err == nil {
