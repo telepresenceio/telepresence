8cc2e24792d5000c24a5c91ad66320f2201ec0b3
Luke Shumaker <lukeshu@datawire.io>
2021-09-04 14:55:27 -0600

    pkg/client/logging: Disolve stat_unix.go in to stat_{darwin,linux}.go
    Signed-off-by: Luke Shumaker <lukeshu@datawire.io>

--- pkg/client/logging/stat_darwin.go
+++ pkg/client/logging/stat_darwin.go
@@ -1,8 +1,32 @@
 package logging
 
 import (
+	"fmt"
+	"os"
+	"syscall"
 	"time"
 )
+
+type fileInfo struct {
+	*syscall.Stat_t
+}
+
+func GetSysInfo(_ string, info os.FileInfo) (SysInfo, error) {
+	return fileInfo{info.Sys().(*syscall.Stat_t)}, nil
+}
+
+func (u fileInfo) SetOwnerAndGroup(name string) error {
+	return os.Chown(name, int(u.Uid), int(u.Gid))
+}
+
+func (u fileInfo) HaveSameOwnerAndGroup(other SysInfo) bool {
+	ou := other.(fileInfo)
+	return u.Uid == ou.Uid && u.Gid == ou.Gid
+}
+
+func (u fileInfo) String() string {
+	return fmt.Sprintf("CTIME %v, UID %d, GID %d", u.BirthTime(), u.Uid, u.Gid)
+}
 
 func (u fileInfo) BirthTime() time.Time {
 	sec, nsec := u.Birthtimespec.Unix()
