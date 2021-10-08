722491c62f31cade66557898a2f8311d66c684a4
Thomas Hallgren <thomas@datawire.io>
2021-10-08 09:28:27 +0200

    Add connection based Stream implementation
    Adds the Stream implementation that is used on top of by the
    manager.Tunnel and it's `BidiPipe` and `Dialer` endpoints.
    
    Signed-off-by: Thomas Hallgren <thomas@datawire.io>

--- pkg/tunnel/stream.go
+++ pkg/tunnel/stream.go
@@ -1,6 +1,14 @@
 package tunnel
 
 import (
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"net"
+	"time"
+
+	"github.com/datawire/dlib/dlog"
 	rpc "github.com/telepresenceio/telepresence/rpc/v2/manager"
 )
 
@@ -9,9 +17,187 @@ import (
 //   1 used MuxTunnel instead of one tunnel per connection.
 const Version = uint16(2)
 
+// Endpoint is an endpoint for a Stream such as a Dialer or a bidirectional pipe.
+type Endpoint interface {
+	Start(ctx context.Context)
+	Done() <-chan struct{}
+}
+
 // GRPCStream is the bare minimum needed for reading and writing TunnelMessages
 // on a Manager_TunnelServer or Manager_TunnelClient.
 type GRPCStream interface {
 	Recv() (*rpc.TunnelMessage, error)
 	Send(*rpc.TunnelMessage) error
+}
+
+// The Stream interface represents a bidirectional, synchronized connection Tunnel
+// that sends TCP or UDP traffic over gRPC using manager.TunnelMessage messages.
+//
+// A Stream is closed by one of six things happening at either end (or at both ends).
+//
+//   1. Read from local connection fails (typically EOF)
+//   2. Write to local connection fails (connection peer closed)
+//   3. Idle timer timed out.
+//   4. Context is cancelled.
+//   5. closeSend request received from Tunnel peer.
+//   6. Disconnect received from Tunnel peer.
+//
+// When #1 or #2 happens, the Stream will either call CloseSend() (if it's a client Stream)
+// or send a closeSend request (if it's a StreamServer) to its Stream peer, shorten the
+// Idle timer, and then continue to serve incoming data from the Stream peer until it's
+// closed or a Disconnect is received. Once that happens, it's guaranteed that the Tunnel
+// peer will send no more messages and the Stream is closed.
+//
+// When #3, #4, or #5 happens, the Tunnel will send a Disconnect to its Stream peer and close.
+//
+// When #6 happens, the Stream will simply close.
+type Stream interface {
+	Tag() string
+	ID() ConnID
+	Receive(context.Context) (Message, error)
+	Send(context.Context, Message) error
+	CloseSend(ctx context.Context) error
+	PeerVersion() uint16
+	SessionID() string
+	DialTimeout() time.Duration
+	RoundtripLatency() time.Duration
+}
+
+// ReadLoop reads from the Stream and dispatches messages and error to the give channels. There
+// will be max one error since the error also terminates the loop.
+func ReadLoop(ctx context.Context, s Stream) (<-chan Message, <-chan error) {
+	msgCh := make(chan Message)
+	errCh := make(chan error)
+	dlog.Debugf(ctx, "   %s %s, ReadLoop starting", s.Tag(), s.ID())
+	go func() {
+		defer func() {
+			dlog.Debugf(ctx, "   %s %s, ReadLoop ended", s.Tag(), s.ID())
+		}()
+
+		for ctx.Err() == nil {
+			m, err := s.Receive(ctx)
+			if err != nil {
+				close(msgCh) // Must close before posting the error to avoid potential deadlock
+				if ctx.Err() == nil && !(errors.Is(err, io.EOF) || errors.Is(err, net.ErrClosed)) {
+					errCh <- fmt.Errorf("!! %s %s, read from grpc.ClientStream failed", s.Tag(), s.ID())
+				}
+				return
+			}
+			select {
+			case <-ctx.Done():
+				close(msgCh)
+				return
+			case msgCh <- m:
+			}
+		}
+	}()
+	return msgCh, errCh
+}
+
+// WriteLoop reads messages from the channel and writes them to the Stream. It will call CloseSend() on the
+// stream when the channel is closed.
+func WriteLoop(ctx context.Context, s Stream, msgCh <-chan Message) {
+	dlog.Debugf(ctx, "   %s %s, WriteLoop starting", s.Tag(), s.ID())
+	go func() {
+		defer func() {
+			dlog.Debugf(ctx, "   %s %s, WriteLoop ended", s.Tag(), s.ID())
+			if err := s.CloseSend(ctx); err != nil {
+				dlog.Errorf(ctx, "!! %s %s, CloseSend failed: %v", s.Tag(), s.ID(), err)
+			}
+		}()
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case m := <-msgCh:
+				if m == nil {
+					return
+				}
+				if err := s.Send(ctx, m); err != nil {
+					if !errors.Is(err, net.ErrClosed) {
+						dlog.Errorf(ctx, "!! %s %s, Send failed: %v", s.Tag(), s.ID(), err)
+					}
+					return
+				}
+			}
+		}
+	}()
+}
+
+type stream struct {
+	grpcStream       GRPCStream
+	id               ConnID
+	dialTimeout      time.Duration
+	roundtripLatency time.Duration
+	sessionID        string
+	tag              string
+	syncRatio        uint32 // send and check sync after each syncRatio message
+	ackWindow        uint32 // maximum permitted difference between sent and received ack
+	peerVersion      uint16
+}
+
+func newStream(tag string, grpcStream GRPCStream) stream {
+	return stream{tag: tag, grpcStream: grpcStream, syncRatio: 8, ackWindow: 1}
+}
+
+func (s *stream) Tag() string {
+	return s.tag
+}
+
+func (s *stream) ID() ConnID {
+	return s.id
+}
+
+func (s *stream) PeerVersion() uint16 {
+	return s.peerVersion
+}
+
+func (s *stream) DialTimeout() time.Duration {
+	return s.dialTimeout
+}
+
+func (s *stream) RoundtripLatency() time.Duration {
+	return s.roundtripLatency
+}
+
+func (s *stream) SessionID() string {
+	return s.sessionID
+}
+
+func (s *stream) Receive(ctx context.Context) (Message, error) {
+	cm, err := s.grpcStream.Recv()
+	if err != nil {
+		return nil, err
+	}
+	m := msg(cm.Payload)
+	switch m.Code() {
+	case closeSend:
+		dlog.Tracef(ctx, "<- %s %s, close send", s.tag, s.id)
+		return nil, net.ErrClosed
+	case streamInfo:
+		dlog.Tracef(ctx, "<- %s, %s", s.tag, m)
+	default:
+		dlog.Tracef(ctx, "<- %s %s, %s", s.tag, s.id, m)
+	}
+	return m, nil
+}
+
+func (s *stream) Send(ctx context.Context, m Message) error {
+	if err := s.grpcStream.Send(m.TunnelMessage()); err != nil {
+		if ctx.Err() == nil && !errors.Is(err, net.ErrClosed) {
+			dlog.Errorf(ctx, "!! %s %s, Send failed: %v", s.tag, s.id, err)
+		}
+		return err
+	}
+	dlog.Tracef(ctx, "-> %s %s, %s", s.tag, s.id, m)
+	return nil
+}
+
+func (s *stream) CloseSend(ctx context.Context) error {
+	if err := s.Send(ctx, NewMessage(closeSend, nil)); err != nil {
+		if ctx.Err() == nil && !(errors.Is(err, io.EOF) || errors.Is(err, net.ErrClosed)) {
+			return fmt.Errorf("send of closeSend message failed: %w", err)
+		}
+	}
+	return nil
 }
---
+++ pkg/tunnel/stream_test.go
@@ -0,0 +1,330 @@
+package tunnel
+
+import (
+	"bytes"
+	"context"
+	"errors"
+	"fmt"
+	"net"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/datawire/dlib/dlog"
+	"github.com/telepresenceio/telepresence/rpc/v2/manager"
+	"github.com/telepresenceio/telepresence/v2/pkg/ipproto"
+	"github.com/telepresenceio/telepresence/v2/pkg/iputil"
+	"github.com/telepresenceio/telepresence/v2/pkg/log"
+)
+
+type uni struct {
+	done <-chan struct{}
+	ch   chan *manager.TunnelMessage
+}
+
+type bidi struct {
+	cToS *uni
+	sToC *uni
+}
+
+func newUni(bufSize int, done <-chan struct{}) *uni {
+	return &uni{ch: make(chan *manager.TunnelMessage, bufSize), done: done}
+}
+
+func newBidi(bufSize int, done <-chan struct{}) *bidi {
+	return &bidi{cToS: newUni(bufSize, done), sToC: newUni(bufSize, done)}
+}
+
+func (t *uni) recv() (*manager.TunnelMessage, error) {
+	select {
+	case <-t.done:
+		return nil, context.Canceled
+	case m := <-t.ch:
+		if m == nil {
+			return nil, net.ErrClosed
+		}
+		// Simulate a network latency of one microsecond per byte
+		time.Sleep(time.Duration(len(m.Payload)) * time.Microsecond)
+		return m, nil
+	}
+}
+
+func (t *uni) send(msg *manager.TunnelMessage) error {
+	select {
+	case <-t.done:
+		return context.Canceled
+	case t.ch <- msg:
+		return nil
+	}
+}
+
+func (t *uni) close() error {
+	close(t.ch)
+	return nil
+}
+
+func (t *bidi) clientSide() GRPClientCStream {
+	return &clientSide{t}
+}
+
+func (t *bidi) serverSide() GRPCStream {
+	return &serverSide{t}
+}
+
+type clientSide struct {
+	*bidi
+}
+
+func (c *clientSide) Recv() (*manager.TunnelMessage, error) {
+	return c.sToC.recv()
+}
+
+func (c *clientSide) Send(msg *manager.TunnelMessage) error {
+	return c.cToS.send(msg)
+}
+
+func (c *clientSide) CloseSend() error {
+	return c.cToS.close()
+}
+
+type serverSide struct {
+	*bidi
+}
+
+func (c *serverSide) Recv() (*manager.TunnelMessage, error) {
+	return c.cToS.recv()
+}
+
+func (c *serverSide) Send(msg *manager.TunnelMessage) error {
+	return c.sToC.send(msg)
+}
+
+func testContext(t *testing.T, timeout time.Duration) (context.Context, context.CancelFunc) {
+	return context.WithTimeout(dlog.WithLogger(context.Background(), log.NewTestLogger(t, dlog.LogLevelDebug)), timeout)
+}
+
+func TestStream_Connect(t *testing.T) {
+	ctx, cancel := testContext(t, time.Second)
+	defer cancel()
+
+	tunnel := newBidi(10, ctx.Done())
+	id := NewConnID(ipproto.TCP, iputil.Parse("127.0.0.1"), iputil.Parse("192.168.0.1"), 1001, 8080)
+	si := uuid.New().String()
+
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		client, err := NewClientStream(ctx, tunnel.clientSide(), id, si, 0, 0)
+		require.NoError(t, err)
+		assert.Equal(t, Version, client.PeerVersion())
+		assert.NoError(t, client.CloseSend(ctx))
+	}()
+
+	go func() {
+		defer wg.Done()
+		server, err := NewServerStream(ctx, tunnel.serverSide())
+		require.NoError(t, err)
+		assert.Equal(t, id, server.ID())
+		assert.Equal(t, Version, server.PeerVersion())
+		assert.Equal(t, si, server.SessionID())
+	}()
+	wg.Wait()
+}
+
+func produce(ctx context.Context, s Stream, msg Message, errs chan<- error) {
+	wrCh := make(chan Message)
+	WriteLoop(ctx, s, wrCh)
+	go func() {
+		for i := 0; i < 100; i++ {
+			wrCh <- msg
+		}
+		close(wrCh)
+	}()
+
+	rdCh, errCh := ReadLoop(ctx, s)
+	select {
+	case <-ctx.Done():
+		errs <- ctx.Err()
+	case err := <-errCh:
+		errs <- err
+	case m := <-rdCh:
+		if m != nil {
+			errs <- fmt.Errorf("unexpected message: %s", m)
+		}
+	}
+}
+
+func consume(ctx context.Context, s Stream, expectedPayload []byte, errs chan<- error) {
+	count := 0
+	wrCh := make(chan Message)
+	WriteLoop(ctx, s, wrCh)
+	rdCh, errCh := ReadLoop(ctx, s)
+	for {
+		select {
+		case <-ctx.Done():
+			errs <- ctx.Err()
+		case err := <-errCh:
+			errs <- err
+		case m := <-rdCh:
+			if m == nil {
+				close(wrCh)
+				return
+			}
+			if m.Code() != Normal {
+				errs <- fmt.Errorf("unexpected message code %s", m.Code())
+				return
+			}
+			if !bytes.Equal(expectedPayload, m.Payload()) {
+				errs <- errors.New("unexpected message content")
+				return
+			}
+			count++
+		}
+	}
+}
+
+func requireNoErrs(t *testing.T, errs chan error) chan error {
+	t.Helper()
+	close(errs)
+	for err := range errs {
+		assert.NoError(t, err)
+	}
+	if t.Failed() {
+		t.FailNow()
+	}
+	return make(chan error, 10)
+}
+
+func TestStream_Xfer(t *testing.T) {
+	ctx, cancel := testContext(t, 30*time.Second)
+	defer cancel()
+
+	id := NewConnID(ipproto.TCP, iputil.Parse("127.0.0.1"), iputil.Parse("192.168.0.1"), 1001, 8080)
+	si := uuid.New().String()
+	b := make([]byte, 0x1000)
+	for i := range b {
+		b[i] = byte(i & 0xff)
+	}
+	large := NewMessage(Normal, b)
+	errs := make(chan error, 10)
+
+	// Send data from client to server
+	t.Run("client to server", func(t *testing.T) {
+		tunnel := newBidi(10, ctx.Done())
+		wg := sync.WaitGroup{}
+		wg.Add(2)
+		go func() {
+			defer wg.Done()
+			if client, err := NewClientStream(ctx, tunnel.clientSide(), id, si, 0, 0); err != nil {
+				errs <- err
+			} else {
+				produce(ctx, client, large, errs)
+			}
+		}()
+		go func() {
+			defer wg.Done()
+			if server, err := NewServerStream(ctx, tunnel.serverSide()); err != nil {
+				errs <- err
+			} else {
+				consume(ctx, server, b, errs)
+			}
+		}()
+		wg.Wait()
+		errs = requireNoErrs(t, errs)
+	})
+
+	t.Run("server to client", func(t *testing.T) {
+		tunnel := newBidi(10, ctx.Done())
+		wg := sync.WaitGroup{}
+		wg.Add(2)
+		go func() {
+			defer wg.Done()
+			if server, err := NewServerStream(ctx, tunnel.serverSide()); err != nil {
+				errs <- err
+			} else {
+				produce(ctx, server, large, errs)
+			}
+		}()
+		go func() {
+			defer wg.Done()
+			if client, err := NewClientStream(ctx, tunnel.clientSide(), id, si, 0, 0); err != nil {
+				errs <- err
+			} else {
+				consume(ctx, client, b, errs)
+			}
+		}()
+		wg.Wait()
+		errs = requireNoErrs(t, errs)
+	})
+
+	t.Run("client to client over BidiPipe", func(t *testing.T) {
+		ta := newBidi(10, ctx.Done())
+		tb := newBidi(10, ctx.Done())
+
+		aCh := make(chan Stream)
+		bCh := make(chan Stream)
+		wg := sync.WaitGroup{}
+		wg.Add(5)
+		go func() {
+			defer wg.Done()
+			if s, err := NewServerStream(ctx, ta.serverSide()); err != nil {
+				errs <- err
+				close(aCh)
+			} else {
+				aCh <- s
+			}
+		}()
+		go func() {
+			defer wg.Done()
+			if s, err := NewServerStream(ctx, tb.serverSide()); err != nil {
+				errs <- err
+				close(bCh)
+			} else {
+				bCh <- s
+			}
+		}()
+		go func() {
+			defer wg.Done()
+			if server, err := NewClientStream(ctx, ta.clientSide(), id, si, 0, 0); err != nil {
+				errs <- err
+			} else {
+				produce(ctx, server, large, errs)
+			}
+		}()
+		go func() {
+			defer wg.Done()
+			if client, err := NewClientStream(ctx, tb.clientSide(), id, si, 0, 0); err != nil {
+				errs <- err
+			} else {
+				consume(ctx, client, b, errs)
+			}
+		}()
+		go func() {
+			wg.Done()
+			var a, b Stream
+			for a == nil || b == nil {
+				select {
+				case <-ctx.Done():
+					errs <- ctx.Err()
+					return
+				case a = <-aCh:
+				case b = <-bCh:
+				}
+			}
+			fwd := NewBidiPipe(a, b)
+			fwd.Start(ctx)
+			select {
+			case <-ctx.Done():
+				errs <- ctx.Err()
+			case <-fwd.Done():
+			}
+		}()
+		wg.Wait()
+		errs = requireNoErrs(t, errs)
+	})
+}
