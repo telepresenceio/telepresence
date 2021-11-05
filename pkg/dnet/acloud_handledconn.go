1aaa80b702c0d3c7d56f49304332ee44bde1cf16
Luke Shumaker <lukeshu@datawire.io>
2021-11-05 00:54:04 -0600

    dnet: Run acloud_handledconn through conntest
    Signed-off-by: Luke Shumaker <lukeshu@datawire.io>

--- pkg/dnet/acloud_handledconn.go
+++ pkg/dnet/acloud_handledconn.go
@@ -1,20 +1,20 @@
 package dnet
 
 import (
-	"bytes"
 	"context"
 	"errors"
 	"fmt"
 	"net"
-	"os"
-	"sync"
-	"time"
 
 	"google.golang.org/grpc"
 
 	"github.com/telepresenceio/telepresence/rpc/v2/manager"
 )
 
+// handledConnectionImpl is the intersection of these two interfaces:
+//
+//   manager.ManagerProxy_HandleConnectionClient
+//   manager.ManagerProxy_HandleConnectionServer
 type handledConnectionImpl interface {
 	Send(*manager.ConnectionChunk) error
 	Recv() (*manager.ConnectionChunk, error)
@@ -38,14 +38,14 @@ func DialFromAmbassadorCloud(ctx context.Context, managerClient manager.ManagerP
 		_ = impl.CloseSend()
 		return nil, err
 	}
-	return wrap(impl), nil
+	return wrapUnbufferedConn(handledConn{conn: impl}), nil
 }
 
 // AcceptFromAmbassadorCloud is used by a Telepresence manger to accept a connection to an intercept
 // for a preview URL from a Ambassador Cloud.
 //
 // It is the counterpart to DialFromAmbassadorCloud.
-func AcceptFromAmbassadorCloud(systema manager.ManagerProxy_HandleConnectionServer) (interceptID string, conn Conn, err error) {
+func AcceptFromAmbassadorCloud(systema manager.ManagerProxy_HandleConnectionServer, closeFn func()) (interceptID string, conn Conn, err error) {
 	chunk, err := systema.Recv()
 	if err != nil {
 		return "", nil, err
@@ -56,129 +56,71 @@ func AcceptFromAmbassadorCloud(systema manager.ManagerProxy_HandleConnectionServ
 	}
 	interceptID = chunkValue.InterceptId
 
-	return interceptID, wrap(systema), nil
+	return interceptID, wrapUnbufferedConn(handledConn{conn: systema, close: closeFn}), nil
 }
 
+// handledConn is an unbufferedConn implementation that uses a gRPC
+// "/telepresence.manager/ManagerProxy/HandleConnection" stream as the underlying transport.
 type handledConn struct {
-	conn handledConnectionImpl
-
-	waitOnce sync.Once
-	wait     chan struct{}
-	waitErr  error
-
-	closeOnce sync.Once
-	closed    chan struct{}
-
-	readMu       sync.Mutex
-	readBuff     bytes.Buffer
-	readDeadline pipeDeadline
-	readErr      error
-
-	writeMu       sync.Mutex
-	writeDeadline pipeDeadline
-	writeErr      error
+	conn  handledConnectionImpl
+	close func()
 }
 
-// Wrap takes a manager.ManagerProxy_HandleConnectionClient or
-// manager.ManagerProxy_HandleConnectionServer and wraps it so that it can be used as a net.Conn.
-func wrap(impl handledConnectionImpl) Conn {
-	return &handledConn{
-		conn: impl,
+// Recv implements unbufferedConn.
+func (c handledConn) Recv() ([]byte, error) {
+	chunk, err := c.conn.Recv()
 
-		wait:          make(chan struct{}),
-		closed:        make(chan struct{}),
-		readDeadline:  makePipeDeadline(),
-		writeDeadline: makePipeDeadline(),
-	}
-}
-
-// Read implements net.Conn.
-func (c *handledConn) Read(b []byte) (int, error) {
-	c.readMu.Lock()
-	defer c.readMu.Unlock()
-	for c.readBuff.Len() == 0 {
-		if c.readErr == nil {
-			switch {
-			case isClosedChan(c.closed):
-				c.readErr = os.ErrClosed
-			case isClosedChan(c.readDeadline.wait()):
-				c.readErr = os.ErrDeadlineExceeded
+	var data []byte
+	if chunk != nil && chunk.Value != nil {
+		switch chunk := chunk.Value.(type) {
+		case *manager.ConnectionChunk_InterceptId:
+			if err == nil {
+				err = errors.New("HandleConnection: unexpected intercept_id chunk")
+			}
+		case *manager.ConnectionChunk_Data:
+			if chunk != nil && len(chunk.Data) > 0 {
+				data = chunk.Data
+			}
+		case *manager.ConnectionChunk_Error:
+			if err == nil {
+				err = fmt.Errorf("HandleConnection: remote error: %s", chunk.Error)
 			}
 		}
-		if c.readErr != nil {
-			return 0, c.readErr
-		}
-
-		chunk, err := c.conn.Recv()
-		if chunk != nil && chunk.Value != nil {
-			switch chunk := chunk.Value.(type) {
-			case *manager.ConnectionChunk_InterceptId:
-				c.readErr = errors.New("HandleConnection: unexpected intercept_id chunk")
-				c.Close()
-				continue
-			case *manager.ConnectionChunk_Data:
-				if chunk != nil && len(chunk.Data) > 0 {
-					c.readBuff.Write(chunk.Data)
-				}
-			case *manager.ConnectionChunk_Error:
-				c.readErr = fmt.Errorf("HandleConnection: remote error: %s", chunk.Error)
-				c.Close()
-				continue
-			}
-		}
-		if err != nil && c.readErr == nil {
-			c.waitOnce.Do(func() { c.waitErr = err; close(c.wait) })
-			c.readErr = err
-		}
 	}
-	return c.readBuff.Read(b)
+
+	return data, err
 }
 
-// Write implements net.Conn.
-func (c *handledConn) Write(b []byte) (int, error) {
-	c.writeMu.Lock()
-	defer c.writeMu.Unlock()
-
-	if c.writeErr == nil {
-		switch {
-		case isClosedChan(c.closed):
-			c.writeErr = os.ErrClosed
-		case isClosedChan(c.writeDeadline.wait()):
-			c.writeErr = os.ErrDeadlineExceeded
-		}
-	}
-	if c.writeErr != nil {
-		return 0, c.writeErr
-	}
-
-	err := c.conn.Send(&manager.ConnectionChunk{
+// Send implements unbufferedConn.
+func (c handledConn) Send(data []byte) error {
+	return c.conn.Send(&manager.ConnectionChunk{
 		Value: &manager.ConnectionChunk_Data{
-			Data: b,
+			Data: data,
 		},
 	})
-	if err != nil {
-		c.waitOnce.Do(func() { c.waitErr = err; close(c.wait) })
-		c.writeErr = err
-		return 0, err
+}
+
+// CloseOnce implements unbufferedConn.
+func (c handledConn) CloseOnce() error {
+	var err error
+	if client, isClient := c.conn.(grpc.ClientStream); isClient {
+		err = client.CloseSend()
 	}
-	return len(b), nil
+	if c.close != nil {
+		c.close()
+	}
+	return err
 }
 
-// Close implements net.Conn.
-func (c *handledConn) Close() error {
-	c.closeOnce.Do(func() {
-		if client, isClient := c.conn.(grpc.ClientStream); isClient {
-			c.writeMu.Lock()
-			defer c.writeMu.Unlock()
-			_ = client.CloseSend()
-		}
-		c.waitOnce.Do(func() { close(c.wait) })
-	})
-	return nil
+// MTU implements unbufferedConn.
+func (c handledConn) MTU() int {
+	// 3MiB; assume the other end's gRPC library uses the go-grpc default 4MiB, plus plenty of
+	// room for overhead.
+	return 3 * 1024 * 1024
 }
 
-// LocalAddr implements net.Conn.
-func (c *handledConn) LocalAddr() net.Addr {
+// LocalAddr implements unbufferedConn.
+func (c handledConn) LocalAddr() net.Addr {
 	_, isClient := c.conn.(grpc.ClientStream)
 	if isClient {
 		return addr{
@@ -193,8 +135,8 @@ func (c *handledConn) LocalAddr() net.Addr {
 	}
 }
 
-// RemoteAddr implements net.Conn.
-func (c *handledConn) RemoteAddr() net.Addr {
+// RemoteAddr implements unbufferedConn.
+func (c handledConn) RemoteAddr() net.Addr {
 	_, isClient := c.conn.(grpc.ClientStream)
 	if isClient {
 		return addr{
@@ -207,35 +149,4 @@ func (c *handledConn) RemoteAddr() net.Addr {
 			addr: "localrole=client,remotehostname=acloud",
 		}
 	}
-}
-
-// SetDeadline implements net.Conn.
-func (c *handledConn) SetDeadline(t time.Time) error {
-	_ = c.SetReadDeadline(t)
-	_ = c.SetWriteDeadline(t)
-	return nil
-}
-
-// SetReadDeadline implements net.Conn.
-func (c *handledConn) SetReadDeadline(t time.Time) error {
-	if isClosedChan(c.closed) {
-		c.readDeadline.set(t)
-	}
-	return nil
-}
-
-// SetWriteDeadline implements net.Conn.
-func (c *handledConn) SetWriteDeadline(t time.Time) error {
-	if isClosedChan(c.closed) {
-		c.writeDeadline.set(t)
-	}
-	return nil
-}
-
-// Wait waits until either the connection is Close()d, or one of Read() or Write() encounters an
-// error (*not* counting errors caused by deadlines).  If this returns because Close() was called,
-// nil is returned; otherwise the triggering error is returned.
-func (c *handledConn) Wait() error {
-	<-c.wait
-	return c.waitErr
 }
