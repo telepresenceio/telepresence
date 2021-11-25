0f40ca7f9cbe7e1aa10b672c37996cb82e6c339f
Thomas Hallgren <thomas@datawire.io>
2021-11-25 10:24:21 +0100

    Move the apitest service code closer to the doc example.
    Signed-off-by: Thomas Hallgren <thomas@datawire.io>

--- integration_test/testdata/apiserveraccess/main.go
+++ integration_test/testdata/apiserveraccess/main.go
@@ -11,21 +11,13 @@ import (
 	"strconv"
 	"strings"
 
-	"github.com/sethvargo/go-envconfig"
 	"golang.org/x/sys/unix"
 
 	"github.com/datawire/dlib/dhttp"
 	"github.com/datawire/dlib/dlog"
 	"github.com/telepresenceio/telepresence/v2/pkg/header"
 	"github.com/telepresenceio/telepresence/v2/pkg/log"
-	"github.com/telepresenceio/telepresence/v2/pkg/restapi"
 )
-
-type Env struct {
-	AppPort  int    `env:"APP_PORT"`
-	APIPort  int    `env:"TELEPRESENCE_API_PORT"`
-	LogLevel string `env:"LOG_LEVEL,default="`
-}
 
 func main() {
 	c, cancel := context.WithCancel(log.MakeBaseLogger(context.Background(), "DEBUG"))
@@ -53,26 +45,33 @@ func main() {
 }
 
 func run(c context.Context) error {
-	env := Env{}
-	if err := envconfig.Process(c, &env); err != nil {
-		return err
-	}
-	if env.LogLevel != "" {
-		log.SetLevel(c, env.LogLevel)
+	if lv, ok := os.LookupEnv("LOG_LEVEL"); ok {
+		log.SetLevel(c, lv)
 	}
 
-	ln, err := net.Listen("tcp", "localhost:"+strconv.Itoa(env.AppPort))
+	ap, ok := os.LookupEnv("APP_PORT")
+	if !ok {
+		ap = "8080"
+	}
+	_, err := strconv.ParseUint(ap, 10, 16)
+	if err != nil {
+		return fmt.Errorf("the value %q of env APP_PORT is not a valid port number", ap)
+	}
+
+	url, err := consumeHereURL()
+	if err != nil {
+		return err
+	}
+
+	ln, err := net.Listen("tcp", "localhost:"+ap)
 	if err != nil {
 		return err
 	}
 
 	mux := http.NewServeMux()
-	if env.APIPort != 0 {
-		apiURL := "http://localhost:" + strconv.Itoa(env.APIPort)
-		mux.HandleFunc(restapi.EndPontConsumeHere, func(w http.ResponseWriter, r *http.Request) {
-			intercepted(c, apiURL, w, r)
-		})
-	}
+	mux.HandleFunc("/consume-here", func(w http.ResponseWriter, r *http.Request) {
+		intercepted(c, url, w, r)
+	})
 	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 	})
@@ -87,58 +86,76 @@ func run(c context.Context) error {
 	return nil
 }
 
-func intercepted(c context.Context, apiURL string, w http.ResponseWriter, r *http.Request) {
-	dlog.Debugf(c, "Received %s request", restapi.EndPontConsumeHere)
+const portEnv = "TELEPRESENCE_API_PORT"
+const interceptIdEnv = "TELEPRESENCE_INTERCEPT_ID"
 
-	apiRq, err := http.NewRequest("GET", apiURL+restapi.EndPontConsumeHere, nil)
-	w.Header().Set("Content-Type", "text/plain")
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		err = fmt.Errorf("failed to create http request: %w", err)
-		if _, wErr := w.Write([]byte(err.Error())); wErr != nil {
-			dlog.Errorf(c, "error %v while responding with error %v", wErr, err)
-		}
-		return
+// apiURL creates the generic URL needed to access the service
+func apiURL() (string, error) {
+	pe := os.Getenv(portEnv)
+	if _, err := strconv.ParseUint(pe, 10, 16); err != nil {
+		return "", fmt.Errorf("value %q of env %s does not represent a valid port number", pe, portEnv)
 	}
+	return "http://localhost:" + pe, nil
+}
 
-	apiRq.Header = make(http.Header, len(r.Header)+1)
-	apiRq.Header.Set(restapi.HeaderCallerInterceptID, os.Getenv("TELEPRESENCE_INTERCEPT_ID"))
+// consumeHereURL creates the URL for the "consume-here" endpoint
+func consumeHereURL() (string, error) {
+	apiURL, err := apiURL()
+	if err != nil {
+		return "", err
+	}
+	return apiURL + "/consume-here", nil
+}
+
+// consumeHere calls the consume-here endpoint with the given headers and returns the result
+func consumeHere(c context.Context, url string, hm map[string]string) (bool, error) {
+	rq, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return false, err
+	}
+	rq.Header = make(http.Header, len(hm)+1)
+	rq.Header.Set("X-Telepresence-Caller-Intercept-Id", os.Getenv(interceptIdEnv))
+	for k, v := range hm {
+		rq.Header.Set(k, v)
+	}
+	dlog.Debugf(c, "%s with headers\n%s", url, header.Stringer(rq.Header))
+	rs, err := http.DefaultClient.Do(rq)
+	if err != nil {
+		return false, err
+	}
+	defer rs.Body.Close()
+	b, err := io.ReadAll(rs.Body)
+	if err != nil {
+		return false, err
+	}
+	return strconv.ParseBool(strings.TrimSpace(string(b)))
+}
+
+func intercepted(c context.Context, url string, w http.ResponseWriter, r *http.Request) {
+	hm := make(map[string]string, len(r.Header))
 
 	// The "X-With-" prefix is used as a backdoor to avoid triggering intercepts during test. It's
 	// stripped off here.
-	for h, v := range r.Header {
-		apiRq.Header[strings.TrimPrefix(h, "X-With-")] = v
+	for h := range r.Header {
+		hm[strings.TrimPrefix(h, "X-With-")] = r.Header.Get(h)
 	}
 
 	// The "X-Without-" prefix is used when the headers that trigger an intercept must be included in
 	// order for the intercept to take place, but should be removed in the subsequent query. Both
 	// the "X-Without-" headers and the header they refer to are stripped off here.
-	for h := range apiRq.Header {
+	for h := range hm {
 		if hw := strings.TrimPrefix(h, "X-Without-"); h != hw {
-			apiRq.Header.Del(h)
-			apiRq.Header.Del(hw)
+			delete(hm, h)
+			delete(hm, hw)
 		}
 	}
+	w.Header().Set("Content-Type", "text/plain")
 
-	dlog.Debugf(c, "Sending request %s with headers %s", apiRq.URL, header.Stringer(apiRq.Header))
-	rsp, err := http.DefaultClient.Do(apiRq)
-	if err != nil {
+	if cs, err := consumeHere(c, url, hm); err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
-		err = fmt.Errorf("failed to execute http request: %w", err)
-		if _, wErr := w.Write([]byte(err.Error())); wErr != nil {
-			dlog.Errorf(c, "error %v while responding with error %v", wErr, err)
-		}
-		return
+		fmt.Fprintf(w, "failed to execute http request: %v", err)
+	} else {
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, "%t", cs)
 	}
-	defer rsp.Body.Close()
-	wh := w.Header()
-	for h, v := range rsp.Header {
-		wh[h] = v
-	}
-	w.WriteHeader(rsp.StatusCode)
-	rl, err := io.Copy(w, rsp.Body)
-	if err != nil {
-		dlog.Errorf(c, "failed to copy message body: %v", err)
-	}
-	dlog.Debugf(c, "response len = %d", rl)
 }
