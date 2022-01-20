cf6f4dbc390b444072216542422e4596ae06a37b
Jose Cortes <josecortes@datawire.io>
2022-01-20 14:00:23 -0500

    hackz
    Signed-off-by: Jose Cortes <josecortes@datawire.io>

--- pkg/client/cli/cliutil/connector.go
+++ pkg/client/cli/cliutil/connector.go
@@ -64,7 +64,7 @@ func withConnector(ctx context.Context, maybeStart bool, withNotify bool, fn fun
 			err = ErrNoUserDaemon
 			if maybeStart {
 				fmt.Println("Launching Telepresence User Daemon")
-				if err = proc.StartInBackground(client.GetExe(), "connector-foreground"); err != nil {
+				if err = proc.StartInBackground("/Users/josecortes/go/src/github.com/datawire/telepresence2-proprietary/telepresence-pro", "connector-foreground"); err != nil {
 					return fmt.Errorf("failed to launch the connector service: %w", err)
 				}
 
