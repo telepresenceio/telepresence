243413343ca635026f3e967f8e7d55034f6f784b
Donny Yung <donaldyung@datawire.io>
2022-02-11 11:06:36 -0500

    Improve quit messages
    Signed-off-by: Donny Yung <donaldyung@datawire.io>

--- pkg/client/cli/cliutil/version_check.go
+++ pkg/client/cli/cliutil/version_check.go
@@ -18,17 +18,27 @@ func versionCheck(ctx context.Context, daemonType string, daemonBinary string, c
 	if ctx.Value(quitting{}) != nil {
 		return nil
 	}
+	var quitFlag string
+	switch {
+	case daemonType == "Root":
+		quitFlag = "-r"
+	case daemonType == "User":
+		quitFlag = "-u"
+	default:
+		return fmt.Errorf("unknown daemonType: %s", daemonType)
+	}
 	// Ensure that the already running daemon has the correct version
 	vi, err := client.Version(ctx, &empty.Empty{})
 	if err != nil {
 		return fmt.Errorf("unable to retrieve version of %s Daemon: %w", daemonType, err)
 	}
 	if version.Version != vi.Version {
-		return errcat.User.Newf("version mismatch. Client %s != %s Daemon %s, please quit telepresence and reconnect", version.Version, daemonType, vi.Version)
+		return errcat.User.Newf("version mismatch. Client %s != %s Daemon %s, please run 'telepresence quit %s' and reconnect", version.Version, daemonType, vi.Version, quitFlag)
 	}
 	if daemonBinary != "" {
 		if vi.Executable != daemonBinary {
-			return errcat.User.Newf("executable mismatch. Connector using %s, configured to use %s, please quit telepresence and reconnect", vi.Executable, daemonBinary)
+			return errcat.User.Newf("executable mismatch. Connector using %s, configured to use %s, please run 'telepresence quit %s' and reconnect",
+				vi.Executable, daemonBinary, quitFlag)
 		}
 	}
 	return nil
