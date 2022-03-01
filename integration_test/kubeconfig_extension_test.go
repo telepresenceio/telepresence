25ababedd23d2e19d7ad4376c572f382aa7d9fb3
Maxime Legault-Venne <maximelegaultvenne@datawire.io>
2022-03-01 09:59:25 -0500

    Bump up status eventual condition wait period
    Signed-off-by: Maxime Legault-Venne <maximelegaultvenne@datawire.io>

--- integration_test/kubeconfig_extension_test.go
+++ integration_test/kubeconfig_extension_test.go
@@ -106,7 +106,7 @@ func (s *notConnectedSuite) Test_NeverProxy() {
 		expected := fmt.Sprintf("Never Proxy: (%d subnets)", neverProxiedCount)
 		s.T().Log("Looking for string: ", expected)
 		return strings.Contains(stdout, expected)
-	}, 5*time.Second, 1*time.Second, fmt.Sprintf("did not find %d never-proxied subnets", neverProxiedCount))
+	}, 10*time.Second, 1*time.Second, fmt.Sprintf("did not find %d never-proxied subnets", neverProxiedCount))
 
 	s.Eventually(func() bool {
 		jsonStdout := itest.TelepresenceOk(ctx, "status", "--json")
@@ -114,7 +114,7 @@ func (s *notConnectedSuite) Test_NeverProxy() {
 		require.NoError(json.Unmarshal([]byte(jsonStdout), &status))
 		s.T().Log("Actual never proxy subnet count: ", len(status.RootDaemon.NeverProxySubnets))
 		return len(status.RootDaemon.NeverProxySubnets) == neverProxiedCount
-	}, 5*time.Second, 1*time.Second, fmt.Sprintf("did not find %d never-proxied subnets in json status", neverProxiedCount))
+	}, 10*time.Second, 1*time.Second, fmt.Sprintf("did not find %d never-proxied subnets in json status", neverProxiedCount))
 
 	s.Eventually(func() bool {
 		return itest.Run(ctx, "curl", "--silent", "--max-time", "0.5", ip) != nil
