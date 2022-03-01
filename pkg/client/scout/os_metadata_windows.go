a9150fc30f13f71e7f1eafcf339e0312c2a2ed65
Maxime Legault-Venne <maximelegaultvenne@datawire.io>
2022-03-01 13:23:24 -0500

    Use wmic instead of systeminfo for windows os metadata
    Signed-off-by: Maxime Legault-Venne <maximelegaultvenne@datawire.io>

--- pkg/client/scout/os_metadata_windows.go
+++ pkg/client/scout/os_metadata_windows.go
@@ -11,34 +11,36 @@ import (
 )
 
 func getOsMetadata(ctx context.Context) map[string]interface{} {
-	cmd := dexec.CommandContext(ctx, "systeminfo")
+	cmd := dexec.CommandContext(ctx, "wmic", "os", "get", "Caption,Version,BuildNumber", "/value")
 	cmd.DisableLogging = true
 	r, err := cmd.Output()
 	osMeta := map[string]interface{}{}
 	if err != nil {
-		dlog.Warnf(ctx, "Error running systeminfo: %v", err)
+		dlog.Warnf(ctx, "Error running wmic: %v", err)
 		return osMeta
 	}
 	scanner := bufio.NewScanner(bytes.NewReader(r))
 	for scanner.Scan() {
 		line := scanner.Text()
-		parts := strings.Split(line, ":")
+		parts := strings.Split(line, "=")
 		if len(parts) < 2 {
 			// Untagged string (empty line, etc)
 			continue
 		}
-		name, value := strings.TrimSpace(parts[0]), strings.TrimSpace(strings.Join(parts[1:], ":"))
+		name, value := strings.TrimSpace(parts[0]), strings.TrimSpace(strings.Join(parts[1:], "="))
 		// systeminfo doesn't have a concept of an OS Build number, so we'll have to set that to unknown
-		if name == "OS Name" {
+		if name == "Caption" {
 			osMeta["os_name"] = value
 		}
-		if name == "OS Version" {
+		if name == "Version" {
 			osMeta["os_version"] = value
 		}
+		if name == "BuildNumber" {
+			osMeta["os_build_version"] = value
+		}
 	}
-	osMeta["os_build_version"] = "unknown"
 	if err := scanner.Err(); err != nil {
-		dlog.Warnf(ctx, "Unable to scan systeminfo output: %v", err)
+		dlog.Warnf(ctx, "Unable to scan wmic output: %v", err)
 	}
 	return osMeta
 }
