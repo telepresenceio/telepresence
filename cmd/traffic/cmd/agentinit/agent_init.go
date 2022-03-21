e0c74b0c364143a1712b4a3da061a51c86edebdc
Thomas Hallgren <thomas@datawire.io>
2022-03-21 18:30:59 +0100

    Configure the agent init-container from the ConfigMap
    Signed-off-by: Thomas Hallgren <thomas@datawire.io>

--- cmd/traffic/cmd/agentinit/agent_init.go
+++ cmd/traffic/cmd/agentinit/agent_init.go
@@ -8,77 +8,129 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"path/filepath"
 	"strconv"
+	"strings"
 
 	"github.com/coreos/go-iptables/iptables"
-	"github.com/sethvargo/go-envconfig"
+	"gopkg.in/yaml.v3"
+
+	"github.com/telepresenceio/telepresence/v2/pkg/dos"
+	"github.com/telepresenceio/telepresence/v2/pkg/install/agent"
 )
 
 const nat = "nat"
 const inboundChain = "TEL_INBOUND"
 
 type config struct {
-	AgentPort     int    `env:"AGENT_PORT,required"`
-	AppPort       int    `env:"APP_PORT,required"`
-	AgentProtocol string `env:"AGENT_PROTOCOL,required"`
+	agent.Config
 }
 
-func configureIptables(ctx context.Context, iptables *iptables.IPTables, loopback string, cfg config) error {
+func loadConfig(ctx context.Context) (*config, error) {
+	cf, err := dos.Open(ctx, filepath.Join(agent.ConfigMountPoint, agent.ConfigFile))
+	if err != nil {
+		return nil, fmt.Errorf("unable to open agent ConfigMap: %w", err)
+	}
+	defer cf.Close()
+
+	c := config{}
+	if err = yaml.NewDecoder(cf).Decode(&c.Config); err != nil {
+		return nil, fmt.Errorf("unable to decode agent ConfigMap: %w", err)
+	}
+	return &c, nil
+}
+
+func (c *config) configureIptables(ctx context.Context, iptables *iptables.IPTables, loopback string) error {
 	// These iptables rules implement routing such that a packet directed to the appPort will hit the agentPort instead.
 	// If there's no mesh this is simply request -> agent -> app (or intercept)
-	// However, if there's a service mesh we want to make sure we don't bypass the mesh, so the traffic will flow request -> mesh -> agent -> app
-	appPort := strconv.Itoa(cfg.AppPort)
-	agentPort := strconv.Itoa(cfg.AgentPort)
+	// However, if there's a service mesh we want to make sure we don't bypass the mesh, so the traffic
+	// will flow request -> mesh -> agent -> app
+
+	// A service mesh will typically use an UID different from the one used by this process
 	agentUID := strconv.Itoa(os.Getuid())
-	// Clearing the inbound chain will create it if it doesn't exist, or clear it out if it does.
-	err := iptables.ClearChain(nat, inboundChain)
-	if err != nil {
-		return fmt.Errorf("failed to clear chain %s: %w", inboundChain, err)
+
+	outputInsertCount := 0
+	for _, proto := range []string{"tcp", "udp"} {
+		hasRule := false
+		for _, cn := range c.Containers {
+			for _, ic := range cn.Intercepts {
+				if strings.EqualFold(proto, ic.Protocol) {
+					hasRule = true
+					break
+				}
+			}
+		}
+		if !hasRule {
+			// no rules for the given proto
+			continue
+		}
+
+		// Clearing the inbound chain will create it if it doesn't exist, or clear it out if it does.
+		chain := inboundChain + "_" + strings.ToUpper(proto)
+		err := iptables.ClearChain(nat, chain)
+		if err != nil {
+			return fmt.Errorf("failed to clear chain %s: %w", chain, err)
+		}
+
+		// Use our inbound chain to direct traffic coming into the app port to the agent port.
+		for _, cn := range c.Containers {
+			for _, ic := range cn.Intercepts {
+				if strings.EqualFold(proto, ic.Protocol) {
+					err = iptables.AppendUnique(nat, chain,
+						"-p", proto, "--dport", strconv.Itoa(int(ic.ContainerPort)),
+						"-j", "REDIRECT", "--to-ports", strconv.Itoa(int(ic.AgentPort)))
+					if err != nil {
+						return fmt.Errorf("failed to append rule to %s: %w", chain, err)
+					}
+					hasRule = true
+				}
+			}
+		}
+
+		// Direct everything coming into PREROUTING into our own inbound chain.
+		// We do this as an append instead of an insert because this will prevent us from interfering with a service mesh
+		// if one exists. If a service mesh exists, its PREROUTING rules will kick in before ours, ensuring traffic
+		// coming into the pod does not bypass the mesh.
+		err = iptables.AppendUnique(nat, "PREROUTING",
+			"-p", proto,
+			"-j", chain)
+		if err != nil {
+			return fmt.Errorf("failed to append prerouting rule to direct to %s: %w", chain, err)
+		}
+
+		// Any traffic heading out of the loopback and into the app port (other than traffic from the agent) needs to
+		// be redirected to the agent. This will ensure that if there's a service mesh, when the mesh's proxy goes to
+		// request the application, it will get a response via the traffic agent.
+		err = iptables.Insert(nat, "OUTPUT", 1,
+			"-o", loopback,
+			"-m", "owner", "!", "--uid-owner", agentUID,
+			"-j", chain)
+		if err != nil {
+			return fmt.Errorf("failed to insert ! --gid-owner rule in OUTPUT: %w", err)
+		}
+		outputInsertCount++
+
+		// Any agent traffic heading out on the loopback but NOT towards localhost needs to be processed in case
+		// it needs to be redirected. This is so that if the traffic agent requests its own IP, it doesn't just
+		// serve the app but actually goes through the agent, and thus through any intercepts.
+		// This is needed to support requesting an intercepted pod by IP (or to intercept a headless service).
+		err = iptables.Insert(nat, "OUTPUT", 1,
+			"-o", loopback,
+			"-p", proto,
+			"!", "-d", "127.0.0.1/32",
+			"-m", "owner", "--uid-owner", agentUID,
+			"-j", chain)
+		if err != nil {
+			return fmt.Errorf("failed to insert --gid-owner rule in OUTPUT: %w", err)
+		}
+		outputInsertCount++
 	}
-	// Use our inbound chain to direct traffic coming into the app port to the agent port.
-	err = iptables.AppendUnique(nat, inboundChain,
-		"-p", cfg.AgentProtocol, "--dport", appPort,
-		"-j", "REDIRECT", "--to-ports", agentPort)
-	if err != nil {
-		return fmt.Errorf("failed to append rule to %s: %w", inboundChain, err)
-	}
-	// Direct everything coming into PREROUTING into our own inbound chain.
-	// We do this as an append instead of an insert because this will prevent us from interfering with a service mesh
-	// if one exists. If a service mesh exists, its PREROUTING rules will kick in before ours, ensuring traffic
-	// coming into the pod does not bypass the mesh.
-	err = iptables.AppendUnique(nat, "PREROUTING",
-		"-p", cfg.AgentProtocol,
-		"-j", inboundChain)
-	if err != nil {
-		return fmt.Errorf("failed to append prerouting rule to direct to %s: %w", inboundChain, err)
-	}
-	// Any traffic heading out of the loopback and into the app port (other than traffic from the agent) needs to
-	// be redirected to the agent. This will ensure that if there's a service mesh, when the mesh's proxy goes to
-	// request the application, it will get a response via the traffic agent.
-	err = iptables.Insert(nat, "OUTPUT", 1, "-o", loopback,
-		"-m", "owner", "!", "--gid-owner", agentUID,
-		"-j", inboundChain)
-	if err != nil {
-		return fmt.Errorf("failed to insert ! --gid-owner rule in OUTPUT: %w", err)
-	}
-	// Any agent traffic heading out on the loopback but NOT towards localhost needs to be processed in case
-	// it needs to be redirected. This is so that if the traffic agent requests its own IP, it doesn't just
-	// serve the app but actually goes through the agent, and thus through any intercepts.
-	// This is needed to support requesting an intercepted pod by IP (or to intercept a headless service).
-	err = iptables.Insert(nat, "OUTPUT", 1,
-		"-o", loopback,
-		"-p", cfg.AgentProtocol,
-		"!", "-d", "127.0.0.1/32",
-		"-m", "owner", "--gid-owner", agentUID,
-		"-j", inboundChain)
-	if err != nil {
-		return fmt.Errorf("failed to insert --gid-owner rule in OUTPUT: %w", err)
-	}
+
 	// Finally, any other traffic heading out of the traffic agent should pass by unperturbed -- it should obviously not be
 	// redirected back into the agent, but it also should not pass through a mesh proxy.
 	// This will include not just agent->manager traffic but also the agent requesting 127.0.0.1:appPort to serve the application
-	err = iptables.Insert(nat, "OUTPUT", 2,
-		"-m", "owner", "--gid-owner", agentUID,
+	err := iptables.Insert(nat, "OUTPUT", 1+outputInsertCount,
+		"-m", "owner", "--uid-owner", agentUID,
 		"-j", "RETURN")
 	if err != nil {
 		return fmt.Errorf("failed to insert --gid-owner rule in OUTPUT: %w", err)
@@ -105,18 +157,18 @@ func findLoopback(ctx context.Context) (string, error) {
 
 // Main is the main function for the agent init container
 func Main(ctx context.Context, args ...string) error {
-	cfg := config{}
-	if err := envconfig.Process(ctx, &cfg); err != nil {
+	cfg, err := loadConfig(ctx)
+	if err != nil {
 		return err
 	}
+
 	lo, err := findLoopback(ctx)
 	if err != nil {
 		return err
 	}
-	iptables, err := iptables.New()
+	it, err := iptables.New()
 	if err != nil {
 		return fmt.Errorf("unable to create iptables instance: %w", err)
 	}
-	err = configureIptables(ctx, iptables, lo, cfg)
-	return err
+	return cfg.configureIptables(ctx, it, lo)
 }
