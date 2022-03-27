ed534edab4a1756e00027848ce16e8ada2cb451f
Thomas Hallgren <thomas@datawire.io>
2022-03-27 13:21:35 +0200

    Add dos.ExpandEnv
    Signed-off-by: Thomas Hallgren <thomas@datawire.io>

--- pkg/dos/env.go
+++ pkg/dos/env.go
@@ -9,6 +9,7 @@ import (
 // Env is an abstraction of the environment related functions with the same name in the os package.
 type Env interface {
 	Environ() []string
+	ExpandEnv(string) string
 	Getenv(string) string
 	Setenv(string, string) error
 	Lookup(string) (string, bool)
@@ -28,6 +29,10 @@ func (e MapEnv) Environ() []string {
 		ks[i] = k + "=" + e[k]
 	}
 	return ks
+}
+
+func (e MapEnv) ExpandEnv(s string) string {
+	return os.Expand(s, e.Getenv)
 }
 
 func (e MapEnv) Getenv(key string) string {
@@ -63,6 +68,10 @@ func Environ(ctx context.Context) []string {
 	return EnvAPI(ctx).Environ()
 }
 
+func ExpandEnv(ctx context.Context, s string) string {
+	return EnvAPI(ctx).ExpandEnv(s)
+}
+
 func Getenv(ctx context.Context, key string) string {
 	return EnvAPI(ctx).Getenv(key)
 }
@@ -79,6 +88,10 @@ type osEnv struct{}
 
 func (osEnv) Environ() []string {
 	return os.Environ()
+}
+
+func (osEnv) ExpandEnv(s string) string {
+	return os.ExpandEnv(s)
 }
 
 func (osEnv) Getenv(s string) string {
