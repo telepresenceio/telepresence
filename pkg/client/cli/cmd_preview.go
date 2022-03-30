7924327b66c66d43a793a9259863f4a3331def36
alex <alex@datawire.io>
2022-03-30 10:42:44 -0400

    Fixing lint issue
    Signed-off-by: alex <alex@datawire.io>

--- pkg/client/cli/cmd_preview.go
+++ pkg/client/cli/cmd_preview.go
@@ -16,7 +16,8 @@ import (
 // fields in the given 'spec'.  If 'prefix' is given, long-flag names are prefixed with it.
 func addPreviewFlags(prefix string, flags *pflag.FlagSet, spec *manager.PreviewSpec) {
 	flags.BoolVarP(&spec.DisplayBanner, prefix+"banner", "b", true, "Display banner on preview page")
-	flags.StringToStringVarP(&spec.InjectHeaders, prefix+"add-headers", "", map[string]string{}, "Additional headers in key1=value1,key2=value2 pairs injected in every preview page request")
+	flags.StringToStringVarP(&spec.InjectHeaders, prefix+"add-headers", "", map[string]string{},
+	"Additional headers in key1=value1,key2=value2 pairs injected in every preview page request")
 }
 
 func previewCommand() *cobra.Command {
