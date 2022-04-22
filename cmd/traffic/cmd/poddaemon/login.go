da2e7727b416703e99e87bc75e1e8eb9152e9f6e
Luke Shumaker <lukeshu@datawire.io>
2022-04-22 10:20:06 -0600

    poddaemon: Clean up login.go
    Signed-off-by: Luke Shumaker <lukeshu@datawire.io>

--- cmd/traffic/cmd/poddaemon/login.go
+++ cmd/traffic/cmd/poddaemon/login.go
@@ -3,37 +3,43 @@ package poddaemon
 import (
 	"context"
 
+	"github.com/telepresenceio/telepresence/v2/pkg/client/userd/auth"
 	"github.com/telepresenceio/telepresence/v2/pkg/client/userd/auth/authdata"
 )
 
-type PoddLoginExecutor struct {
+// loginExecutor implements auth.LoginExecutor.  It mostly panics with "not implemented"; the only
+// part of the login flow that the pod-daemon aught to be implementing is emitting an API key (which
+// is simple and static, since the pod-daemon takes it an an env-var).
+type loginExecutor struct {
 	key string
 }
 
-func (PoddLoginExecutor) Worker(ctx context.Context) error {
-	panic("unimplemented: worker")
+var _ auth.LoginExecutor = loginExecutor{}
+
+func (loginExecutor) Worker(ctx context.Context) error {
+	panic("not implemented")
 }
 
-func (PoddLoginExecutor) Login(ctx context.Context) error {
-	panic("unimplemented: login")
+func (loginExecutor) Login(ctx context.Context) error {
+	panic("not implemented")
 }
 
-func (PoddLoginExecutor) LoginAPIKey(ctx context.Context, key string) (bool, error){
-	panic("unimplmented: loginapikey")
+func (loginExecutor) LoginAPIKey(ctx context.Context, key string) (bool, error) {
+	panic("not implemented")
 }
 
-func (PoddLoginExecutor) Logout(ctx context.Context) error {
-	panic("unimplemented: logout")
+func (loginExecutor) Logout(ctx context.Context) error {
+	panic("not implemented")
 }
 
-func (p PoddLoginExecutor) GetAPIKey(ctx context.Context, description string) (string, error) {
+func (p loginExecutor) GetAPIKey(ctx context.Context, description string) (string, error) {
 	return p.key, nil
 }
 
-func (PoddLoginExecutor) GetLicense(ctx context.Context, id string) (string, string, error) {
-	panic("unimplemented: getlicense")
+func (loginExecutor) GetLicense(ctx context.Context, id string) (string, string, error) {
+	panic("not implemented")
 }
 
-func (PoddLoginExecutor) GetUserInfo(ctx context.Context, refresh bool) (*authdata.UserInfo, error) {
-	panic("unimplemented: getuserinfo")
+func (loginExecutor) GetUserInfo(ctx context.Context, refresh bool) (*authdata.UserInfo, error) {
+	panic("not implemented")
 }
