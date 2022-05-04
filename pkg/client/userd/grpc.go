164eeeb68fa3485c903c65c89bee8e9c563e1872
Jose Cortes <josecortes@datawire.io>
2022-05-04 17:26:01 -0400

    Replace user daemon systema connections with a8rcloud pool
    Signed-off-by: Jose Cortes <josecortes@datawire.io>

--- pkg/client/userd/grpc.go
+++ pkg/client/userd/grpc.go
@@ -22,6 +22,8 @@ import (
 	"github.com/telepresenceio/telepresence/rpc/v2/common"
 	rpc "github.com/telepresenceio/telepresence/rpc/v2/connector"
 	"github.com/telepresenceio/telepresence/rpc/v2/manager"
+	"github.com/telepresenceio/telepresence/rpc/v2/userdaemon"
+	"github.com/telepresenceio/telepresence/v2/pkg/a8rcloud"
 	"github.com/telepresenceio/telepresence/v2/pkg/client"
 	"github.com/telepresenceio/telepresence/v2/pkg/client/cli"
 	"github.com/telepresenceio/telepresence/v2/pkg/client/cli/cliutil"
@@ -431,6 +433,20 @@ func (s *service) RunCommand(ctx context.Context, req *rpc.RunCommandRequest) (r
 			Stdout: outW.Bytes(),
 			Stderr: errW.Bytes(),
 		}
+	})
+	return
+}
+
+func (s *service) ResolveIngressInfo(ctx context.Context, req *userdaemon.IngressInfoRequest) (resp *userdaemon.IngressInfoResponse, err error) {
+	err = s.withSession(ctx, "ResolveIngressInfo", func(ctx context.Context, session trafficmgr.Session) error {
+		pool := a8rcloud.GetSystemAPool[*SessionClient](ctx, a8rcloud.UserdConnName)
+		systemacli, err := pool.Get(ctx)
+		if err != nil {
+			return err
+		}
+		defer pool.Done(ctx)
+		resp, err = systemacli.ResolveIngressInfo(ctx, req)
+		return err
 	})
 	return
 }
---
+++ pkg/client/userd/systema.go
@@ -0,0 +1,51 @@
+package userd
+
+import (
+	"context"
+	"net"
+
+	"github.com/telepresenceio/telepresence/rpc/v2/userdaemon"
+	"github.com/telepresenceio/telepresence/v2/pkg/a8rcloud"
+	"github.com/telepresenceio/telepresence/v2/pkg/client/cli/cliutil"
+	"github.com/telepresenceio/telepresence/v2/pkg/client/userd/trafficmgr"
+	"google.golang.org/grpc"
+	"google.golang.org/protobuf/types/known/emptypb"
+)
+
+type SessionClientProvider struct {
+	session trafficmgr.Session
+}
+
+type SessionClient struct {
+	userdaemon.SystemAClient
+}
+
+func (c *SessionClient) Close(ctx context.Context) error {
+	return nil
+}
+
+func (p *SessionClientProvider) GetSystemaAddress(ctx context.Context) (string, error) {
+	managerClient := p.session.ManagerClient()
+	cloudConfig, err := managerClient.GetCloudConfig(ctx, &emptypb.Empty{})
+	if err != nil {
+		return "", err
+	}
+	return net.JoinHostPort(cloudConfig.GetHost(), cloudConfig.GetPort()), nil
+}
+
+func (p *SessionClientProvider) GetAPIKey(ctx context.Context) (string, error) {
+	return cliutil.GetCloudAPIKey(ctx, a8rcloud.KeyDescWorkstation, true)
+}
+
+func (p *SessionClientProvider) GetInstallID(ctx context.Context) (string, error) {
+	return "", nil
+}
+
+func (p *SessionClientProvider) GetExtraHeaders(ctx context.Context) (map[string]string, error) {
+	return map[string]string{}, nil
+}
+
+func (p *SessionClientProvider) BuildClient(ctx context.Context, conn *grpc.ClientConn) (*SessionClient, error) {
+	client := userdaemon.NewSystemAClient(conn)
+	return &SessionClient{client}, nil
+}
