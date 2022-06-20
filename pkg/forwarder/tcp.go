4b56d40927225c555e96e3feafff1566a9e54abe
Thomas Hallgren <thomas@datawire.io>
2022-06-20 13:36:59 +0200

    Enable UDP intercepts
    Adds an UDP implementation of the `forwarder.Interceptor` that uses
    a new `udpListener` `Endpoint`. The listener spawns new connections
    for each unique reply address in the incoming datagrams so that it
    can maintain forwarding of multiple bidirectional connections made to
    the same port.
    
    Signed-off-by: Thomas Hallgren <thomas@datawire.io>

---
+++ pkg/forwarder/tcp.go
@@ -0,0 +1,171 @@
+package forwarder
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"net"
+	"time"
+
+	"github.com/datawire/dlib/dlog"
+	"github.com/telepresenceio/telepresence/rpc/v2/manager"
+	"github.com/telepresenceio/telepresence/v2/pkg/iputil"
+	"github.com/telepresenceio/telepresence/v2/pkg/tunnel"
+)
+
+type tcp struct {
+	interceptor
+}
+
+func newTCP(listen net.Addr, targetHost string, targetPort uint16) Interceptor {
+	return &tcp{
+		interceptor: interceptor{
+			listenAddr: listen,
+			targetHost: targetHost,
+			targetPort: targetPort,
+		},
+	}
+}
+
+func (f *tcp) Serve(ctx context.Context) error {
+	listener, err := f.Listen(ctx)
+	if err != nil {
+		return err
+	}
+	defer listener.Close()
+
+	dlog.Debugf(ctx, "Forwarding from %s", f.listenAddr.String())
+	defer dlog.Debugf(ctx, "Done forwarding from %s", f.listenAddr.String())
+
+	go func() {
+		<-ctx.Done()
+		listener.Close()
+	}()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+		default:
+		}
+
+		conn, err := listener.AcceptTCP()
+		if err != nil {
+			if ctx.Err() != nil {
+				return nil
+			}
+			dlog.Infof(ctx, "Error on accept: %+v", err)
+			continue
+		}
+		go func() {
+			if err := f.forwardConn(conn); err != nil {
+				dlog.Error(ctx, err)
+			}
+		}()
+	}
+}
+
+func (f *tcp) Listen(ctx context.Context) (*net.TCPListener, error) {
+	f.mu.Lock()
+
+	// Set up listener lifetime (same as the overall forwarder lifetime)
+	f.lCtx, f.lCancel = context.WithCancel(ctx)
+	f.lCtx = dlog.WithField(f.lCtx, "lis", f.listenAddr.String())
+
+	// Set up target lifetime
+	f.tCtx, f.tCancel = context.WithCancel(f.lCtx)
+	listenAddr := f.listenAddr
+
+	f.mu.Unlock()
+	return net.ListenTCP("tcp", listenAddr.(*net.TCPAddr))
+}
+
+func (f *tcp) forwardConn(clientConn *net.TCPConn) error {
+	f.mu.Lock()
+	ctx := f.tCtx
+	targetHost := f.targetHost
+	targetPort := f.targetPort
+	intercept := f.intercept
+	f.mu.Unlock()
+	if intercept != nil {
+		return f.interceptConn(ctx, clientConn, intercept)
+	}
+
+	targetAddr, err := net.ResolveTCPAddr("tcp", fmt.Sprintf("%s:%d", targetHost, targetPort))
+	if err != nil {
+		return fmt.Errorf("error on resolve(%s:%d): %w", targetHost, targetPort, err)
+	}
+
+	ctx = dlog.WithField(ctx, "client", clientConn.RemoteAddr().String())
+	ctx = dlog.WithField(ctx, "target", targetAddr.String())
+
+	dlog.Debug(ctx, "Forwarding...")
+	defer dlog.Debug(ctx, "Done forwarding")
+
+	defer clientConn.Close()
+
+	targetConn, err := net.DialTCP("tcp", nil, targetAddr)
+	if err != nil {
+		return fmt.Errorf("error on dial: %w", err)
+	}
+	defer targetConn.Close()
+
+	done := make(chan struct{})
+
+	go func() {
+		if _, err := io.Copy(targetConn, clientConn); err != nil {
+			dlog.Debugf(ctx, "Error clientConn->targetConn: %+v", err)
+		}
+		_ = targetConn.CloseWrite()
+		done <- struct{}{}
+	}()
+	go func() {
+		if _, err := io.Copy(clientConn, targetConn); err != nil {
+			dlog.Debugf(ctx, "Error targetConn->clientConn: %+v", err)
+		}
+		_ = clientConn.CloseWrite()
+		done <- struct{}{}
+	}()
+
+	// Wait for both sides to close the connection
+	for numClosed := 0; numClosed < 2; {
+		select {
+		case <-ctx.Done():
+			return nil
+		case <-done:
+			numClosed++
+		}
+	}
+	return nil
+}
+
+func (f *interceptor) interceptConn(ctx context.Context, conn net.Conn, iCept *manager.InterceptInfo) error {
+	addr := conn.RemoteAddr()
+	dlog.Infof(ctx, "Accept got connection from %s", addr)
+
+	srcIp, srcPort, err := iputil.SplitToIPPort(addr)
+	if err != nil {
+		return fmt.Errorf("failed to parse intercept source address %s", addr)
+	}
+
+	spec := iCept.Spec
+	destIp := iputil.Parse(spec.TargetHost)
+	id := tunnel.NewConnID(tunnel.IPProto(addr.Network()), srcIp, destIp, srcPort, uint16(spec.TargetPort))
+
+	ms, err := f.manager.Tunnel(ctx)
+	if err != nil {
+		return fmt.Errorf("call to manager.Tunnel() failed. Id %s: %v", id, err)
+	}
+
+	s, err := tunnel.NewClientStream(ctx, ms, id, f.sessionInfo.SessionId, time.Duration(spec.RoundtripLatency), time.Duration(spec.DialTimeout))
+	if err != nil {
+		return err
+	}
+	if err = s.Send(ctx, tunnel.SessionMessage(iCept.ClientSession.SessionId)); err != nil {
+		return fmt.Errorf("unable to send client session id. Id %s: %v", id, err)
+	}
+	d := tunnel.NewConnEndpoint(s, conn)
+	d.Start(ctx)
+	<-d.Done()
+	return nil
+}
---
+++ pkg/forwarder/udp.go
@@ -0,0 +1,206 @@
+package forwarder
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"time"
+
+	"github.com/datawire/dlib/dlog"
+	"github.com/telepresenceio/telepresence/rpc/v2/manager"
+	"github.com/telepresenceio/telepresence/v2/pkg/iputil"
+	"github.com/telepresenceio/telepresence/v2/pkg/tunnel"
+)
+
+type udp struct {
+	interceptor
+	targets *tunnel.Pool
+}
+
+func newUDP(listen *net.UDPAddr, targetHost string, targetPort uint16) Interceptor {
+	return &udp{
+		interceptor: interceptor{
+			listenAddr: listen,
+			targetHost: targetHost,
+			targetPort: targetPort,
+		},
+		targets: tunnel.NewPool(),
+	}
+}
+
+func (f *udp) Serve(ctx context.Context) error {
+	// Set up listener lifetime (same as the overall forwarder lifetime)
+	f.mu.Lock()
+	la := f.listenAddr
+	ctx, f.lCancel = context.WithCancel(ctx)
+	ctx = dlog.WithField(ctx, "lis", la.String())
+	f.lCtx = ctx
+
+	// Set up target lifetime
+	f.tCtx, f.tCancel = context.WithCancel(ctx)
+	f.mu.Unlock()
+
+	defer func() {
+		f.lCancel()
+		f.targets.CloseAll(ctx)
+		dlog.Infof(ctx, "Done forwarding udp from %s", la)
+	}()
+
+	dlog.Infof(ctx, "Forwarding udp from %s", la)
+	for {
+		f.mu.Lock()
+		ctx = f.tCtx
+		intercept := f.intercept
+		f.mu.Unlock()
+		if ctx.Err() != nil {
+			return nil
+		}
+		if err := f.forward(ctx, intercept); err != nil {
+			return err
+		}
+	}
+}
+
+func (f *udp) forward(ctx context.Context, intercept *manager.InterceptInfo) error {
+	lc := net.ListenConfig{}
+	pc, err := lc.ListenPacket(ctx, f.listenAddr.Network(), f.listenAddr.String())
+	if err == nil {
+		defer pc.Close()
+		conn := pc.(*net.UDPConn)
+		if intercept != nil {
+			err = f.interceptConn(ctx, conn, intercept)
+		} else {
+			err = f.forwardConn(ctx, conn)
+		}
+	}
+	return err
+}
+
+// forwardConn reads packets from the given connection and writes the packages to the
+// target host:port of this forwarder using a connection that will use the reply address
+// from the read as the destination for packages going in the other direction.
+func (f *udp) forwardConn(ctx context.Context, conn *net.UDPConn) error {
+	la := f.listenAddr
+	targetAddr, err := net.ResolveUDPAddr("udp", fmt.Sprintf("%s:%d", f.targetHost, f.targetPort))
+	if err != nil {
+		return fmt.Errorf("error on resolve(%s:%d): %w", f.targetHost, f.targetPort, err)
+	}
+
+	dlog.Infof(ctx, "Forwarding udp from %s to %s", la, targetAddr)
+	defer func() {
+		f.targets.CloseAll(ctx)
+		dlog.Infof(ctx, "Done forwarding udp from %s to %s", la, targetAddr)
+	}()
+
+	ch := make(chan tunnel.UdpReadResult)
+	go tunnel.UdpReader(ctx, conn, ch)
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+		case rr, ok := <-ch:
+			if !ok {
+				return nil
+			}
+			id := tunnel.ConnIDFromUDP(rr.Addr, targetAddr)
+			dlog.Tracef(ctx, "<- SRC udp %s, len %d", id, len(rr.Payload))
+			h, _, err := f.targets.GetOrCreate(ctx, id, func(ctx context.Context, release func()) (tunnel.Handler, error) {
+				tc, err := net.DialUDP("udp", nil, id.DestinationAddr().(*net.UDPAddr))
+				if err != nil {
+					return nil, err
+				}
+				return &udpHandler{
+					UDPConn:   tc,
+					id:        id,
+					replyWith: conn,
+					release:   release,
+				}, nil
+			})
+			if err != nil {
+				return err
+			}
+			uh := h.(*udpHandler)
+			pn := len(rr.Payload)
+			for n := 0; n < pn; {
+				wn, err := uh.Write(rr.Payload[n:])
+				if err != nil {
+					dlog.Errorf(ctx, "!! TRG udp %s write: %v", id, err)
+					return err
+				}
+				dlog.Tracef(ctx, "-> TRG udp %s, len %d", id, wn)
+				n += wn
+			}
+		}
+	}
+}
+
+type udpHandler struct {
+	*net.UDPConn
+	id        tunnel.ConnID
+	replyWith net.PacketConn
+	release   func()
+}
+
+func (u *udpHandler) Close() error {
+	u.release()
+	return u.UDPConn.Close()
+}
+
+func (u *udpHandler) Stop(_ context.Context) {
+	_ = u.Close()
+}
+
+func (u *udpHandler) Start(ctx context.Context) {
+	go u.forward(ctx)
+}
+
+func (u *udpHandler) forward(ctx context.Context) {
+	ch := make(chan tunnel.UdpReadResult)
+	go tunnel.UdpReader(ctx, u, ch)
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case rr, ok := <-ch:
+			if !ok {
+				return
+			}
+			dlog.Tracef(ctx, "<- TRG udp %s, len %d", u.id, len(rr.Payload))
+			pn := len(rr.Payload)
+			for n := 0; n < pn; {
+				wn, err := u.replyWith.WriteTo(rr.Payload[n:], u.id.SourceAddr())
+				if err != nil {
+					dlog.Errorf(ctx, "!! SRC udp %s write: %v", u.id, err)
+					return
+				}
+				dlog.Tracef(ctx, "-> SRC udp %s, len %d", u.id, wn)
+				n += wn
+			}
+		}
+	}
+}
+
+func (f *udp) interceptConn(ctx context.Context, conn *net.UDPConn, iCept *manager.InterceptInfo) error {
+	spec := iCept.Spec
+	dest := &net.UDPAddr{IP: iputil.Parse(spec.TargetHost), Port: int(spec.TargetPort)}
+
+	dlog.Infof(ctx, "Forwarding udp from %s to %s %s", conn.LocalAddr(), spec.Client, dest)
+	defer dlog.Infof(ctx, "Done forwarding udp from %s to %s %s", conn.LocalAddr(), spec.Client, dest)
+	d := tunnel.NewUDPListener(conn, dest, func(ctx context.Context, id tunnel.ConnID) (tunnel.Stream, error) {
+		ms, err := f.manager.Tunnel(ctx)
+		if err != nil {
+			return nil, fmt.Errorf("call to manager.Tunnel() failed. Id %s: %v", id, err)
+		}
+		s, err := tunnel.NewClientStream(ctx, ms, id, f.sessionInfo.SessionId, time.Duration(spec.RoundtripLatency), time.Duration(spec.DialTimeout))
+		if err != nil {
+			return nil, err
+		}
+		if err = s.Send(ctx, tunnel.SessionMessage(iCept.ClientSession.SessionId)); err != nil {
+			return nil, fmt.Errorf("unable to send client session id. Id %s: %v", id, err)
+		}
+		return s, nil
+	})
+	d.Start(ctx)
+	<-d.Done()
+	return nil
+}
