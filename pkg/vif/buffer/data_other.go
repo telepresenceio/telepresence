4968900efc0a15f6f972180cbbce34cb9a1924cd
Thomas Hallgren <thomas@datawire.io>
2022-07-12 12:56:01 +0200

    Get rid of data pooling in the network stack
    The network stack is fairly complex and not really a good candidate
    for a data pool. Packages differ in size and there's no single place to
    release the packets. Tests show that there's virtually no performance
    gain in using the pool, so this commit removes it.
    
    Signed-off-by: Thomas Hallgren <thomas@datawire.io>

--- pkg/vif/buffer/data_other.go
+++ pkg/vif/buffer/data_other.go
@@ -9,28 +9,22 @@ type Data struct {
 
 // Buf returns this Data's buffer. This is the buffer that should be used everywhere
 // except for the tun.Device ReadPacket and WritePacket methods.
-func (b *Data) Buf() []byte {
-	return b.buf
+func (d *Data) Buf() []byte {
+	return d.buf
 }
 
-// Copy copies n bytes from the given Data buffer into a new Data which is obtained from
-// the pool, and returns the new data.
-func (p *Pool) Copy(s *Data, n int) *Data {
-	c := p.Get(n)
-	copy(c.buf, s.buf)
+// Copy copies n bytes from the given Data buffer into a new Data and returns it.
+func (d *Data) Copy(n int) *Data {
+	c := NewData(n)
+	copy(c.buf, d.buf)
 	return c
-}
-
-// setLength sets the length of this buffer. This will change the slice that Buf and Raw returns
-func (b *Data) setLength(l int) {
-	b.buf = b.buf[:l]
 }
 
 // Raw returns this Data's raw buffer. This is the buffer that should be used by the tun.Device
 // ReadPacket and WritePacket methods. It uses the same underlying byte array as Buf but might be
 // offset before Buf to allow for leading bytes that are provided before the IP header.
-func (b *Data) Raw() []byte {
-	return b.buf
+func (d *Data) Raw() []byte {
+	return d.buf
 }
 
 func NewData(sz int) *Data {
