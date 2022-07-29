a68ac5fc7ada2e7913a4a09a40f2721d0e4f3af2
Raphael Reyna <raphaelreyna@protonmail.com>
2022-07-29 14:21:59 -0700

    fix volume mount path issues
    Signed-off-by: Raphael Reyna <raphaelreyna@protonmail.com>

--- pkg/client/cli/cliutil/prepare_mount_unix.go
+++ pkg/client/cli/cliutil/prepare_mount_unix.go
@@ -8,15 +8,16 @@ import (
 	"path/filepath"
 )
 
-func PrepareMount(mountPoint string) (string, error) {
+func PrepareMount(cwd string, mountPoint string) (string, error) {
 	if mountPoint == "" {
 		return os.MkdirTemp("", "telfs-")
-	} else {
-		var err error
-		mountPoint, err = filepath.Abs(mountPoint)
-		if err != nil {
-			return "", err
-		}
 	}
+
+	// filepath.Abs uses os.Getwd but we need the working dir of the cli
+	if !filepath.IsAbs(mountPoint) {
+		mountPoint = filepath.Join(cwd, mountPoint)
+		mountPoint = filepath.Clean(mountPoint)
+	}
+
 	return mountPoint, os.MkdirAll(mountPoint, 0700)
 }
