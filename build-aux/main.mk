f19036bef82cb7500c3f62c5e2a14ab44663250b
Signed-off-by: Christian Moser <onmomo@users.noreply.github.com>
2022-08-30 09:57:57 +0200

    use buildx to build for linux/amd64 cluster
    Signed-off-by: Christian Moser <christian.moser@starmind.com>

--- build-aux/main.mk
+++ build-aux/main.mk
@@ -134,7 +134,7 @@ endif
 tel2:
 	mkdir -p $(BUILDDIR)
 	printf $(TELEPRESENCE_VERSION) > $(BUILDDIR)/version.txt ## Pass version in a file instead of a --build-arg to maximize cache usage
-	docker build --target $@ --tag $@ --tag $(TELEPRESENCE_REGISTRY)/$@:$(patsubst v%,%,$(TELEPRESENCE_VERSION)) -f base-image/Dockerfile .
+	docker buildx build --platform linux/amd64 --target $@ --tag "$@" --tag $(TELEPRESENCE_REGISTRY)/$@:$(patsubst v%,%,$(TELEPRESENCE_VERSION)) -f base-image/Dockerfile .
 
 .PHONY: push-image
 push-image: tel2 ## (Build) Push the manager/agent container image to $(TELEPRESENCE_REGISTRY)
