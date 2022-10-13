b44c4fac3f5fe982ce4e4ca4ece007d3e459b0f6
Rafael Schloming <rhs@datawire.io>
2022-10-13 10:59:49 -0400

    Fix typo.
    Signed-off-by: Rafael Schloming <rhs@datawire.io>

--- .github/actions/provision-cluster/delete.js
+++ .github/actions/provision-cluster/delete.js
@@ -4,7 +4,7 @@ const github = require('@actions/github');
 try {
   // inputs are defined in action metadata file
   const distribution = core.getInput('distribution');
-  const distribution = core.getInput('version');
+  const version = core.getInput('version');
   console.log(`Deleting ${distribution} ${version}!`);
   // Get the JSON webhook payload for the event that triggered the workflow
   //const payload = JSON.stringify(github.context.payload, undefined, 2)
