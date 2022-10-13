82daace7974858e9abe1ea5389a467cebfaab526
Rafael Schloming <rhs@datawire.io>
2022-10-13 13:18:43 -0400

    Write a mock kubeconfig file.
    Signed-off-by: Rafael Schloming <rhs@datawire.io>

--- .github/actions/provision-cluster/create.js
+++ .github/actions/provision-cluster/create.js
@@ -1,12 +1,18 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const fs = require('fs');
 
 try {
   // inputs are defined in action metadata file
   const distribution = core.getInput('distribution');
   const version = core.getInput('version');
-  console.log(`Creating ${distribution} ${version}!`);
-  core.setOutput('kubeconfig', "Mock kubeconfig file for ${distribution} ${version}.");
+  const kubeconfig = core.getInput('kubeconfig');
+  console.log(`Creating ${distribution} ${version} and writing kubeconfig to file: ${kubeconfig}!`);
+  fs.writeFile(kubeconfig, `Mock kubeconfig file for ${distribution} ${version}.\n`, err => {
+    if (err) {
+      core.setFailed(`${err}`);
+    }
+  });
   // Get the JSON webhook payload for the event that triggered the workflow
   //const payload = JSON.stringify(github.context.payload, undefined, 2)
   //console.log(`The event payload: ${payload}`);
