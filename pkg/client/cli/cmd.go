e3e7bf10cf17ae4a4761815f1622a8802be8f7d9
Thomas Hallgren <thomas@datawire.io>
2022-11-08 06:15:36 +0100

    Revert "Add shorthand "-o" for the global "--output" flag."
    This reverts commit 6b4f89c7f63fbee5355fdf261bc7372d7cee926a.

--- pkg/client/cli/cmd.go
+++ pkg/client/cli/cmd.go
@@ -269,8 +269,8 @@ func GlobalFlags() *pflag.FlagSet {
 		"no-report", false,
 		"turn off anonymous crash reports and log submission on failure",
 	)
-	flags.StringP(
-		"output", "o", "default",
+	flags.String(
+		"output", "default",
 		"set the output format, supported values are 'json', 'yaml', and 'default'",
 	)
 	return flags
