69a71189a294d896f1bc174d578b1677f843dc8e
Jose Cortes <josecortes@datawire.io>
2023-04-19 12:38:26 -0400

    Fix parsing of default routes in macos
    Signed-off-by: Jose Cortes <josecortes@datawire.io>

--- pkg/iputil/ipnet.go
+++ pkg/iputil/ipnet.go
@@ -78,3 +78,12 @@ func (s *Subnet) UnmarshalYAML(node *yaml.Node) error {
 func (s *Subnet) String() string {
 	return (*net.IPNet)(s).String()
 }
+
+func IsZeroMask(n *net.IPNet) bool {
+	for _, b := range n.Mask {
+		if b != 0 {
+			return false
+		}
+	}
+	return true
+}
