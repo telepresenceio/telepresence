896ca6ac1b8f207d59fc0b6670efc20d83af3510
Jose Cortes <josecortes@datawire.io>
2023-04-21 11:53:59 -0400

    Add a mechanism to whitelist routes in the face of conflicts.
    Signed-off-by: Jose Cortes <josecortes@datawire.io>

--- pkg/vif/router.go
+++ pkg/vif/router.go
@@ -8,7 +8,6 @@ import (
 	"go.opentelemetry.io/otel"
 
 	"github.com/datawire/dlib/dlog"
-	"github.com/telepresenceio/telepresence/v2/pkg/iputil"
 	"github.com/telepresenceio/telepresence/v2/pkg/routing"
 	"github.com/telepresenceio/telepresence/v2/pkg/subnet"
 	"github.com/telepresenceio/telepresence/v2/pkg/tracing"
@@ -23,10 +22,24 @@ type Router struct {
 	staticOverrides []*routing.Route
 	// The subnets that are currently being routed
 	routedSubnets []*net.IPNet
+	// The subnets that are allowed to be routed even in the presence of conflicting routes
+	whitelistedSubnets []*net.IPNet
 }
 
 func NewRouter(device Device) *Router {
 	return &Router{device: device}
+}
+
+func (rt *Router) firstAndLastIPs(n *net.IPNet) (net.IP, net.IP) {
+	firstIP := make(net.IP, len(n.IP))
+	lastIP := make(net.IP, len(n.IP))
+	copy(firstIP, n.IP)
+	copy(lastIP, n.IP)
+	for i := range n.Mask {
+		firstIP[i] &= n.Mask[i]
+		lastIP[i] |= ^n.Mask[i]
+	}
+	return firstIP, lastIP
 }
 
 func (rt *Router) isNeverProxied(n *net.IPNet) bool {
@@ -42,20 +55,41 @@ func (rt *Router) GetRoutedSubnets() []*net.IPNet {
 	return rt.routedSubnets
 }
 
+func (rt *Router) UpdateWhitelist(whitelist []*net.IPNet) {
+	rt.whitelistedSubnets = whitelist
+}
+
 func (rt *Router) ValidateRoutes(ctx context.Context, routes []*net.IPNet) error {
 	// We need the entire table because we need to check for any overlaps, not just "is this IP already routed"
 	table, err := routing.GetRoutingTable(ctx)
 	if err != nil {
 		return err
 	}
+	_, nonWhitelisted := subnet.Partition(routes, func(_ int, r *net.IPNet) bool {
+		first, last := rt.firstAndLastIPs(r)
+		for _, w := range rt.whitelistedSubnets {
+			if w.Contains(first) && w.Contains(last) {
+				// This is a whitelisted subnet, so we'll overlap it if needed
+				return true
+			}
+		}
+		return false
+	})
+	// Slightly awkward nested loops, since they can both continue (i.e. there's probably wasted iterations) but it's okay, there's not gonna be hundreds of routes.
+	// In any case, we really wanna run over the table as the outer loop, since it's bigger.
 	for _, tr := range table {
 		dlog.Tracef(ctx, "checking for overlap with route %q", tr)
-		if iputil.IsZeroMask(tr.RoutedNet) || tr.Default {
+		if subnet.IsZeroMask(tr.RoutedNet) || tr.Default {
 			// This is a default route, so we'll overlap it if needed
 			continue
+		} else if subnet.IsHalfOfDefault(tr.RoutedNet) {
+			// Some VPN providers will cover one half of the address space with a /1 mask, and the other half with another.
+			// When we run into that just keep going and treat it as a default route that we can overlap.
+			// Crucially, OpenVPN is one of the providers who does this; luckily they will also add specific routes for the subnets that are covered by the VPC (e.g. /8 or /24 routes); the /1 routes are just for the public internet.
+			continue
 		}
-		for _, r := range routes {
-			if routing.SubnetsOverlap(r, tr.RoutedNet) {
+		for _, r := range nonWhitelisted {
+			if subnet.Overlaps(tr.RoutedNet, r) {
 				return fmt.Errorf("subnet %s overlaps with existing route %q", r, tr)
 			}
 		}
