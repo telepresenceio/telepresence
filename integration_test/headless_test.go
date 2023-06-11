19c77f000d1ea2f8106eb451f0a28fd06d0eab54
Thomas Hallgren <thomas@datawire.io>
2023-06-11 15:52:08 +0200

    Remove redundant tests.
    Annotations controlling on-demand versus up-front injections are tested
    elsewhere so this test now just checks if a headless servce can be
    intercepted.
    
    Signed-off-by: Thomas Hallgren <thomas@datawire.io>

--- integration_test/headless_test.go
+++ integration_test/headless_test.go
@@ -2,7 +2,6 @@ package integration_test
 
 import (
 	"context"
-	"fmt"
 	"strconv"
 	"strings"
 	"time"
@@ -24,107 +23,21 @@ func (s *connectedSuite) Test_SuccessfullyInterceptsHeadlessService() {
 
 	s.ApplyApp(ctx, "echo-headless", "statefulset/echo-headless")
 	defer s.DeleteSvcAndWorkload(ctx, "statefulset", "echo-headless")
+	require := s.Require()
+	stdout := itest.TelepresenceOk(ctx, "intercept", "--namespace", s.AppNamespace(), "--mount", "false", svc, "--port", strconv.Itoa(svcPort))
+	require.Contains(stdout, "Using StatefulSet echo-headless")
+	s.CapturePodLogs(ctx, "service=echo-headless", "traffic-agent", s.AppNamespace())
 
-	for _, test := range []struct {
-		webhook bool
-		name    string
-	}{
-		{
-			webhook: true,
-			name:    "injected from webhook",
+	defer itest.TelepresenceOk(ctx, "leave", "echo-headless-"+s.AppNamespace())
+
+	require.Eventually(
+		func() bool {
+			stdout, _, err := itest.Telepresence(ctx, "list", "--namespace", s.AppNamespace(), "--intercepts")
+			return err == nil && strings.Contains(stdout, "echo-headless: intercepted")
 		},
-		{
-			webhook: false,
-			name:    "injected from command",
-		},
-	} {
-		s.Run(test.name, func() {
-			require := s.Require()
-			ctx := s.Context()
-			if test.webhook {
-				require.NoError(annotateForWebhook(ctx, "statefulset", "echo-headless", s.AppNamespace(), 8080))
-				require.Eventually(
-					func() bool {
-						stdout, _, err := itest.Telepresence(ctx, "list", "--namespace", s.AppNamespace(), "--agents")
-						return err == nil && strings.Contains(stdout, "echo-headless: ready to intercept")
-					},
-					30*time.Second, // waitFor
-					3*time.Second,  // polling interval
-					`never gets install agent`)
-			}
-			stdout := itest.TelepresenceOk(ctx, "intercept", "--namespace", s.AppNamespace(), "--mount", "false", svc, "--port", strconv.Itoa(svcPort))
-			require.Contains(stdout, "Using StatefulSet echo-headless")
-			s.CapturePodLogs(ctx, "service=echo-headless", "traffic-agent", s.AppNamespace())
+		30*time.Second, // waitFor
+		3*time.Second,  // polling interval
+		`intercepted workload never show up in list`)
 
-			defer func() {
-				itest.TelepresenceOk(ctx, "leave", "echo-headless-"+s.AppNamespace())
-				if test.webhook {
-					require.NoError(dropWebhookAnnotation(ctx, "statefulset", "echo-headless", s.AppNamespace()))
-				}
-
-				// Switch to default user and uninstall the agent
-				itest.TelepresenceQuitOk(ctx)
-				dfltCtx := itest.WithUser(ctx, "default")
-				itest.TelepresenceOk(dfltCtx, "uninstall", "--agent", "echo-headless", "-n", s.AppNamespace())
-				itest.TelepresenceQuitOk(dfltCtx)
-				itest.TelepresenceOk(ctx, "connect", "--manager-namespace", s.ManagerNamespace())
-
-				require.Eventually(
-					func() bool {
-						stdout, _, err := itest.Telepresence(ctx, "list", "--namespace", s.AppNamespace(), "--agents")
-						return err == nil && !strings.Contains(stdout, "echo-headless")
-					},
-					30*time.Second, // waitFor
-					3*time.Second,  // polling interval
-					`agent is never removed`)
-			}()
-
-			require.Eventually(
-				func() bool {
-					stdout, _, err := itest.Telepresence(ctx, "list", "--namespace", s.AppNamespace(), "--intercepts")
-					return err == nil && strings.Contains(stdout, "echo-headless: intercepted")
-				},
-				30*time.Second, // waitFor
-				3*time.Second,  // polling interval
-				`intercepted workload never show up in list`)
-
-			itest.PingInterceptedEchoServer(ctx, svc, "8080")
-		})
-	}
-}
-
-func annotateForWebhook(ctx context.Context, objKind, objName, objNamespace string, servicePort int) error {
-	err := itest.Kubectl(ctx, objNamespace, "patch", objKind, objName, "-p", fmt.Sprintf(`
-{
-	"spec": {
-		"template": {
-			"metadata": {
-				"annotations": {
-					"telepresence.getambassador.io/inject-traffic-agent": "enabled",
-					"telepresence.getambassador.io/inject-service-port": "%d"
-				}
-			}
-		}
-	}
-}`, servicePort))
-	if err != nil {
-		return err
-	}
-	return itest.RolloutStatusWait(ctx, objNamespace, objKind+"/"+objName)
-}
-
-func dropWebhookAnnotation(ctx context.Context, objKind, objName, objNamespace string) error {
-	err := itest.Kubectl(ctx, objNamespace, "patch", objKind, objName, "--type=json", "-p", `[{
-	"op": "remove",
-	"path": "/spec/template/metadata/annotations/telepresence.getambassador.io~1inject-traffic-agent"
-},
-{
-	"op": "remove",
-	"path": "/spec/template/metadata/annotations/telepresence.getambassador.io~1inject-service-port"
-}
-]`)
-	if err != nil {
-		return err
-	}
-	return itest.RolloutStatusWait(ctx, objNamespace, objKind+"/"+objName)
+	itest.PingInterceptedEchoServer(ctx, svc, "8080")
 }
