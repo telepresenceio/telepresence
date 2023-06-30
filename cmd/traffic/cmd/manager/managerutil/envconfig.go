7f1d6f3a0d4fb538e8c4e1e3ff2acaa0c256b775
njayp <nickjaypowell@gmail.com>
2023-06-29 22:41:52 -0700

    update chart readme
    Signed-off-by: njayp <nickjaypowell@gmail.com>

--- cmd/traffic/cmd/manager/managerutil/envconfig.go
+++ cmd/traffic/cmd/manager/managerutil/envconfig.go
@@ -52,7 +52,7 @@ type Env struct {
 
 	AgentRegistry             string                      `env:"AGENT_REGISTRY,           parser=nonempty-string"`
 	AgentImage                string                      `env:"AGENT_IMAGE,              parser=string,         default="`
-	AgentImagePullPolicy      string                      `env:"AGENT_IMAGE_PULL_POLICY,  parser=string,		 default="`
+	AgentImagePullPolicy      string                      `env:"AGENT_IMAGE_PULL_POLICY,  parser=string,         default="`
 	AgentImagePullSecrets     []core.LocalObjectReference `env:"AGENT_IMAGE_PULL_SECRETS, parser=json-local-refs,default="`
 	AgentInjectPolicy         agentconfig.InjectPolicy    `env:"AGENT_INJECT_POLICY,      parser=enable-policy"`
 	AgentAppProtocolStrategy  k8sapi.AppProtocolStrategy  `env:"AGENT_APP_PROTO_STRATEGY, parser=app-proto-strategy"`
