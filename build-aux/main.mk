578d983e0da2aa663d47907c02cc533cba7bb6c6
Jose Cortes <josecortes@datawire.io>
2023-09-06 14:39:32 -0400

    Test-report no longer depends on telepresence/rpc/v2
    Signed-off-by: Jose Cortes <josecortes@datawire.io>

--- build-aux/main.mk
+++ build-aux/main.mk
@@ -250,10 +250,6 @@ prepare-release: generate wix
 	  go mod edit -require=github.com/telepresenceio/telepresence/rpc/v2@$(TELEPRESENCE_VERSION) && \
 	  git add go.mod)
 
-	(cd tools/src/test-report && \
-	  go mod edit -require=github.com/telepresenceio/telepresence/rpc/v2@$(TELEPRESENCE_VERSION) && \
-	  git add go.mod)
-
 	sed -i.bak "s/^### (TBD).*/### $(TELEPRESENCE_VERSION)/" charts/telepresence/CHANGELOG.md
 	rm -f charts/telepresence/CHANGELOG.md.bak
 	git add charts/telepresence/CHANGELOG.md
