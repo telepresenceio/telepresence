d440430d065b9feb6d7093ba66545c52e6508bf8
Thomas Hallgren <thomas@datawire.io>
2024-06-24 15:36:11 +0200

    Introduce DNS fallback on Windows.
    Telepresence belongs to a virtual network device with excellent network
    performance, and is therefore often rated with a low metric and placed
    first in the list of DNS servers.
    
    Utilities using `nslookup` or `node.resolveXXX` calls fail when
    Telepresence is connected, because those methods will use the first
    resolver in the list and then give up when it fails.
    
    This commit introduces a `network.defaultDNSWithFallback` option on
    Windows. It will cause the DNS-resolver to fall back to the resolver
    that was first in the list prior to when Telepresence establishes a
    connection. The option is default `true` since it is believed to give
    the best experience but can be set to false to get the old behavior.
    
    Signed-off-by: Thomas Hallgren <thomas@datawire.io>

--- pkg/client/config_windows.go
+++ pkg/client/config_windows.go
@@ -28,18 +28,25 @@ func (c *OSSpecificConfig) Merge(o *OSSpecificConfig) {
 type GSCStrategy string
 
 const (
+	defaultDNSWithFallback = true
+
 	// defaultVirtualIPSubnet is an IP that, on windows, is built from 16 class C subnets which were chosen randomly,
 	// hoping that they don't collide with another subnet.
 	defaultVirtualIPSubnet = "211.55.48.0/20"
 )
 
 type Network struct {
+	DNSWithFallback bool `json:"dnsWithFallback,omitempty" yaml:"dnsWithFallback,omitempty"`
 }
 
 func (n *Network) merge(o *Network) {
+	if o.DNSWithFallback != defaultDNSWithFallback { //nolint:gosimple // explicit default comparison
+		n.DNSWithFallback = o.DNSWithFallback
+	}
 }
 
 func (n Network) IsZero() bool {
+	return n.DNSWithFallback == defaultDNSWithFallback //nolint:gosimple // explicit default comparison
 }
 
 func (n *Network) UnmarshalYAML(node *yaml.Node) (err error) {
@@ -55,6 +62,11 @@ func (n *Network) UnmarshalYAML(node *yaml.Node) (err error) {
 		}
 		v := ms[i+1]
 		switch kv {
+		case "dnsWithFallback":
+			err = v.Decode(&n.DNSWithFallback)
+			if err != nil {
+				return err
+			}
 		case "globalDNSSearchConfigStrategy":
 			logrus.Warn(WithLoc(fmt.Sprintf(`deprecated key %q, no longer needed`, kv), ms[i]))
 		default:
--- pkg/client/rootd/dns/server_windows.go
+++ pkg/client/rootd/dns/server_windows.go
@@ -5,8 +5,13 @@ import (
 	"fmt"
 	"net"
 	"time"
+	"unsafe"
+
+	"golang.org/x/sys/windows"
 
 	"github.com/datawire/dlib/dgroup"
+	"github.com/datawire/dlib/dlog"
+	"github.com/telepresenceio/telepresence/v2/pkg/client"
 	"github.com/telepresenceio/telepresence/v2/pkg/vif"
 )
 
@@ -26,6 +31,30 @@ func (s *Server) Worker(c context.Context, dev vif.Device, configureDNS func(net
 	}
 	configureDNS(s.remoteIP, dnsAddr)
 
+	var pool FallbackPool
+	if client.GetConfig(c).OSSpecific().Network.DNSWithFallback {
+		// Create the connection pool later used for fallback.
+		dnsServers, err := getDNSServerList()
+		if err != nil {
+			dlog.Warnf(c, "Failed to get DNS servers: %v", err)
+		} else {
+			for _, dnsServer := range dnsServers {
+				p, err := NewConnPool(dnsServer, 10)
+				if err == nil {
+					dlog.Infof(c, "Using fallback DNS server: %s", dnsServer)
+					pool = p
+					break
+				}
+				dlog.Warn(c, err)
+			}
+			if pool == nil {
+				dlog.Warnf(c, "No viable fallback DNS server found")
+			} else {
+				defer pool.Close()
+			}
+		}
+	}
+
 	// Start local DNS server
 	g := dgroup.NewGroup(c, dgroup.GroupConfig{})
 	g.Go("Server", func(c context.Context) error {
@@ -41,7 +70,7 @@ func (s *Server) Worker(c context.Context, dev vif.Device, configureDNS func(net
 			return err
 		}
 		s.processSearchPaths(g, s.updateRouterDNS, dev)
-		return s.Run(c, make(chan struct{}), []net.PacketConn{listener}, nil, s.resolveInCluster)
+		return s.Run(c, make(chan struct{}), []net.PacketConn{listener}, pool, s.resolveInCluster)
 	})
 	return g.Wait()
 }
@@ -55,4 +84,53 @@ func (s *Server) updateRouterDNS(c context.Context, dev vif.Device) error {
 		return fmt.Errorf("failed to set DNS: %w", err)
 	}
 	return nil
+}
+
+func getDNSServerList() ([]string, error) {
+	iphlpapi := windows.NewLazyDLL("iphlpapi.dll")
+	getNetworkParams := iphlpapi.NewProc("GetNetworkParams")
+
+	// First, call GetNetworkParams with a nil buffer to get the required buffer size
+	var bufferSize uint32
+	ret, _, _ := getNetworkParams.Call(uintptr(unsafe.Pointer(nil)), uintptr(unsafe.Pointer(&bufferSize)))
+	if ret != uintptr(windows.ERROR_BUFFER_OVERFLOW) {
+		return nil, windows.Errno(ret)
+	}
+
+	// Allocate the required buffer size
+	buffer := make([]byte, bufferSize)
+
+	// Call GetNetworkParams with the allocated buffer
+	ret, _, _ = getNetworkParams.Call(uintptr(unsafe.Pointer(&buffer[0])), uintptr(unsafe.Pointer(&bufferSize)))
+	if ret != 0 {
+		return nil, windows.Errno(ret)
+	}
+
+	// Define the FIXED_INFO structure
+	type FIXED_INFO struct {
+		HostName         [132]byte
+		DomainName       [132]byte
+		CurrentDNSServer *windows.IpAddrString
+		DNSServerList    windows.IpAddrString
+		NodeType         uint32
+		ScopeID          [260]byte
+		EnableRouting    uint32
+		EnableProxy      uint32
+		EnableDNS        uint32
+	}
+
+	// Convert buffer to FIXED_INFO structure
+	fi := (*FIXED_INFO)(unsafe.Pointer(&buffer[0]))
+
+	// Traverse the DNS server list
+	sl := fi.DNSServerList
+	var svcs []string
+	for {
+		svcs = append(svcs, windows.BytePtrToString(&sl.IpAddress.String[0]))
+		if sl.Next == nil {
+			break
+		}
+		sl = *sl.Next
+	}
+	return svcs, nil
 }
