5e687ecf8cb0298f881c5b28be525ce979c4348e
Thomas Hallgren <thomas@tada.se>
2025-04-25 07:37:18 +0200

    Add the ability to use a name for the target host.
    Knowing the IP of the local container that acts as the handler service
    for an intercept, replace, or wiretap is not possible until the
    container has been started.
    
    This commit introduces the concept of target host synthetic IP. A named
    target host will be replaced by a random synthesized IPv6 IP when the
    engagement is initialized. Later, when the traffic-agent container dials
    this IP, the original name will be found and resolved into an IP, thus
    deferring the name resolution until an actual request is made to the
    engaged pod.
    
    This allows the engagement to be created before the creation of the
    handler service, and removes the need for the user to use IP addresses.
    
    Signed-off-by: Thomas Hallgren <thomas@tada.se>

--- pkg/tunnel/context.go
+++ pkg/tunnel/context.go
@@ -1,6 +1,9 @@
 package tunnel
 
-import "context"
+import (
+	"context"
+	"net/netip"
+)
 
 type poolKey struct{}
 
@@ -15,4 +18,27 @@ func GetPool(ctx context.Context) *Pool {
 		return nil
 	}
 	return pool
+}
+
+type synIpRsvKey struct{}
+
+func WithSyntheticIPResolver(ctx context.Context, resolver SyntheticIPResolver) context.Context {
+	return context.WithValue(ctx, synIpRsvKey{}, resolver)
+}
+
+type noopSyntheticIPResolver struct{}
+
+func (noopSyntheticIPResolver) Resolve(addr netip.Addr) (netip.Addr, error) {
+	return addr, nil
+}
+
+func (noopSyntheticIPResolver) ResolveName(netip.Addr) string {
+	return ""
+}
+
+func GetSyntheticIPResolver(ctx context.Context) SyntheticIPResolver {
+	if resolver, ok := ctx.Value(synIpRsvKey{}).(SyntheticIPResolver); ok {
+		return resolver
+	}
+	return noopSyntheticIPResolver{}
 }
