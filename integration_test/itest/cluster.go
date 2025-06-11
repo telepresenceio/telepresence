d9c91d70f7cf46828e2b47fe0223fed655b945fe
Thomas Hallgren <thomas@tada.se>
2025-06-11 22:55:51 +0200

    Don't set intercept.mountsRoot when testing clients < 2.23.0
    Signed-off-by: Thomas Hallgren <thomas@tada.se>

--- integration_test/itest/cluster.go
+++ integration_test/itest/cluster.go
@@ -457,7 +457,9 @@ func (s *cluster) withBasicConfig(c context.Context, t *testing.T) context.Conte
 
 	config.Grpc().MaxReceiveSizeV, _ = resource.ParseQuantity("10Mi")
 	config.Intercept().UseFtp = true
-	config.Intercept().MountsRoot = TempDir(c)
+	if s.ClientIsVersion(">=2.23.0") {
+		config.Intercept().MountsRoot = TempDir(c)
+	}
 	config.Routing().RecursionBlockDuration = 10 * time.Millisecond
 	config = config.Merge(client.GetConfig(c))
 
