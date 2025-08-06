ded2332cbacf6f534829b54dd6b9f4d1e1797cb3
Thomas Hallgren <thomas@tada.se>
2025-08-06 05:42:06 +0200

    Make the MountPolicy type case-insensitive.
    Signed-off-by: Thomas Hallgren <thomas@tada.se>

--- pkg/types/mountpolicy.go
+++ pkg/types/mountpolicy.go
@@ -50,7 +50,9 @@ func (mp *MountPolicy) UnmarshalJSONFrom(in *jsontext.Decoder) error {
 	var s string
 	err := json.UnmarshalDecode(in, &s)
 	if err == nil {
-		if ix := slices.Index(mountPolicyNames, s); ix >= 0 {
+		if ix := slices.IndexFunc(mountPolicyNames, func(pn string) bool {
+			return strings.EqualFold(pn, s)
+		}); ix >= 0 {
 			*mp = MountPolicy(ix)
 		} else {
 			err = fmt.Errorf("invalid mount policy: %q", s)
