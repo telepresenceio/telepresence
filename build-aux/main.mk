bf849eb4519ad4fed35e126f561ccdd83cebc67a
Chris Werner Rau <cwrau@cwrau.info>
2025-09-08 10:23:15 +0200

    fix: work with custom go versions, like `go1.25.1 X:nodwarf5`
    Closes #3960
    
    Signed-off-by: Chris Werner Rau <cwrau@cwrau.info>

--- build-aux/main.mk
+++ build-aux/main.mk
@@ -94,7 +94,7 @@ protoc: protoc-clean $(tools/protoc) $(tools/protoc-gen-go) $(tools/protoc-gen-g
 .PHONY: generate
 generate: ## (Generate) Update generated files that get checked in to Git
 generate: generate-clean
-generate: protoc $(tools/go-mkopensource) $(BUILDDIR)/$(shell go env GOVERSION).src.tar.gz
+generate: protoc $(tools/go-mkopensource) $(BUILDDIR)/$(shell go env GOVERSION | awk '{print $$1}').src.tar.gz
 	cd ./rpc && export GOFLAGS=-mod=mod && go mod tidy && go mod vendor && rm -rf vendor
 	cd ./pkg/vif/testdata/router && export GOFLAGS=-mod=mod && go mod tidy && go mod vendor && rm -rf vendor
 	cd ./tools/src/test-report && export GOFLAGS=-mod=mod && go mod tidy && go mod vendor && rm -rf vendor
