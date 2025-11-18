132df2e9a236d18ea5dc3e359ede667144e046a8
Tom van Dinther <39470469+tvandinther@users.noreply.github.com>
2025-11-18 12:07:27 +0100

    Change name
    Signed-off-by: Tom van Dinther <39470469+tvandinther@users.noreply.github.com>

--- pkg/client/k8s/connect.go
+++ pkg/client/k8s/connect.go
@@ -28,7 +28,7 @@ import (
 )
 
 func (kc *Cluster) ConnectToManager(dialCtx context.Context, namespace string) (conn *grpc.ClientConn, name string, ver semver.Version, err error) {
-	grpcAddr := net.JoinHostPort("svc/traffic-manager."+namespace, "http")
+	grpcAddr := net.JoinHostPort("svc/traffic-manager."+namespace, "grpc")
 
 	dialCtx, cancel := client.GetConfig(kc).Timeouts().TimeoutContext(dialCtx, client.TimeoutTrafficManagerConnect)
 	defer cancel()
